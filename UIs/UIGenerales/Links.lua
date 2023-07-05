89cefec6413316f12eebf587bad35f5fbe567133
OneCreatorX <138682257+OneCreatorX@users.noreply.github.com>
2023-07-05 15:00:57 -0300

    Actualizar Links.lua

--- UIs/UIGenerales/Links.lua
+++ UIs/UIGenerales/Links.lua
@@ -2,16 +2,16 @@ Modo Espectador:Final/ModoEspectador
 Caer:Final/Caer
 Anti AFK:Final/AntiAfk
 Caminar sobre una plataforma:Final/CaminarPlataforma
-Fullbright-Brillo aumentado:Final/Fullbright
+Fullbright Brillo aumentado:Final/Fullbright
 Altura de salto-PowerJump:Final/Salto
 Tp Players:Final/TPplayers
-Speed-Velocidad:Final/Velocidad
+Speed Velocidad:Final/Velocidad
 Waypoints:Final/Waypoints
-Tp height-altura:Final/TpAltura
+Tp height altura:Final/TpAltura
 Generador Ininify plataformas FUN1:Final/SobrePlataformasV1
 Generador Ininify plataformas FUN2:Final/SobrePlataformasV2
 Subir +10 plataforma:Final/PlataformaUP+10
 Reproductor de musica ID:Beta/Reproductor
 Tp jugador cercano:Beta/Tpjugadorcercano
 Tp por Name:Beta/TpName
-Quitar Colision-NoClick:ParaMejorar/Nocolision
+Quitar Colision NoClip:ParaMejorar/Nocolision
