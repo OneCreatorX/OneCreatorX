176d0ed4e24529c97185bc4484cc7971394b8cc6
OneCreatorX <138682257+OneCreatorX@users.noreply.github.com>
2023-07-05 21:53:03 -0300

    Actualizar LinksLoadyUI.lua

--- UIs/Loady/LinksLoadyUI.lua
+++ UIs/Loady/LinksLoadyUI.lua
@@ -3,43 +3,55 @@
   "Generales/Tools":{
      "Creator": "OneCreatorX",
     "Status": "Functional  and in Development",
-"Url": "https://raw.githubusercontent.com/OneCreatorX/OneCreatorX/main/UIs/UIGenerales/GeneralesUI.lua?token=GHSAT0AAAAAACEXSESJSKTM2L45MWNB72G2ZFFOYRQ",
+"Url": "https://raw.githubusercontent.com/OneCreatorX/OneCreatorX/main/UIs/UIGenerales/GeneralesUI.lua",
   "Updated": "04/07/2023"
   },
  "13276475159":{
     "Creator": "OneCreatorX",
     "Status": "Functional and in Development",
-"Url": "https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/CoffeShop.lua",
+"Url": "https://raw.githubusercontent.com/OneCreatorX/OneCreatorX/main/Scripts/Games/Ejecutables/CoffeShop.lua",
   "Updated": "04/07/2023"
   },
  "6305942109":{
     "Creator": "OneCreatorX",
     "Status": "Beta but Working",
-"Url": "https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/Pasteler%C3%ADa/LaundryExe.lua",
+"Url": "https://raw.githubusercontent.com/OneCreatorX/OneCreatorX/main/Scripts/Games/Ejecutables/LaundryExe.lua",
   "Updated": "04/07/2023"
   },
  "10108131074":{
     "Creator": "OneCreatorX",
     "Status": "Functional from 1-8 worlds",
-"Url": "https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/EjecuteAllCespet.lua",
+"Url": "https://raw.githubusercontent.com/OneCreatorX/OneCreatorX/main/Scripts/Games/Ejecutables/CortaCesped.lua",
   "Updated": "04/07/2023"
   },
  "6915291292":{
     "Creator": "OneCreatorX",
     "Status": "Under Maintenance",
-"Url": "https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/Pasteler%C3%ADa/ExePanaderia.lua",
+"Url": "arreglar",
   "Updated": "04/07/2023"
   },
  "370731277":{
     "Creator": "OneCreatorX",
     "Status": "Functional",
-"Url": "https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/LoadyCargadorLinks/Meepcity.lua",
+"Url": "https://raw.githubusercontent.com/OneCreatorX/OneCreatorX/main/Scripts/Games/Ejecutables/Meepcity.lua",
   "Updated": "04/07/2023"
   },
  "12398414727":{
     "Creator": "Cat, OneCreatorX",
     "Status": "Functional",
-"Url": "https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/Math%20answer.lua",
+"Url": "https://raw.githubusercontent.com/OneCreatorX/OneCreatorX/main/Scripts/Games/Ejecutables/MathAnswer.lua",
+  "Updated": "04/07/2023"
+  }
+"10261818242":{
+    "Creator": "OneCreatorX",
+    "Status": "Functional",
+"Url": "https://raw.githubusercontent.com/OneCreatorX/OneCreatorX/main/Scripts/Games/Ejecutables/DonutsTycoon.lua",
+  "Updated": "04/07/2023"
+  }
+"10456811278":{
+    "Creator": "OneCreatorX",
+    "Status": "Functional",
+"Url": "https://raw.githubusercontent.com/OneCreatorX/OneCreatorX/main/Scripts/Games/Ejecutables/TycoonMilk.lua",
   "Updated": "04/07/2023"
   }
 }
