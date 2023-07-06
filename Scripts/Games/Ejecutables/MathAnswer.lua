b782eb26de326f712269e334c77df83564c7f9b7
OneCreatorX <138682257+OneCreatorX@users.noreply.github.com>
2023-07-05 22:57:36 -0300

    Actualizar MathAnswer.lua

--- Scripts/Games/Ejecutables/MathAnswer.lua
+++ Scripts/Games/Ejecutables/MathAnswer.lua
@@ -1,19 +1,13 @@
-local Debounce = false
-
 game:GetService("ReplicatedStorage").Events.Round.RoundRE.OnClientEvent:Connect(function(Status, Time, Data)
+    if Status == "countdown" and Time == game:GetService("ReplicatedStorage").GameData.RoundCountdown.Value then
+        Data.CurrentQuestion = Data.CurrentQuestion:gsub("= %?", "")
 
-        if Status == "countdown" and not Debounce then
-            Debounce = true
-            Data.CurrentQuestion = Data.CurrentQuestion:gsub("= %?", "")
-
-            if Data.CurrentQuestion:match("÷") then
-                Data.CurrentQuestion = Data.CurrentQuestion:gsub("÷", "/")
-            elseif Data.CurrentQuestion:match("×") then
-                Data.CurrentQuestion = Data.CurrentQuestion:gsub("×", "*")
-            end
-
-            game:GetService("ReplicatedStorage").Functions.Answer.AnswerRF:InvokeServer("answer", loadstring("return "..Data.CurrentQuestion)())
-        elseif Status == "idle" then
-            Debounce = false
+        if Data.CurrentQuestion:match("÷") then
+            Data.CurrentQuestion = Data.CurrentQuestion:gsub("÷", "/")
+        elseif Data.CurrentQuestion:match("×") then
+            Data.CurrentQuestion = Data.CurrentQuestion:gsub("×", "*")
         end
-    end)
+
+        game:GetService("ReplicatedStorage").Functions.Answer.AnswerRF:InvokeServer("answer", loadstring("return "..Data.CurrentQuestion)())
+    end
+end)
