fb49dde51403ff27a4c12f7a70d1a1ea3e8f2741
OneCreatorX <138682257+OneCreatorX@users.noreply.github.com>
2023-07-05 22:00:24 -0300

    Actualizar Meepcity.lua

--- Scripts/Games/Ejecutables/Meepcity.lua
+++ Scripts/Games/Ejecutables/Meepcity.lua
@@ -2,17 +2,17 @@
   "Updated":"05/07/2023"
    "Colocar Objetos no area":{
     "Status": "Functional",
-   "Url": "https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/ColocarYenMeep.lua",
+   "Url": "https://raw.githubusercontent.com/OneCreatorX/OneCreatorX/main/Scripts/Games/Scripts/Meepcity/ColocarObjectos.lua",
     "Updated":"05/07/2023"
 },
 "Plantas Options":{
 "Status": "Functional",
-"Url": "https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/plantsMeepC.lua",
+"Url": "https://raw.githubusercontent.com/OneCreatorX/OneCreatorX/main/Scripts/Games/Scripts/Meepcity/plantsMeepC.lua",
     "Updated":"04/07/2023"
     },
 "Fun Pizzería Trolling":{
 "Status": "Functional",
-"Url":"https://raw.githubusercontent.com/AnonyProArg/ScriptsRobloz/main/PizzaMeep.lua",
+"Url":"https://raw.githubusercontent.com/OneCreatorX/OneCreatorX/main/Scripts/Games/Scripts/Meepcity/PizzaMeep.lua",
     "Updated":"07/05/2023"
 }
 }
