bd867a59039e991fe0c43ca81f09a0d0c455677f
OneCreatorX <138682257+OneCreatorX@users.noreply.github.com>
2023-07-10 09:59:34 -0300

    Actualizar Preguntasyrespuestas.lua

--- Scripts/Games/Scripts/Preguntasyrespuestas.lua
+++ Scripts/Games/Scripts/Preguntasyrespuestas.lua
@@ -1,28 +1,28 @@
-Name a popular Superhero:Spider-Man
-Name a popular fruit:Manzana
-Name any natural hair color:Castaño
-Name something you eat with:Cuchara
-Name a red fruit:Fresa
-Name one of the four seasons:Verano
-Name a food that starts with the letter P:Pizza
-Name a famous sport that is played in teams:Fútbol
-What is something you can sit on:Silla
+Name a popular Superhero:Scarlet Witch
+Name a popular fruit:granadilla
+Name any natural hair color:castaño oscuro
+Name something you eat with:Cuchillo
+Name a red fruit:frambuesa
+Name one of the four seasons:Invierno
+Name a food that starts with the letter P:pulpo a la gallega
+Name a famous sport that is played in teams:baloncesto
+What is something you can sit on:Taburete
 Name a type of weather:Lluvia
 Name an animal that walks slowly:Tortuga
 Name any month that has 31 days:Julio
 Name one of the seven colors of the rainbow:Azul
 Name a social media app:Instagram
 Name a popular vegetable:Zanahoria
-Name an animal that can fly:Pájaro
+Name an animal that can fly:Archilochus colubris
 Name a topping that is usually in a hamburger:Queso
 Name a day of the week:Miércoles
 Name something you do in your sleep:Soñar
-Name a country that starts with the letter A:Argentina
+Name a country that starts with the letter A:Afganistán
 Name a planet that is part of our solar system:Marte
 Name any capital city in Europe:Madrid
 Name one of the world's most popular car colors:Negro
 Name something you find on pizza:Pepperoni
 Name something people wear:Sombrero
 Name a musical instrument:Guitarra
-Name a popular electronic device:Teléfono inteligente
-Name a popular game on Roblox:Meepcity 
+Name a popular electronic device:Celulares
+Name a popular game on Roblox:Adopt Me
