42c3c31447e3316bd88ce89c2b6143804f5d4af2
OneCreatorX <138682257+OneCreatorX@users.noreply.github.com>
2023-07-10 11:30:19 -0300

    Actualizar Links.lua

--- UIs/Loady/Comunidad/Links.lua
+++ UIs/Loady/Comunidad/Links.lua
@@ -3,7 +3,7 @@
   "2768379856":{
      "Creator": "SCP.SF",
     "Status": "Verified",
-"Url":"loadstring(game:HttpGet("https://raw.githubusercontent.com/ScriptV2PE/SCP.SF/main/.github/workflows/Scp3008WZ"))(),
+"Url":"https://raw.githubusercontent.com/ScriptV2PE/SCP.SF/main/.github/workflows/Scp3008WZ",
   "Updated": "10/07/2023"
   }
 }
