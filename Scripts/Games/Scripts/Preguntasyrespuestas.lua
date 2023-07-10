80fcd529c46d00dd2b80c42f74d4635c8f19fee8
OneCreatorX <138682257+OneCreatorX@users.noreply.github.com>
2023-07-10 07:10:47 -0300

    Actualizar Preguntasyrespuestas.lua

--- Scripts/Games/Scripts/Preguntasyrespuestas.lua
+++ Scripts/Games/Scripts/Preguntasyrespuestas.lua
@@ -1,188 +1,27 @@
-Name a popular Superhero:
-Respuesta 1: Spider-Man
-Respuesta 2: Superman
-Respuesta 3: Batman
-Respuesta 4: Iron Man
-Respuesta 5: Captain America
-
-Name a popular fruit:
-Respuesta 1: Apple
-Respuesta 2: Banana
-Respuesta 3: Orange
-Respuesta 4: Strawberry
-Respuesta 5: Watermelon
-
-Name any natural hair color:
-Respuesta 1: Brown
-Respuesta 2: Blonde
-Respuesta 3: Black
-Respuesta 4: Red
-Respuesta 5: Gray
-
-Name something you eat with:
-Respuesta 1: Spoon
-Respuesta 2: Fork
-Respuesta 3: Chopsticks
-Respuesta 4: Knife
-Respuesta 5: Hands
-
-Name a red fruit:
-Respuesta 1: Strawberry
-Respuesta 2: Cherry
-Respuesta 3: Raspberry
-Respuesta 4: Red apple
-Respuesta 5: Cranberry
-
-Name one of the four seasons:
-Respuesta 1: Summer
-Respuesta 2: Winter
-Respuesta 3: Spring
-Respuesta 4: Autumn
-Respuesta 5: Fall
-
-Name a food that starts with the letter P:
-Respuesta 1: Pizza
-Respuesta 2: Pasta
-Respuesta 3: Pineapple
-Respuesta 4: Popcorn
-Respuesta 5: Pancakes
-
-Name a famous sport that is played in teams:
-Respuesta 1: Soccer
-Respuesta 2: Basketball
-Respuesta 3: Football
-Respuesta 4: Baseball
-Respuesta 5: Volleyball
-
-What is something you can sit on:
-Respuesta 1: Chair
-Respuesta 2: Sofa
-Respuesta 3: Bench
-Respuesta 4: Stool
-Respuesta 5: Throne
-
-Name a type of weather:
-Respuesta 1: Rain
-Respuesta 2: Sunny
-Respuesta 3: Cloudy
-Respuesta 4: Stormy
-Respuesta 5: Windy
-
-Name an animal that walks slowly:
-Respuesta 1: Turtle
-Respuesta 2: Snail
-Respuesta 3: Sloth
-Respuesta 4: Caterpillar
-Respuesta 5: Tortoise
-
-Name any month that has 31 days:
-Respuesta 1: July
-Respuesta 2: January
-Respuesta 3: March
-Respuesta 4: December
-Respuesta 5: October
-
-Name one of the seven colors of the rainbow:
-Respuesta 1: Blue
-Respuesta 2: Red
-Respuesta 3: Green
-Respuesta 4: Yellow
-Respuesta 5: Orange
-
-Name a social media app:
-Respuesta 1: Instagram
-Respuesta 2: Facebook
-Respuesta 3: Twitter
-Respuesta 4: Snapchat
-Respuesta 5: TikTok
-
-Name a popular vegetable:
-Respuesta 1: Carrot
-Respuesta 2: Broccoli
-Respuesta 3: Tomato
-Respuesta 4: Spinach
-Respuesta 5: Cabbage
-
-Name an animal that can fly:
-Respuesta 1: Bird
-Respuesta 2: Butterfly
-Respuesta 3: Eagle
-Respuesta 4: Bat
-Respuesta 5: Dragonfly
-
-Name a topping that is usually in a hamburger:
-Respuesta 1: Cheese
-Respuesta 2: Lettuce
-Respuesta 3: Tomato
-Respuesta 4: Pickles
-Respuesta 5: Bacon
-
-Name a day of the week:
-Respuesta 1: Wednesday
-Respuesta 2: Sunday
-Respuesta 3: Friday
-Respuesta 4: Monday
-Respuesta 5: Thursday
-
-Name something you do in your sleep:
-Respuesta 1: Dream
-Respuesta 2: Snore
-Respuesta 3: Sleepwalk
-Respuesta 4: Talk
-Respuesta 5: Toss and turn
-
-Name a country that starts with the letter A:
-Respuesta 1: Argentina
-Respuesta 2: Australia
-Respuesta 3: Austria
-Respuesta 4: Algeria
-Respuesta 5: Afghanistan
-
-Name a planet that is part of our solar system:
-Respuesta 1: Mars
-Respuesta 2: Venus
-Respuesta 3: Jupiter
-Respuesta 4: Saturn
-Respuesta 5: Earth
-
-Name any capital city in Europe:
-Respuesta 1: London
-Respuesta 2: Paris
-Respuesta 3: Rome
-Respuesta 4: Berlin
-Respuesta 5: Madrid
-
-Name one of the world's most popular car colors:
-Respuesta 1: Black
-Respuesta 2: White
-Respuesta 3: Silver
-Respuesta 4: Blue
-Respuesta 5: Red
-
-Name something you find on pizza:
-Respuesta 1: Cheese
-Respuesta 2: Pepperoni
-Respuesta 3: Mushrooms
-Respuesta 4: Olives
-Respuesta 5: Pineapple
-
-Name something people wear:
-Respuesta 1: Shirt
-Respuesta 2: Pants
-Respuesta 3: Shoes
-Respuesta 4: Hat
-Respuesta 5: Jacket
-
-Name a musical instrument:
-Respuesta 1: Piano
-Respuesta 2: Guitar
-Respuesta 3: Violin
-Respuesta 4: Drums
-Respuesta 5: Flute
-
-Name a popular electronic device:
-Respuesta 1: Smartphone
-Respuesta 2: Laptop
-Respuesta 3: Tablet
-Respuesta 4: Smartwatch
-Respuesta 5: Game console
+Name a popular Superhero:Spider-Man
+Name a popular fruit:Manzana
+Name any natural hair color:Castaño
+Name something you eat with:Cuchara
+Name a red fruit:Fresa
+Name one of the four seasons:Verano
+Name a food that starts with the letter P:Pizza
+Name a famous sport that is played in teams:Fútbol
+What is something you can sit on:Silla
+Name a type of weather:Lluvia
+Name an animal that walks slowly:Tortuga
+Name any month that has 31 days:Julio
+Name one of the seven colors of the rainbow:Azul
+Name a social media app:Instagram
+Name a popular vegetable:Zanahoria
+Name an animal that can fly:Pájaro
+Name a topping that is usually in a hamburger:Queso
+Name a day of the week:Miércoles
+Name something you do in your sleep:Soñar
+Name a country that starts with the letter A:Argentina
+Name a planet that is part of our solar system:Marte
+Name any capital city in Europe:Madrid
+Name one of the world's most popular car colors:Negro
+Name something you find on pizza:Pepperoni
+Name something people wear:Sombrero
+Name a musical instrument:Guitarra
+Name a popular electronic device:Teléfono inteligente
