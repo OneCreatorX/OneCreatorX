0bf96afce76c21a8e3f66d635ee5202b3b3656f1
OneCreatorX <138682257+OneCreatorX@users.noreply.github.com>
2023-07-11 03:54:47 -0300

    Actualizar LaundryExe.lua

--- Scripts/Games/Ejecutables/LaundryExe.lua
+++ Scripts/Games/Ejecutables/LaundryExe.lua
@@ -16,26 +16,19 @@ end
 local gui = Instance.new("ScreenGui")
 gui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
 
+-- Crear el marco principal
 local mainFrame = Instance.new("Frame")
-mainFrame.Size = UDim2.new(0, 200, -0.2, 240)
+mainFrame.Size = UDim2.new(0, 200, -0.5, 240)
 mainFrame.Position = UDim2.new(0.5, -100, 0, 0)
 mainFrame.BackgroundTransparency = 0.8 -- Valor entre 0 (opaco) y 1 (transparente)
 mainFrame.Active = true
 mainFrame.Draggable = true
 mainFrame.Parent = gui
 
--- Agregar una imagen de fondo al marco principal
-local backgroundImage = Instance.new("ImageLabel")
-backgroundImage.Name = "BackgroundImage"
-backgroundImage.Size = UDim2.new(1, 0, 0.30, -1)
-backgroundImage.BackgroundTransparency = 1
-backgroundImage.Image = "rbxassetid://13959537980" -- Reemplaza el ID de asset con la imagen deseada
-backgroundImage.Parent = mainFrame
-
 -- Crear el título
 local titleText = Instance.new("TextLabel")
 titleText.Size = UDim2.new(1, 0, 0, 40)
-titleText.Position = UDim2.new(0, 0, -0.4, 20)
+titleText.Position = UDim2.new(0, 0, -0.4, 40)
 titleText.BackgroundTransparency = 0.4
 titleText.BackgroundColor3 = Color3.fromRGB(30, 30, 30)
 titleText.BorderSizePixel = 1
@@ -69,24 +62,43 @@ specialClothingButton.TextTransparency = 0.2
 specialClothingButton.ClipsDescendants = true
 specialClothingButton.Parent = mainFrame
 
--- Crear el botón de Auto Game
-local autofarmButton = Instance.new("TextButton")
-autofarmButton.Size = UDim2.new(0, 180, 0, 40)
-autofarmButton.Position = UDim2.new(0.5, 0, 0, 97)
-autofarmButton.AnchorPoint = Vector2.new(0.5, 0)
-autofarmButton.BackgroundTransparency = 0.5
-autofarmButton.BackgroundColor3 = Color3.fromRGB(30, 30, 30)
-autofarmButton.BorderSizePixel = 1
-autofarmButton.Text = "Auto Game"
-autofarmButton.TextColor3 = Color3.new(1, 0, 0)
-autofarmButton.Font = Enum.Font.GothamSemibold
-autofarmButton.TextSize = 15
-autofarmButton.TextScaled = true
-autofarmButton.TextStrokeColor3 = Color3.fromRGB(0, 0, 0)
-autofarmButton.TextStrokeTransparency = 0.5
-autofarmButton.TextTransparency = 0.2
-autofarmButton.ClipsDescendants = true
-autofarmButton.Parent = mainFrame
+-- Crear el botón para todas las prendas
+local allClothingButton = Instance.new("TextButton")
+allClothingButton.Size = UDim2.new(0, 180, 0, 40)
+allClothingButton.Position = UDim2.new(0.5, 0, 0, 97)
+allClothingButton.AnchorPoint = Vector2.new(0.5, 0)
+allClothingButton.BackgroundTransparency = 0.5
+allClothingButton.BackgroundColor3 = Color3.fromRGB(30, 30, 30)
+allClothingButton.BorderSizePixel = 1
+allClothingButton.Text = "Todas las Prendas"
+allClothingButton.TextColor3 = Color3.new(1, 0, 0)
+allClothingButton.Font = Enum.Font.GothamSemibold
+allClothingButton.TextSize = 15
+allClothingButton.TextScaled = true
+allClothingButton.TextStrokeColor3 = Color3.fromRGB(0, 0, 0)
+allClothingButton.TextStrokeTransparency = 0.5
+allClothingButton.TextTransparency = 0.2
+allClothingButton.ClipsDescendants = true
+allClothingButton.Parent = mainFrame
+
+-- Crear el botón para AutoFarm
+local autoFarmButton = Instance.new("TextButton")
+autoFarmButton.Size = UDim2.new(0, 180, 0, 40)
+autoFarmButton.Position = UDim2.new(0.5, 0, 0, 142)
+autoFarmButton.AnchorPoint = Vector2.new(0.5, 0)
+autoFarmButton.BackgroundTransparency = 0.5
+autoFarmButton.BackgroundColor3 = Color3.fromRGB(30, 30, 30)
+autoFarmButton.BorderSizePixel = 1
+autoFarmButton.Text = "AutoFarm"
+autoFarmButton.TextColor3 = Color3.new(1, 0, 0)
+autoFarmButton.Font = Enum.Font.GothamSemibold
+autoFarmButton.TextSize = 15
+autoFarmButton.TextScaled = true
+autoFarmButton.TextStrokeColor3 = Color3.fromRGB(0, 0, 0)
+autoFarmButton.TextStrokeTransparency = 0.5
+autoFarmButton.TextTransparency = 0.2
+autoFarmButton.ClipsDescendants = true
+autoFarmButton.Parent = mainFrame
 
 local titleTexts = {"Laundry X", "YT:@OneCreatorX"}
 local titleColors = {
@@ -111,19 +123,76 @@ spawn(function()
     end
 end)
 
-local isAutoFarmEnabled = false
 local isSpecialClothingButtonHighlighted = false
+local isAllClothingButtonHighlighted = false
 local isAutoFarmButtonHighlighted = false
+local invokeClothingProcessCoroutine
+local manageWashingMachineCoroutine
+local dropClothesInChuteCoroutine
 
-local function ToggleAutoFarm()
-    isAutoFarmEnabled = not isAutoFarmEnabled
+local function ToggleSpecialClothing()
+    isSpecialClothingButtonHighlighted = not isSpecialClothingButtonHighlighted
 
-    if not isAutoFarmEnabled then
-        -- Detener el modo de autofarm
-        return
+    if isSpecialClothingButtonHighlighted then
+        specialClothingButton.TextColor3 = Color3.new(0, 1, 0) -- Cambiar a verde
+    else
+        specialClothingButton.TextColor3 = Color3.new(1, 0, 0) -- Cambiar a rojo
     end
 
-    while isAutoFarmEnabled do
+    if not isAllClothingButtonHighlighted then
+        invokeClothingProcessCoroutine = coroutine.create(invokeClothingProcess)
+        coroutine.resume(invokeClothingProcessCoroutine)
+    end
+end
+
+local function ToggleAllClothing()
+    isAllClothingButtonHighlighted = not isAllClothingButtonHighlighted
+
+    if isAllClothingButtonHighlighted then
+        allClothingButton.TextColor3 = Color3.new(0, 1, 0) -- Cambiar a verde
+    else
+        allClothingButton.TextColor3 = Color3.new(1, 0, 0) -- Cambiar a rojo
+    end
+
+    if not isSpecialClothingButtonHighlighted then
+        invokeClothingProcessCoroutine = coroutine.create(invokeClothingProcess)
+        coroutine.resume(invokeClothingProcessCoroutine)
+    end
+end
+
+local function ToggleAutoFarm()
+    isAutoFarmButtonHighlighted = not isAutoFarmButtonHighlighted
+
+    if isAutoFarmButtonHighlighted then
+        autoFarmButton.TextColor3 = Color3.new(0, 1, 0) -- Cambiar a verde
+        manageWashingMachineCoroutine = coroutine.create(manageWashingMachine)
+        dropClothesInChuteCoroutine = coroutine.create(dropClothesInChute)
+        coroutine.resume(manageWashingMachineCoroutine, Plot)
+        coroutine.resume(dropClothesInChuteCoroutine)
+    else
+        autoFarmButton.TextColor3 = Color3.new(1, 0, 0) -- Cambiar a rojo
+        if manageWashingMachineCoroutine then
+            coroutine.kill(manageWashingMachineCoroutine)
+            manageWashingMachineCoroutine = nil
+        end
+        if dropClothesInChuteCoroutine then
+            coroutine.kill(dropClothesInChuteCoroutine)
+            dropClothesInChuteCoroutine = nil
+        end
+    end
+end
+
+-- Evento MouseButton1Click del botón de Ropas Especiales
+specialClothingButton.MouseButton1Click:Connect(ToggleSpecialClothing)
+
+-- Evento MouseButton1Click del botón de Todas las Prendas
+allClothingButton.MouseButton1Click:Connect(ToggleAllClothing)
+
+-- Evento MouseButton1Click del botón de AutoFarm
+autoFarmButton.MouseButton1Click:Connect(ToggleAutoFarm)
+
+local function invokeSpecialClothing()
+    if isSpecialClothingButtonHighlighted then
         local clothingList = UpdateClothingDirectory()
         local specialClothingList = {}
 
@@ -140,40 +209,73 @@ local function ToggleAutoFarm()
             InvokeClothing(v)
             wait(0.2)
         end
-
-        wait(1)  -- Esperar un segundo antes de realizar la próxima comprobación
     end
 end
 
-local function ChangeSpecialClothingButtonColor()
-    isSpecialClothingButtonHighlighted = not isSpecialClothingButtonHighlighted
+local function invokeAllClothing()
+    if isAllClothingButtonHighlighted then
+        local clothingList = UpdateClothingDirectory()
+        local playerPosition = game.Players.LocalPlayer.Character.HumanoidRootPart.Position
 
-    if isSpecialClothingButtonHighlighted then
-        specialClothingButton.TextColor3 = Color3.new(0, 1, 0) -- Cambiar a verde
+        table.sort(clothingList, function(a, b)
+            local distanceA = (a.Position - playerPosition).Magnitude
+            local distanceB = (b.Position - playerPosition).Magnitude
+            return distanceA < distanceB
+        end)
+
+        for _, clothing in ipairs(clothingList) do
+            InvokeClothing(clothing)
+            wait(0.01)
+        end
+    end
+end
+
+local function invokeClothingProcess()
+    while true do
+        invokeSpecialClothing()
+        invokeAllClothing()
+        wait(1.6)
+    end
+end
+
+local function manageWashingMachine(washingMachine)
+    while true do
+        -- Iniciar lavadora
+        game:GetService("ReplicatedStorage").Events.LoadWashingMachine:FireServer(washingMachine)
+
+        -- Detener lavadora
+        game:GetService("ReplicatedStorage").Events.UnloadWashingMachine:FireServer(washingMachine)
+        wait(0.8)
+    end
+end
+
+local function dropClothesInChute()
+    while true do
+        game:GetService("ReplicatedStorage").Events.DropClothesInChute:FireServer()
+        wait(1)
+    end
+end
+
+local function startSpam()
+    local plot = Plot
+    if plot then
+        local washingMachines = plot.WashingMachines:GetChildren()
+
+        for _, washingMachine in ipairs(washingMachines) do
+            spawn(function()
+                manageWashingMachine(washingMachine)
+            end)
+        end
+
+        invokeClothingProcessCoroutine = coroutine.create(invokeClothingProcess)
+        coroutine.resume(invokeClothingProcessCoroutine)
+        spawn(dropClothesInChute)
     else
-        specialClothingButton.TextColor3 = Color3.new(1, 0, 0) -- Cambiar a rojo
+        print("No se encontró el Plot del jugador.")
     end
 end
 
-local function ChangeAutoFarmButtonColor()
-    isAutoFarmButtonHighlighted = true
-    autofarmButton.TextColor3 = Color3.new(0, 1, 0) -- Cambiar a verde
-end
-
--- Evento MouseButton1Click del botón de Ropas Especiales
-specialClothingButton.MouseButton1Click:Connect(ChangeSpecialClothingButtonColor)
-specialClothingButton.MouseButton1Click:Connect(ToggleAutoFarm)
-
--- Evento MouseButton1Click del botón de Auto Game
-autofarmButton.MouseButton1Click:Connect(ChangeAutoFarmButtonColor)
-autofarmButton.MouseButton1Click:Connect(ToggleAutoFarm)
-
-local function AutoFarm()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/OneCreatorX/OneCreatorX/main/Scripts/Games/Scripts/Simulator/Laundry/AutoFarm.lua"))()
-end
-
--- Evento MouseButton1Click del botón de Auto Game
-autofarmButton.MouseButton1Click:Connect(AutoFarm)
+autoFarmButton.MouseButton1Click:Connect(startSpam)
 
 local Gamepasses = game.Players.LocalPlayer.Gamepasses
 
