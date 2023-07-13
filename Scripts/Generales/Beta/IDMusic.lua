0dbdc276df17dd1d92f9a1d81b57feca22e6a3ab
OneCreatorX <138682257+OneCreatorX@users.noreply.github.com>
2023-07-13 04:49:20 -0300

    Create IDMusic.lua

---
+++ Scripts/Generales/Beta/IDMusic.lua
@@ -0,0 +1,33 @@
+186317099:2Pac – Life Goes On
+1326909345:XXXTentacion – Jocelyn Flores
+1321038120:Billie Eilish – Ocean Eyes
+1003325030:XXXTentacion – Carry On
+5145539495:Tina Turner – What’s Love Got to Do with It
+7081437616:Lil Nas X ft Jack Harlow – Industry Baby
+6177409271:Kim Dracula – Paparazzi
+6657083880:Doja Cat ft. SZA – Kiss Me More
+521116871:Doja Cat – Say So
+5925841720:2Pac ft. Dr. Dre – California Love
+6447077697:PinkPantheress – Pain
+6620108916:Lil Nas X – Call Me By Your Name
+6678031214:Polo G – RAPSTAR
+292861322:Snoop Dogg – Drop It Like It’s Hot
+6957372976:Bad Bunny – Yonaguni
+6432181830:Glass Animals – Heat Wave
+6887728970:Rauw Alejandro – Todo De Ti
+6794553622:Syko Brooklyn Blood Pop
+1259050178:A Roblox Rap / Merry Christmas Roblox
+5253604010:Capone – Oh No
+5595658625:Royal & the Serpent – Overwhelmed
+5760198930:Clairo – Sofia
+1845016505:Believer
+5937000690:Chikatto – Chika Chika
+224845627:The Kitty Cat Dance
+154664102:You’ve Been Trolled
+249672730:Illijah – On My Way
+143666548:Mii Channel Music
+189105508:Frozen – Let It Go
+6463211475:Tesher – Jalebi Baby
+1725273277:Frank Ocean – Chanel
+3400778682:Pokemon Sword and Shield Gym theme
+1243143051:Jingle Oof (Jingle Bells parody)
