e0a1a979070acdfd7293b8fcdb4e5f96cebab5de
OneCreatorX <138682257+OneCreatorX@users.noreply.github.com>
2023-07-18 02:31:02 -0300

    Actualizar Bakery.lua

--- Scripts/Games/Ejecutables/Bakery.lua
+++ Scripts/Games/Ejecutables/Bakery.lua
@@ -1,12 +1,5 @@
 local replicatedStorage = game:GetService("ReplicatedStorage")
 
-local buttonState = {
-    invokeButton = false,
-    ovensButton = false,
-    finalButton = false,
-    startButton = false
-}
-
 local function invokeIngredient(ingredient)
     local remote = replicatedStorage.Remotes.TI_0
     remote:FireServer(ingredient.Ingredient_Collider)
@@ -17,17 +10,17 @@ screenGui.Name = "ButtonContainer"
 screenGui.Parent = game.Players.LocalPlayer.PlayerGui
 
 local frame = Instance.new("Frame")
-frame.Size = UDim2.new(0, 250, 0, 230)
-frame.Position = UDim2.new(0.5, -frame.Size.X.Offset / 2, 0.5, -frame.Size.Y.Offset / 2)
-frame.BackgroundTransparency = 0.5
-frame.BackgroundColor3 = Color3.fromRGB(50, 50, 50)
+frame.Size = UDim2.new(0, 220, 0, 200) -- Ajustamos el tamaño del marco
+frame.Position = UDim2.new(0.5, -frame.Size.X.Offset / 2, 0.7, -frame.Size.Y.Offset / 2) -- Ajustamos la posición para centrarla verticalmente
+frame.BackgroundTransparency = 0.8 -- Hacemos el fondo más transparente para un aspecto más minimalista
+frame.BackgroundColor3 = Color3.fromRGB(40, 40, 40) -- Cambiamos el color de fondo
 frame.BorderSizePixel = 0
-frame.Active = true
-frame.Draggable = true
+frame.Active = true -- Activamos el marco para permitir el arrastre
+frame.Draggable = true -- Permitimos el arrastre del marco
 frame.Parent = screenGui
 
 local titleContainer = Instance.new("Frame")
-titleContainer.Size = UDim2.new(1, 0, 0, 40)
+titleContainer.Size = UDim2.new(1, 0, 0, 30) -- Reducimos la altura del contenedor del título
 titleContainer.BackgroundColor3 = Color3.fromRGB(30, 30, 30)
 titleContainer.BorderSizePixel = 0
 titleContainer.Parent = frame
@@ -38,57 +31,22 @@ title.Position = UDim2.new(0, 5, 0, 5)
 title.BackgroundTransparency = 1
 title.Text = "Panadería | YT:@OneCreatorX"
 title.TextColor3 = Color3.new(1, 1, 1)
-title.TextSize = 20
+title.TextSize = 14 -- Reducimos el tamaño del texto del título
 title.Font = Enum.Font.GothamBold
 title.TextWrapped = true
+title.TextXAlignment = Enum.TextXAlignment.Left -- Alineamos el texto del título a la izquierda
 title.Parent = titleContainer
 
-local buttonSize = UDim2.new(0.8, 0, 0, 40)
-local buttonSpacing = 10
+local buttonSize = UDim2.new(0.8, 0, 0, 30) -- Reducimos el tamaño de los botones
+local buttonSpacing = 8 -- Reducimos el espacio entre los botones
 
-local invokeButton = Instance.new("TextButton")
-invokeButton.Size = buttonSize
-invokeButton.Position = UDim2.new(0.1, 0, 0, 50)
-invokeButton.Text = "Invocar ingredientes"
-invokeButton.Font = Enum.Font.Gotham
-invokeButton.TextColor3 = Color3.new(1, 1, 1)
-invokeButton.TextSize = 16
-invokeButton.BackgroundColor3 = Color3.fromRGB(128, 128, 128)
-invokeButton.BorderSizePixel = 0
-invokeButton.Parent = frame
-
-local ovensButton = Instance.new("TextButton")
-ovensButton.Size = buttonSize
-ovensButton.Position = UDim2.new(0.1, 0, 0, 50 + buttonSize.Y.Offset + buttonSpacing)
-ovensButton.Text = "Ingre Hornos"
-ovensButton.Font = Enum.Font.Gotham
-ovensButton.TextColor3 = Color3.new(1, 1, 1)
-ovensButton.TextSize = 16
-ovensButton.BackgroundColor3 = Color3.fromRGB(128, 128, 128)
-ovensButton.BorderSizePixel = 0
-ovensButton.Parent = frame
-
-local finalButton = Instance.new("TextButton")
-finalButton.Size = buttonSize
-finalButton.Position = UDim2.new(0.1, 0, 0, 50 + (buttonSize.Y.Offset + buttonSpacing) * 2)
-finalButton.Text = "Proceso final"
-finalButton.Font = Enum.Font.Gotham
-finalButton.TextColor3 = Color3.new(1, 1, 1)
-finalButton.TextSize = 16
-finalButton.BackgroundColor3 = Color3.fromRGB(128, 128, 128)
-finalButton.BorderSizePixel = 0
-finalButton.Parent = frame
-
-local startButton = Instance.new("TextButton")
-startButton.Size = buttonSize
-startButton.Position = UDim2.new(0.1, 0, 0, 50 + (buttonSize.Y.Offset + buttonSpacing) * 3)
-startButton.Text = "Iniciar cocción"
-startButton.Font = Enum.Font.Gotham
-startButton.TextColor3 = Color3.new(1, 1, 1)
-startButton.TextSize = 16
-startButton.BackgroundColor3 = Color3.fromRGB(128, 128, 128)
-startButton.BorderSizePixel = 0
-startButton.Parent = frame
+local buttonStyle = {
+    BackgroundColor3 = Color3.fromRGB(80, 80, 80),
+    BorderColor3 = Color3.fromRGB(60, 60, 60),
+    TextColor3 = Color3.new(1, 1, 1),
+    Font = Enum.Font.Gotham,
+    TextSize = 12, -- Reducimos el tamaño del texto de los botones
+}
 
 local buttonState = {
     invokeButton = false,
@@ -97,16 +55,83 @@ local buttonState = {
     startButton = false,
 }
 
+local function applyButtonStyle(button)
+    button.BackgroundColor3 = buttonStyle.BackgroundColor3
+    button.BorderColor3 = buttonStyle.BorderColor3
+    button.TextColor3 = buttonStyle.TextColor3
+    button.Font = buttonStyle.Font
+    button.TextSize = buttonStyle.TextSize
+end
+
 local function toggleButtonState(buttonName, button)
     buttonState[buttonName] = not buttonState[buttonName]
     if buttonState[buttonName] then
         button.BackgroundColor3 = Color3.fromRGB(255, 0, 0)
         button.TextColor3 = Color3.fromRGB(255, 255, 255)
     else
-        button.BackgroundColor3 = Color3.fromRGB(128, 128, 128)
-        button.TextColor3 = Color3.fromRGB(200, 200, 200)
+        button.BackgroundColor3 = buttonStyle.BackgroundColor3
+        button.TextColor3 = buttonStyle.TextColor3
     end
 end
+
+local invokeButton = Instance.new("TextButton")
+invokeButton.Size = buttonSize
+invokeButton.Position = UDim2.new(0.1, 0, 0, 40) -- Ajustamos la posición de los botones
+invokeButton.Text = "I.Ingrendientes"
+invokeButton.Parent = frame
+applyButtonStyle(invokeButton)
+
+local ovensButton = Instance.new("TextButton")
+ovensButton.Size = buttonSize
+ovensButton.Position = UDim2.new(0.1, 0, 0, 40 + buttonSize.Y.Offset + buttonSpacing)
+ovensButton.Text = "D.Ingredientes"
+ovensButton.Parent = frame
+applyButtonStyle(ovensButton)
+
+local finalButton = Instance.new("TextButton")
+finalButton.Size = buttonSize
+finalButton.Position = UDim2.new(0.1, 0, 0, 40 + (buttonSize.Y.Offset + buttonSpacing) * 2)
+finalButton.Text = "Finish"
+finalButton.Parent = frame
+applyButtonStyle(finalButton)
+
+local startButton = Instance.new("TextButton")
+startButton.Size = buttonSize
+startButton.Position = UDim2.new(0.1, 0, 0, 40 + (buttonSize.Y.Offset + buttonSpacing) * 3)
+startButton.Text = "Hornear"
+startButton.Parent = frame
+applyButtonStyle(startButton)
+
+local function adjustTitleSize()
+    local textBounds = title.TextBounds
+    local containerWidth = titleContainer.AbsoluteSize.X
+    if textBounds > containerWidth then
+        local scale = containerWidth / textBounds
+        title.TextScaled = true
+        title.TextSize = title.TextSize * scale
+    else
+        title.TextScaled = false
+    end
+end
+
+title:GetPropertyChangedSignal("TextBounds"):Connect(adjustTitleSize)
+
+local dragStartPos
+local frameStartPos
+
+frame.InputBegan:Connect(function(input)
+    if input.UserInputType == Enum.UserInputType.MouseButton1 then
+        dragStartPos = input.Position
+        frameStartPos = frame.Position
+    end
+end)
+
+frame.InputChanged:Connect(function(input)
+    if input.UserInputType == Enum.UserInputType.MouseMovement then
+        local dragDelta = input.Position - dragStartPos
+        frame.Position = UDim2.new(frameStartPos.X.Scale, frameStartPos.X.Offset + dragDelta.X, frameStartPos.Y.Scale, frameStartPos.Y.Offset + dragDelta.Y)
+    end
+end)
 
 invokeButton.MouseButton1Click:Connect(function()
     toggleButtonState("invokeButton", invokeButton)
