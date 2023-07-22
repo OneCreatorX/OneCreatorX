1ba688435267b679cad57970f75f12cf84b9bb84
OneCreatorX <138682257+OneCreatorX@users.noreply.github.com>
2023-07-22 01:28:26 -0300

    Actualizar Esp.lua

--- Scripts/Generales/Final/Esp.lua
+++ Scripts/Generales/Final/Esp.lua
@@ -16,7 +16,6 @@ local espFolder = Instance.new("Folder")
 espFolder.Name = "ESPFolder"
 espFolder.Parent = game.Workspace
 
--- Tabla para mapear los nombres originales a los nombres personalizados
 local nameMap = {
     ["NmmsRblx"] = {Name = "Esposo de Ann", ColorIndex = 1}, -- Rojo
     ["AnnLuv01"] = {Name = "Esposa de Matías", ColorIndex = 2}, -- Azul
@@ -24,7 +23,6 @@ local nameMap = {
     ["DJFARERTER"] = {Name = "Esposo de Vicky", ColorIndex = 4}, -- Rojo fuerte
 }
 
--- Lista de colores para los nombres personalizados
 local nameColors = {
     Color3.new(1, 0, 0), -- Rojo
     Color3.new(0, 0, 1), -- Azul
@@ -32,7 +30,6 @@ local nameColors = {
     Color3.new(1, 0.2, 0), -- Rojo fuerte
 }
 
--- Función para seleccionar un color de la lista en orden
 local function getNextColor(index)
     return nameColors[(index - 1) % #nameColors + 1]
 end
@@ -41,66 +38,59 @@ local function createNameTag(playerPart, playerName, nameColor)
     local billboardGui = Instance.new("BillboardGui")
     billboardGui.Adornee = playerPart
     billboardGui.Size = UDim2.new(0, 100, 0, 20)
-    billboardGui.StudsOffset = Vector3.new(0, 2, 0) -- Ajustar la posición del nombre sobre el jugador
-    billboardGui.AlwaysOnTop = true -- Hacer que el nombre siempre esté visible, incluso a través de las paredes
+    billboardGui.StudsOffset = Vector3.new(0, 2, 0)
+    billboardGui.AlwaysOnTop = true
 
     local nameLabel = Instance.new("TextLabel")
     nameLabel.Size = UDim2.new(1, 0, 1, 0)
     nameLabel.BackgroundTransparency = 1
     nameLabel.TextColor3 = nameColor
-    nameLabel.Text = playerName -- Usar el nombre personalizado
+    nameLabel.Text = playerName
     nameLabel.Parent = billboardGui
 
     billboardGui.Parent = playerPart
 end
 
 local function updateESP()
+    espFolder:ClearAllChildren()
+
     if espEnabled then
         local localCharacter = game.Players.LocalPlayer.Character
         local localPosition = localCharacter and localCharacter:FindFirstChild("HumanoidRootPart") and localCharacter.HumanoidRootPart.Position
 
-        espFolder:ClearAllChildren()
+        if localPosition then
+            for _, player in ipairs(game.Players:GetPlayers()) do
+                if player ~= game.Players.LocalPlayer and player.Character then
+                    local character = player.Character
+                    local targetPosition = character:FindFirstChild("HumanoidRootPart") and character.HumanoidRootPart.Position
 
-        for i, v in pairs(game.Players:GetPlayers()) do
-            local character = v.Character
-            if v ~= game.Players.LocalPlayer and character and character:FindFirstChild("HumanoidRootPart") and localPosition then
-                local targetPosition = character.HumanoidRootPart.Position
-                local distance = (targetPosition - localPosition).Magnitude
+                    if targetPosition then
+                        local distance = (targetPosition - localPosition).Magnitude
+                        local minBoxSize = 1
+                        local maxBoxSize = 5
+                        local normalizedDistance = math.clamp((30 - distance) / 30, 0, 1)
 
-                local minBoxSize = 1 -- Tamaño mínimo del cuadro
-                local maxBoxSize = 5 -- Tamaño máximo del cuadro
-                local normalizedDistance = math.clamp((30 - distance) / 30, 0, 1) -- Normalizar la distancia
+                        local boxSize = minBoxSize + (maxBoxSize - minBoxSize) * normalizedDistance
+                        local box = Instance.new("BoxHandleAdornment")
+                        box.Name = "ESPBox"
+                        box.Adornee = character.HumanoidRootPart
+                        box.Size = Vector3.new(boxSize, boxSize, boxSize)
+                        box.Color3 = Color3.new(0, 1 - normalizedDistance, normalizedDistance)
+                        box.Transparency = 0.5
+                        box.ZIndex = 5
+                        box.AlwaysOnTop = true
+                        box.Parent = espFolder
 
-                local boxSize = minBoxSize + (maxBoxSize - minBoxSize) * normalizedDistance
-                local box = Instance.new("BoxHandleAdornment")
-                box.Name = "ESPBox"
-                box.Adornee = character.HumanoidRootPart
-                box.Size = Vector3.new(boxSize, boxSize, boxSize)
-                box.Color3 = Color3.new(0, 1 - normalizedDistance, normalizedDistance) -- Cambiar el color del cuadro basado en la distancia
-                box.Transparency = 0.5
-                box.ZIndex = 5
-                box.AlwaysOnTop = true
-                box.Parent = espFolder
-
-                local playerPart = Instance.new("Part")
-                playerPart.Name = "PlayerPart"
-                playerPart.Anchored = true
-                playerPart.Size = Vector3.new(0.2, 0.2, 0.2)
-                playerPart.Position = character.HumanoidRootPart.Position
-                playerPart.Transparency = 1
-                playerPart.CanCollide = false
-                playerPart.Parent = espFolder
-
-                local nameData = nameMap[v.Name]
-                if nameData then
-                    createNameTag(playerPart, nameData.Name, getNextColor(nameData.ColorIndex))
-                else
-                    createNameTag(playerPart, v.Name, Color3.new(1, 1, 1)) -- Color blanco para nombres no personalizados
+                        local nameData = nameMap[player.Name]
+                        if nameData then
+                            createNameTag(character.HumanoidRootPart, nameData.Name, getNextColor(nameData.ColorIndex))
+                        else
+                            createNameTag(character.HumanoidRootPart, player.Name, Color3.new(1, 1, 1))
+                        end
+                    end
                 end
             end
         end
-    else
-        espFolder:ClearAllChildren()
     end
 end
 
@@ -110,5 +100,4 @@ button.MouseButton1Click:Connect(function()
     updateESP()
 end)
 
--- Asegurarse de que el ESP esté actualizado al principio
 updateESP()
