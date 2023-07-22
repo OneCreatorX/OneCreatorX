def54497c3d6421f6d777bdd3af43ffd7ec6ac97
OneCreatorX <138682257+OneCreatorX@users.noreply.github.com>
2023-07-22 11:04:25 -0300

    Actualizar Alejarse+.lua

--- Scripts/Generales/Beta/Alejarse+.lua
+++ Scripts/Generales/Beta/Alejarse+.lua
@@ -1,122 +1,90 @@
--- Inserta este script en un objeto en el juego de Roblox
+local JugadorLocal = game.Players.LocalPlayer
+local HumanoidRootPart = JugadorLocal.Character and JugadorLocal.Character:WaitForChild("HumanoidRootPart")
 
--- Configuración
-local player = game.Players.LocalPlayer -- Jugador local
-local distanceFromPlayer = 10 -- Distancia del Part respecto al jugador (ajusta según tus necesidades)
+local INTERVALO_COMPROBACION = 0.1 -- Intervalo de tiempo en segundos para realizar el cálculo de alejamiento
+local TIEMPO_ULTIMA_COMPROBACION = 0
+local comprobadorActivo = false -- El botón estará desactivado por defecto
 
--- Crear el Part y el SurfaceGUI
-local part = Instance.new("Part")
-part.Name = "ButtonPart"
-part.Size = Vector3.new(8, 5, 1)
-part.CFrame = player.Character.HumanoidRootPart.CFrame + (player.Character.HumanoidRootPart.CFrame.LookVector * distanceFromPlayer)
-part.Anchored = true
-part.CanCollide = false
-part.TopSurface = Enum.SurfaceType.Smooth
-part.BottomSurface = Enum.SurfaceType.Smooth
-part.Parent = workspace
+local function CalcularNuevaPosicion(jugador)
+    if not comprobadorActivo then return end -- Si el comprobador no está activo, no realizar la comprobación
 
-local surfaceGui = Instance.new("SurfaceGui")
-surfaceGui.Face = Enum.NormalId.Front
-surfaceGui.Parent = part
+    local PosicionLocal = HumanoidRootPart and HumanoidRootPart.Position
+    local PosicionJugador = jugador.Character and jugador.Character:WaitForChild("HumanoidRootPart").Position
 
-local activateButton = Instance.new("TextButton")
-activateButton.Name = "ActivateButton"
-activateButton.Size = UDim2.new(1, 0, 0.5, 0) -- Ajustar tamaño del botón
-activateButton.Position = UDim2.new(0, 0, 0, 0) -- Ajustar posición del botón
-activateButton.BackgroundColor3 = Color3.fromRGB(0, 255, 0) -- Color verde para el botón de activar
-activateButton.Text = "Activar"
-activateButton.FontSize = Enum.FontSize.Size48 -- Ajustar tamaño de fuente
-activateButton.TextColor3 = Color3.fromRGB(255, 255, 255)
-activateButton.TextScaled = true -- Escalar el texto para que ocupe el espacio
-activateButton.Parent = surfaceGui
+    if PosicionLocal and PosicionJugador then
+        local Distancia = (PosicionJugador - PosicionLocal).Magnitude
 
-local deactivateButton = Instance.new("TextButton")
-deactivateButton.Name = "DeactivateButton"
-deactivateButton.Size = UDim2.new(1, 0, 0.5, 0) -- Ajustar tamaño del botón
-deactivateButton.Position = UDim2.new(0, 0, 0.5, 0) -- Ajustar posición del botón
-deactivateButton.BackgroundColor3 = Color3.fromRGB(255, 0, 0) -- Color rojo para el botón de desactivar
-deactivateButton.Text = "Desactivar"
-deactivateButton.FontSize = Enum.FontSize.Size48 -- Ajustar tamaño de fuente
-deactivateButton.TextColor3 = Color3.fromRGB(255, 255, 255)
-deactivateButton.TextScaled = true -- Escalar el texto para que ocupe el espacio
-deactivateButton.Parent = surfaceGui
-
--- Variables
-local active = false -- Estado de movimiento activado/desactivado
-
--- Función para alejar al jugador local de otros jugadores
-local function moveAwayFromPlayers()
-    local character = player.Character
-    local rootPart = character and character:FindFirstChild("HumanoidRootPart")
-    
-    if rootPart then
-        local currentPosition = rootPart.Position
-        
-        -- Buscar jugadores cercanos
-        for _, otherPlayer in ipairs(game.Players:GetPlayers()) do
-            if otherPlayer ~= player then
-                local otherCharacter = otherPlayer.Character
-                local otherRootPart = otherCharacter and otherCharacter:FindFirstChild("HumanoidRootPart")
-                
-                if otherRootPart then
-                    local distance = (currentPosition - otherRootPart.Position).Magnitude
-                    
-                    -- Alejar al jugador local si se encuentra dentro del rango
-                    if distance < 10 then
-                        local direction = (currentPosition - otherRootPart.Position).Unit
-                        local newPosition = currentPosition + (direction * 5)
-                        
-                        -- Comprobar si hay obstáculos en el camino
-                        local raycastParams = RaycastParams.new()
-                        raycastParams.FilterType = Enum.RaycastFilterType.Blacklist
-                        raycastParams.FilterDescendantsInstances = {character, otherCharacter}
-                        local raycastResult = workspace:Raycast(currentPosition, direction, raycastParams)
-                        
-                        if raycastResult then
-                            newPosition = rootPart.Position
-                        end
-                        
-                        -- Mover al jugador local
-                        rootPart.CFrame = CFrame.new(newPosition)
-                    end
-                end
-            end
+        if Distancia <= 7 then
+            local Direccion = (PosicionLocal - PosicionJugador).Unit
+            local NuevaPosicion = PosicionLocal + Direccion * 5
+            HumanoidRootPart.CFrame = CFrame.new(NuevaPosicion)
         end
     end
 end
 
--- Función para activar el movimiento
-local function activateMovement()
-    active = true
-end
-
--- Función para desactivar el movimiento
-local function deactivateMovement()
-    active = false
-end
-
--- Función para verificar la distancia y regresar al jugador si está lejos
-local function checkDistance()
-    local character = player.Character
-    if character then
-        local rootPart = character:FindFirstChild("HumanoidRootPart")
-        if rootPart then
-            local distance = (part.Position - rootPart.Position).Magnitude
-            if distance > 50 then
-                character:SetPrimaryPartCFrame(part.CFrame)
-            end
+game.Players.PlayerAdded:Connect(function(player)
+    player.CharacterAdded:Connect(function(character)
+        if player ~= JugadorLocal then -- Solo verifica otros jugadores, no al propio jugador local.
+            CalcularNuevaPosicion(player)
         end
-    end
-end
-
--- Conexiones de eventos
-activateButton.MouseButton1Click:Connect(activateMovement)
-deactivateButton.MouseButton1Click:Connect(deactivateMovement)
-
-game:GetService("RunService").Heartbeat:Connect(function()
-    if active then
-        moveAwayFromPlayers()
-    end
-    checkDistance()
+    end)
 end)
 
+game:GetService("RunService").Heartbeat:Connect(function()
+    if tick() - TIEMPO_ULTIMA_COMPROBACION >= INTERVALO_COMPROBACION then
+        TIEMPO_ULTIMA_COMPROBACION = tick()
+        for _, jugador in ipairs(game.Players:GetPlayers()) do
+            if jugador ~= JugadorLocal then
+                CalcularNuevaPosicion(jugador)
+            end
+        end
+    end
+end)
+
+local gui = Instance.new("ScreenGui")
+gui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+
+local mainFrame = Instance.new("Frame")
+mainFrame.Size = UDim2.new(0, 100, 0, 50) -- Tamaño más pequeño
+mainFrame.Position = UDim2.new(1, -110, 1, -70) -- Posición ajustada
+mainFrame.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+mainFrame.BackgroundTransparency = 0.5
+mainFrame.BorderSizePixel = 0
+mainFrame.Active = true
+mainFrame.Draggable = true -- Permite arrastrar la interfaz
+mainFrame.Parent = gui
+
+local closeButton = Instance.new("TextButton")
+closeButton.Text = "x"
+closeButton.Size = UDim2.new(0, 20, 0, 20)
+closeButton.Position = UDim2.new(1, -25, 0, 5) -- Sobrepuesto en la esquina
+closeButton.BackgroundTransparency = 1 -- Sin fondo
+closeButton.Font = Enum.Font.SourceSansBold
+closeButton.TextColor3 = Color3.fromRGB(255, 0, 0) -- Rojo para indicar cerrar
+closeButton.TextSize = 18
+closeButton.Parent = mainFrame
+
+local toggleButton = Instance.new("TextButton")
+toggleButton.Text = "Alejarse"
+toggleButton.Size = UDim2.new(0, 80, 0, 25) -- Tamaño más pequeño
+toggleButton.Position = UDim2.new(0, 10, 0, 20) -- Posición ajustada
+toggleButton.BackgroundColor3 = Color3.fromRGB(255, 0, 0) -- Rojo para indicar desactivado por defecto
+toggleButton.Parent = mainFrame
+
+local function cambiarEstadoComprobador()
+    comprobadorActivo = not comprobadorActivo
+    if comprobadorActivo then
+        toggleButton.Text = "Alejandose"
+        toggleButton.BackgroundColor3 = Color3.fromRGB(0, 255, 0) -- Verde para indicar activado
+    else
+        toggleButton.Text = "Alejarse"
+        toggleButton.BackgroundColor3 = Color3.fromRGB(255, 0, 0) -- Rojo para indicar desactivado
+    end
+end
+
+local function destruirGUI()
+    gui:Destroy()
+end
+
+toggleButton.MouseButton1Click:Connect(cambiarEstadoComprobador)
+closeButton.MouseButton1Click:Connect(destruirGUI)
