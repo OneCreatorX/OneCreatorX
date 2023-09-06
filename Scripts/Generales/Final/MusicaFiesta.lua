6eb3be0ba7f88d3a80aa675e450257fcd697beeb
OneCreatorX <138682257+OneCreatorX@users.noreply.github.com>
2023-09-06 01:57:02 -0300

    Actualizar MusicaFiesta.lua

--- Scripts/Generales/Final/MusicaFiesta.lua
+++ Scripts/Generales/Final/MusicaFiesta.lua
@@ -1,139 +1,103 @@
+-- Crear un ScreenGUI
 local player = game.Players.LocalPlayer
 local gui = Instance.new("ScreenGui")
 gui.Parent = player.PlayerGui
 
-local frame = Instance.new("Frame")
-frame.Size = UDim2.new(0.2, 0, 0.35, 0)
-frame.Position = UDim2.new(0.4, 0, 0.3, 0)
-frame.BackgroundColor3 = Color3.new(0.2, 0.2, 0.2)
-frame.BorderSizePixel = 2
-frame.BorderColor3 = Color3.new(0, 0, 0)
-frame.Active = true
-frame.Draggable = true
-frame.Parent = gui
+-- Crear un Frame principal (fondo)
+local background = Instance.new("Frame")
+background.Size = UDim2.new(0.3, 0, 0.2, 0) -- Aumentar el tamaño de la interfaz
+background.Position = UDim2.new(0.35, 0, 0.4, 0) -- Ajustar la posición
+background.BackgroundColor3 = Color3.new(0.2, 0.2, 0.2)
+background.BorderSizePixel = 0
+background.Active = true
+background.Draggable = true
+background.Parent = gui
 
-local titleLabel = Instance.new("TextLabel")
-titleLabel.Size = UDim2.new(1, 0, 0.1, 0)
-titleLabel.Position = UDim2.new(0, 0, 0, 0)
-titleLabel.Parent = frame
-titleLabel.Text = "ID de Música"
-titleLabel.TextColor3 = Color3.new(1, 1, 1)
-titleLabel.BackgroundTransparency = 1
-titleLabel.Font = Enum.Font.SourceSansBold
-titleLabel.TextSize = 16
-titleLabel.TextStrokeTransparency = 0.5
+-- Crear el directorio "MiMusica" en Workspace y una única instancia de Sound
+local workspace = game:GetService("Workspace")
+local musicFolder = workspace:FindFirstChild("MiMusica")
+local sound = nil
 
-local closeButton = Instance.new("TextButton")
-closeButton.Size = UDim2.new(0.1, 0, 0.1, 0)
-closeButton.Position = UDim2.new(0.9, 0, 0, 0)
-closeButton.Parent = frame
-closeButton.BackgroundColor3 = Color3.new(1, 0, 0)
-closeButton.Text = "X"
-closeButton.TextColor3 = Color3.new(1, 1, 1)
-closeButton.TextSize = 16
-closeButton.AutoButtonColor = false
+if not musicFolder then
+    musicFolder = Instance.new("Folder")
+    musicFolder.Name = "MiMusica"
+    musicFolder.Parent = workspace
+end
 
+if not sound then
+    sound = Instance.new("Sound")
+    sound.Parent = musicFolder
+end
+
+-- Crear un TextBox
 local textBox = Instance.new("TextBox")
-textBox.Size = UDim2.new(0.9, 0, 0.15, 0)
-textBox.Position = UDim2.new(0.05, 0, 0.2, 0)
-textBox.Parent = frame
-textBox.PlaceholderText = "ID de música"
-textBox.Font = Enum.Font.SourceSans
-textBox.TextSize = 16
+textBox.Size = UDim2.new(0.7, 0, 0.15, 0) -- Ajustar el tamaño del TextBox
+textBox.Position = UDim2.new(0.15, 0, 0.25, 0) -- Ajustar la posición del TextBox
+textBox.Parent = background
 
-local button = Instance.new("TextButton")
-button.Size = UDim2.new(0.6, 0, 0.15, 0)
-button.Position = UDim2.new(0.2, 0, 0.4, 0)
-button.Parent = frame
-button.BackgroundColor3 = Color3.new(0, 0.5, 0)
-button.Text = "Reproducir"
-button.TextColor3 = Color3.new(1, 1, 1)
-button.TextSize = 16
+-- Crear un botón de cierre (X)
+local buttonCerrar = Instance.new("TextButton")
+buttonCerrar.Size = UDim2.new(0.05, 0, 0.05, 0)
+buttonCerrar.Position = UDim2.new(0.9, 0, 0.05, 0)
+buttonCerrar.Text = "X"
+buttonCerrar.TextColor3 = Color3.new(1, 1, 1)
+buttonCerrar.BackgroundColor3 = Color3.new(1, 0, 0)
+buttonCerrar.BorderSizePixel = 0
+buttonCerrar.Parent = background
 
-local confirmButton = Instance.new("TextButton")
-confirmButton.Size = UDim2.new(0.6, 0, 0.15, 0)
-confirmButton.Position = UDim2.new(0.2, 0, 0.6, 0)
-confirmButton.Parent = frame
-confirmButton.BackgroundColor3 = Color3.new(0, 0.5, 0)
-confirmButton.Text = "Confirmar Envío"
-confirmButton.TextColor3 = Color3.new(1, 1, 1)
-confirmButton.TextSize = 16
-confirmButton.Visible = false
-
-local audio = nil
-local currentID = nil
-
-local function createNotification(message, color)
-    local notification = Instance.new("TextLabel")
-    notification.Size = UDim2.new(0.6, 0, 0.1, 0)
-    notification.Position = UDim2.new(0.2, 0, 0.8, 0)
-    notification.Parent = frame
-    notification.Text = message
-    notification.TextColor3 = color
-    notification.BackgroundTransparency = 0.5
-    notification.Font = Enum.Font.SourceSansBold
-    notification.TextSize = 16
-    notification.TextStrokeTransparency = 0.5
-    wait(5)
-    notification:Destroy()
+-- Función para cambiar el fondo de color
+local function cambiarColor(color)
+    background.BackgroundColor3 = color
 end
 
-local function playAudio(musicID)
-    if audio then
-        audio:Stop()
-        audio:Destroy()
-    end
-
-    audio = Instance.new("Sound")
-    audio.Parent = game.Workspace
-    audio.SoundId = "rbxassetid://" .. musicID
-    audio.Looped = false
-    audio.Volume = 10
-
-    audio:Play()
-    wait(5)
-    audio:Stop()
-end
-
-local function sendMusicID(musicID)
-    local args = { musicID }
-    game:GetService("ReplicatedStorage"):WaitForChild("MusicEvents"):WaitForChild("RequestSong"):FireServer(unpack(args))
-    textBox.Text = ""
-    createNotification("Música enviada con éxito", Color3.new(0, 1, 0))
-end
-
-local function handleButton()
-    local musicID = tonumber(textBox.Text)
-    if musicID then
-        if currentID == musicID then
-            playAudio(musicID)
-            button.Text = "Reproducir de Nuevo"
-            confirmButton.Visible = true
-        else
-            button.Text = "Reproducir"
-            confirmButton.Visible = false
-        end
-        currentID = musicID
-    else
-        createNotification("ID de música inválido", Color3.new(1, 0, 0))
-    end
-end
-
-button.MouseButton1Click:Connect(handleButton)
-
-confirmButton.MouseButton1Click:Connect(function()
-    local musicID = tonumber(textBox.Text)
-    if musicID then
-        sendMusicID(musicID)
-        button.Text = "Reproducir"
-        confirmButton.Visible = false
-    end
-end)
-
-closeButton.MouseButton1Click:Connect(function()
-    if audio then
-        audio:Stop()
-        audio:Destroy()
-    end
+-- Función para cerrar la interfaz
+local function cerrarInterfaz()
     gui:Destroy()
-end)
+end
+
+-- Conectar la función al botón de cierre
+buttonCerrar.MouseButton1Click:Connect(cerrarInterfaz)
+
+-- Función para verificar el ID y cargar la música
+local function verificarID()
+    local musicID = tonumber(textBox.Text)
+    print("Verificando ID:", musicID)
+
+    -- Escuchar el evento Loaded para saber cuándo se carga el sonido
+    local loadedConnection
+    loadedConnection = sound.Loaded:Connect(function()
+        loadedConnection:Disconnect()
+        print("Música cargada, fondo verde")
+
+        print("Enviando ID al servidor...")
+
+        local args = { musicID }
+        game:GetService("ReplicatedStorage"):WaitForChild("MusicEvents"):WaitForChild("RequestSong"):FireServer(unpack(args))
+        cambiarColor(Color3.new(0, 1, 0))
+
+        print("ID enviado al servidor.")
+
+        wait(3)
+        cambiarColor(Color3.new(0.2, 0.2, 0.2))
+    end)
+
+    local loadSuccess, loadErrorMsg = pcall(function()
+        sound.SoundId = "rbxassetid://" .. musicID
+        sound:LoadAsync()
+    end)
+
+    if not loadSuccess then
+        cambiarColor(Color3.new(1, 0, 0))
+        print("Error al cargar la música:", loadErrorMsg)
+        wait(2)
+        cambiarColor(Color3.new(0.2, 0.2, 0.2))
+    end
+end
+
+-- Conectar la función al botón
+local button = Instance.new("TextButton")
+button.Size = UDim2.new(0.4, 0, 0.15, 0) -- Aumentar el tamaño del botón
+button.Position = UDim2.new(0.3, 0, 0.55, 0) -- Ajustar la posición del botón
+button.Text = "Enviar ID"
+button.Parent = background
+button.MouseButton1Click:Connect(verificarID)
