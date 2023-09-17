9946f56b5e239f22d5b6e7908bf65debf7afdcfb
OneCreatorX <138682257+OneCreatorX@users.noreply.github.com>
2023-09-17 06:31:50 -0300

    Actualizar EvitaCaida.lua

--- Scripts/Generales/Beta/EvitaCaida.lua
+++ Scripts/Generales/Beta/EvitaCaida.lua
@@ -1,54 +1,56 @@
+
 local Players = game:GetService("Players")
 local LocalPlayer = Players.LocalPlayer
 local Character = LocalPlayer.Character or LocalPlayer.CharacterAdded:Wait()
 local Humanoid = Character:WaitForChild("Humanoid")
 
-local userInputService = game:GetService("UserInputService")
+local functionActivada = false
+local botonActivar = nil
 
-local function ShowNegativeValueTextBox()
-    -- Crear una instancia de la clase TextBox
-    local textBox = Instance.new("TextBox")
-    textBox.Size = UDim2.new(0, 200, 0, 30) -- Establecer el tamaño del cuadro de texto
-    textBox.Position = UDim2.new(0.5, -100, 0.5, -15) -- Establecer la posición del cuadro de texto
-    textBox.AnchorPoint = Vector2.new(0.5, 0.5) -- Establecer el punto de anclaje en el centro
-    textBox.FontSize = Enum.FontSize.Size14 -- Establecer el tamaño de fuente
-    textBox.ClearTextOnFocus = true -- Borrar el texto cuando se hace clic en el cuadro de texto
-    textBox.PlaceholderText = "Ingresa un valor negativo" -- Establecer el texto de marcador de posición
-    textBox.Parent = LocalPlayer.PlayerGui
+local function ShowTeleportControls()
 
-    -- Función para verificar el valor negativo y realizar la teletransportación si es necesario
-    local function checkNegativeValue()
-        local value = tonumber(textBox.Text)
-        if value and value < 0 then
-            local currentPosition = Character.PrimaryPart.Position
-            local playerHeight = Character:GetExtentsSize().Y
-            local dangerZone = currentPosition.Y + playerHeight + value
+    local screenGui = Instance.new("ScreenGui")
+    screenGui.Parent = LocalPlayer.PlayerGui
 
-            -- Conectar el evento StateChanged para verificar la caída del jugador en la zona de peligro
-            local function onStateChanged(old, new)
-                if new == Enum.HumanoidStateType.Freefall and old ~= Enum.HumanoidStateType.Jumping and Character.PrimaryPart.Position.Y <= dangerZone then
-                    -- Teletransportar al jugador a la posición guardada
-                    Character:SetPrimaryPartCFrame(CFrame.new(currentPosition))
-                    print("Teleportado a la posición guardada")
-                end
-            end
 
-            Humanoid.StateChanged:Connect(onStateChanged)
+    botonActivar = Instance.new("TextButton")
+    botonActivar.Size = UDim2.new(0, 30, 0, 30)
+    botonActivar.Position = UDim2.new(0, 10, 0, 10)
+    botonActivar.Text = "[ ]"
+    botonActivar.Parent = screenGui
+
+    botonActivar.MouseButton1Click:Connect(function()
+        if functionActivada then
+            functionActivada = false
+            botonActivar.Text = " [ ]"
+        else
+            functionActivada = true
+            botonActivar.Text = "[✓]"
         end
-
-        textBox:Destroy()
-    end
-
-    -- Enfocar el cuadro de texto
-    userInputService.OverrideMouseIconBehavior = Enum.OverrideMouseIconBehavior.ForceHide
-    userInputService.MouseIconEnabled = false
-    textBox:CaptureFocus()
-    userInputService.MouseIconEnabled = true
-    userInputService.OverrideMouseIconBehavior = Enum.OverrideMouseIconBehavior.None
-
-    -- Conectar el evento FocusLost para verificar el valor negativo
-    textBox.FocusLost:Connect(checkNegativeValue)
+    end)
 end
 
--- Llamar a la función ShowNegativeValueTextBox() para iniciar la captura de entrada del usuario
-ShowNegativeValueTextBox()
+ShowTeleportControls()
+
+local zonaDePeligro = Character.PrimaryPart.Position.Y + Character:GetExtentsSize().Y - 1
+
+Humanoid.StateChanged:Connect(function(oldState, newState)
+    if newState == Enum.HumanoidStateType.Freefall or newState == Enum.HumanoidStateType.Physics then
+        
+        zonaDePeligro = Character.PrimaryPart.Position.Y + Character:GetExtentsSize().Y - 15
+        functionActivada = true
+        botonActivar.Text = "[✓]"
+    end
+end)
+
+game:GetService("RunService").Heartbeat:Connect(function()
+    if functionActivada and Character and Character:IsDescendantOf(game) and Character:FindFirstChild("Humanoid") then
+        local playerHeight = Character:GetExtentsSize().Y
+
+        if Character.PrimaryPart.Position.Y <= zonaDePeligro then
+            -- Teletransportar al jugador a la posición de teletransporte
+            Character:SetPrimaryPartCFrame(CFrame.new(Character.PrimaryPart.Position.X, Character.PrimaryPart.Position.Y + playerHeight, Character.PrimaryPart.Position.Z))
+            print("Teletransportado a la posición de teletransporte")
+        end
+    end
+end)
