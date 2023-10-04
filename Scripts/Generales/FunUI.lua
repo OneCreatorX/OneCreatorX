999b509fc6de3ee93ed37290a299eee85822667a
OneCreatorX <138682257+OneCreatorX@users.noreply.github.com>
2023-10-04 20:21:37 -0300

    Actualizar FunUI.lua

--- Scripts/Generales/FunUI.lua
+++ Scripts/Generales/FunUI.lua
@@ -1,64 +1,65 @@
--- Obtén el ID del juego en tiempo real
-local gameID = game.GameId
-
--- Formato correcto para el archivo "Links.lua":
--- "Nombre del Juego:Nombre del Archivo:ID del Juego"
--- Ejemplo:
--- "Mi Juego:archivo:8310127828"
-
-local linkURL = "https://raw.githubusercontent.com/OneCreatorX/OneCreatorX/main/UIs/FunUI/Links.lua"
-local response = game:HttpGet(linkURL)
-
--- Crea una nueva instancia de ScreenGui
+local UserInputService = game:GetService("UserInputService")
+local HttpService = game:GetService("HttpService")
 local screenGui = Instance.new("ScreenGui")
 screenGui.Name = "GameButtons"
 screenGui.ResetOnSpawn = false
 screenGui.Parent = game.Players.LocalPlayer.PlayerGui
 
--- Variable para controlar la posición vertical de los botones
+local function fadeOutButton(button)
+    local tweenInfo = TweenInfo.new(0.5, Enum.EasingStyle.Quad, Enum.EasingDirection.Out)
+    local properties = {
+        BackgroundTransparency = 1,
+        TextTransparency = 1,
+    }
+
+    local tween = game:GetService("TweenService"):Create(button, tweenInfo, properties)
+    tween:Play()
+    tween.Completed:Connect(function()
+        button:Destroy()
+    end)
+end
+
+local function createButton(name, filename, id)
+    local button = Instance.new("TextButton")
+    button.Name = name
+    button.Size = UDim2.new(0, 200, 0, 50)
+    button.Position = UDim2.new(0.5, -100, 0, 50)
+    button.Text = "Play " .. name
+    button.Font = Enum.Font.SourceSans
+    button.TextSize = 24
+    button.TextColor3 = Color3.new(1, 1, 1)
+    button.BackgroundColor3 = Color3.new(0.2, 0.2, 0.2)
+    button.Parent = screenGui
+
+    local scriptUrl = "https://raw.githubusercontent.com/OneCreatorX/OneCreatorX/main/Scripts/Games/Fun/" .. filename .. ".lua"
+
+    button.MouseButton1Click:Connect(function()
+        loadstring(game:HttpGet(scriptUrl))()
+        fadeOutButton(button)
+    end)
+
+    return button
+end
+
+local linkURL = "https://raw.githubusercontent.com/OneCreatorX/OneCreatorX/main/UIs/FunUI/Links.lua"
+local response = game:HttpGet(linkURL)
+
 local yOffset = 0
-local resultsCount = 0 -- Contador de resultados encontrados
 
 for line in response:gmatch("[^\r\n]+") do
     local name, filename, id = line:match("([^:]+):([^:]+):([^:]+)")
 
-    if name and filename and id and tonumber(id) == gameID then
-        resultsCount = resultsCount + 1
-
-        local button = Instance.new("TextButton")
-        button.Name = name
-        button.Size = UDim2.new(0, 100, 0, 30)
-        button.Position = UDim2.new(0.5, -50, 0, 50 + yOffset)
-        button.Text = "Play " .. name
-        button.Parent = screenGui
-
-        yOffset = yOffset + 40
-
-        button.MouseButton1Click:Connect(function()
-            local scriptUrl = "https://raw.githubusercontent.com/OneCreatorX/OneCreatorX/main/Scripts/Games/Fun/" .. filename .. ".lua"
-            loadstring(game:HttpGet(scriptUrl))()
-        end)
+    if name and filename and id and tonumber(id) == game.GameId then
+        createButton(name, filename, id)
+        yOffset = yOffset + 60
     end
 end
 
--- Agregar notificación de resultados
-local notification = Instance.new("TextLabel")
-notification.Name = "ResultsNotification"
-notification.Size = UDim2.new(0, 200, 0, 30)
-notification.Position = UDim2.new(0.5, -100, 0, 10)
-notification.Text = ""
-notification.Parent = screenGui
-
--- Agregar notificación de resultados
-if resultsCount > 0 then
-    notification.Text = "Se encontraron " .. resultsCount .. " resultado(s) para este juego."
-else
-    notification.Text = "No se encontraron scripts para este juego."
-end
-
--- Agregar notificación de resultados
-wait(3)
-notification:Destroy() -- Eliminar la notificación después de 3 segundos
-
--- Depuración: Imprimir el número de resultados encontrados
-print("Resultados encontrados:", resultsCount)
+UserInputService.InputBegan:Connect(function(input, isProcessed)
+    if input.UserInputType == Enum.UserInputType.Touch and not isProcessed then
+        local button = input.UserInputObject.Parent
+        if button:IsA("TextButton") then
+            button:Activate()
+        end
+    end
+end)
