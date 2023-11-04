2489929e839613113b2a438a5fbf912a2eee3df7
OneCreatorX <138682257+OneCreatorX@users.noreply.github.com>
2023-11-04 05:33:43 -0300

    Actualizar Trolling1.lua

--- Scripts/Generales/Final/Trolling1.lua
+++ Scripts/Generales/Final/Trolling1.lua
@@ -3045,7 +3045,7 @@ GhostAnim_Button.MouseButton1Click:Connect(function()
 	Animate.Disabled = true
 	StopAnim()
 	Animate.idle.Animation1.AnimationId = "http://www.roblox.com/asset/?id=1149612882"
-	Animate.idle.Animation2.AnimationId = "http://www.roblox.com/asset/?id=1150842221"
+	Animate.idle.Animation2.AnimationId = "http://www.roblox.com/asset/?id=707855907"
 	Animate.walk.WalkAnim.AnimationId = "http://www.roblox.com/asset/?id=1151231493"
 	Animate.run.RunAnim.AnimationId = "http://www.roblox.com/asset/?id=1150967949"
 	Animate.jump.JumpAnim.AnimationId = "http://www.roblox.com/asset/?id=616008936"
