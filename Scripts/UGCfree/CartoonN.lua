e6331d2a036f5c31924ab6e81fe05052c2e26eac
OneCreatorX <138682257+OneCreatorX@users.noreply.github.com>
2023-11-16 18:20:08 -0300

    Update CartoonN.lua

--- Scripts/UGCfree/CartoonN.lua
+++ Scripts/UGCfree/CartoonN.lua
@@ -5,11 +5,11 @@ screenGui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
 
 -- Crear un marco principal para la interfaz
 local mainFrame = Instance.new("Frame")
-mainFrame.Size = UDim2.new(0, 250, 0, 150)
-mainFrame.Position = UDim2.new(0.5, -125, 0, 20)
-mainFrame.BackgroundColor3 = Color3.new(0.2, 0.2, 0.2)
-mainFrame.BorderSizePixel = 2
-mainFrame.BorderColor3 = Color3.new(1, 1, 1)
+mainFrame.Size = UDim2.new(0, 300, 0, 200)
+mainFrame.Position = UDim2.new(0.5, -150, 0, 20)
+mainFrame.BackgroundColor3 = Color3.fromRGB(33, 33, 33)
+mainFrame.BorderSizePixel = 3
+mainFrame.BorderColor3 = Color3.fromRGB(255, 255, 255)
 mainFrame.Active = true -- Hacer el marco interactivo para arrastrar
 mainFrame.Draggable = true -- Hacer que el marco sea arrastrable
 mainFrame.Parent = screenGui
@@ -17,33 +17,40 @@ mainFrame.Parent = screenGui
 -- Crear un título dentro del marco principal
 local titleLabel = Instance.new("TextLabel")
 titleLabel.Text = "Cartoon Network Game On"
-titleLabel.TextSize = 20
-titleLabel.Size = UDim2.new(1, 0, 0.2, 0)
-titleLabel.BackgroundColor3 = Color3.new(0.4, 0.4, 0.4)
-titleLabel.TextColor3 = Color3.new(1, 1, 1)
+titleLabel.TextSize = 24
+titleLabel.Size = UDim2.new(1, 0, 0.15, 0)
+titleLabel.BackgroundColor3 = Color3.fromRGB(60, 60, 60)
+titleLabel.TextColor3 = Color3.fromRGB(255, 255, 255)
 titleLabel.Parent = mainFrame
 
 -- Crear un marco para la información adicional
 local infoFrame = Instance.new("Frame")
-infoFrame.Size = UDim2.new(1, 0, 0.6, 0)
-infoFrame.Position = UDim2.new(0, 0, 0.2, 0)
-infoFrame.BackgroundColor3 = Color3.new(0.4, 0.4, 0.4)
+infoFrame.Size = UDim2.new(1, 0, 0.85, 0)
+infoFrame.Position = UDim2.new(0, 0, 0.15, 0)
+infoFrame.BackgroundColor3 = Color3.fromRGB(45, 45, 45)
 infoFrame.Parent = mainFrame
+
+-- Crear un recuadro para la información adicional
+local infoTextFrame = Instance.new("Frame")
+infoTextFrame.Size = UDim2.new(0.8, 0, 0.8, 0)
+infoTextFrame.Position = UDim2.new(0.1, 0, 0.1, 0)
+infoTextFrame.BackgroundColor3 = Color3.fromRGB(80, 80, 80)
+infoTextFrame.Parent = infoFrame
 
 -- Crear un texto para la información adicional
 local infoLabel = Instance.new("TextLabel")
 infoLabel.Text = "Auto collect All - UGC\nYT: OneCreatorX"
-infoLabel.TextSize = 16
+infoLabel.TextSize = 18
 infoLabel.Size = UDim2.new(1, 0, 1, 0)
-infoLabel.TextColor3 = Color3.new(1, 1, 1)
-infoLabel.Parent = infoFrame
+infoLabel.TextColor3 = Color3.fromRGB(255, 255, 255)
+infoLabel.Parent = infoTextFrame
 
 -- Sobresalir la interfaz gráfica
-mainFrame.Position = UDim2.new(0.5, -125, 0, -75)
+mainFrame.Position = UDim2.new(0.5, -150, 0, -100)
 
 -- Restaurar la posición cuando se inicia el juego
 game.Players.LocalPlayer.CharacterAdded:Connect(function()
-    mainFrame.Position = UDim2.new(0.5, -125, 0, 20)
+    mainFrame.Position = UDim2.new(0.5, -150, 0, 20)
 end)
 
 -- Función para invocar acciones en el servidor
