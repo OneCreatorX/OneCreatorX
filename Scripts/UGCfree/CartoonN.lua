8c6752298db6663268f50042237242923da660e4
OneCreatorX <138682257+OneCreatorX@users.noreply.github.com>
2023-11-17 04:49:38 -0300

    Update CartoonN.lua

--- Scripts/UGCfree/CartoonN.lua
+++ Scripts/UGCfree/CartoonN.lua
@@ -1,107 +1,51 @@
--- Crear un ScreenGui para la interfaz gráfica
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local player = game.Players.LocalPlayer
+local playerGui = player:WaitForChild("PlayerGui")
 local screenGui = Instance.new("ScreenGui")
-screenGui.Name = "GameInterface"
-screenGui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+local background = Instance.new("Frame")
+local collectablesFolder = workspace:WaitForChild("Powerpuff Girls Collectables")
+local isUGCButtonActive = false
 
--- Crear un marco principal para la interfaz
-local mainFrame = Instance.new("Frame")
-mainFrame.Size = UDim2.new(0, 300, 0, 200)
-mainFrame.Position = UDim2.new(0.5, -150, 0, 20)
-mainFrame.BackgroundColor3 = Color3.fromRGB(33, 33, 33)
-mainFrame.BorderSizePixel = 3
-mainFrame.BorderColor3 = Color3.fromRGB(255, 255, 255)
-mainFrame.Active = true -- Hacer el marco interactivo para arrastrar
-mainFrame.Draggable = true -- Hacer que el marco sea arrastrable
-mainFrame.Parent = screenGui
+-- Function to create buttons
+local function createButton(text, position, size, callback)
+    local button = Instance.new("TextButton")
+    button.Size = size
+    button.Position = position
+    button.Text = text
+    button.TextColor3 = Color3.new(1, 1, 1)
+    button.BackgroundColor3 = Color3.new(0.2, 0.6, 1)  -- Color azul claro
+    button.BorderSizePixel = 0
+    button.Font = Enum.Font.SourceSansBold
+    button.Parent = background
+    button.MouseButton1Click:Connect(callback)
+    return button
+end
 
--- Crear un título dentro del marco principal
-local titleLabel = Instance.new("TextLabel")
-titleLabel.Text = "Cartoon Network Game On"
-titleLabel.TextSize = 21
-titleLabel.Size = UDim2.new(1, 0, 0.15, 0)
-titleLabel.BackgroundColor3 = Color3.fromRGB(60, 60, 60)
-titleLabel.TextColor3 = Color3.fromRGB(255, 255, 255)
-titleLabel.Parent = mainFrame
-
--- Crear un marco para la información adicional
-local infoFrame = Instance.new("Frame")
-infoFrame.Size = UDim2.new(1, 0, 0.85, 0)
-infoFrame.Position = UDim2.new(0, 0, 0.15, 0)
-infoFrame.BackgroundColor3 = Color3.fromRGB(45, 45, 45)
-infoFrame.Parent = mainFrame
-
--- Crear un recuadro para la información adicional
-local infoTextFrame = Instance.new("Frame")
-infoTextFrame.Size = UDim2.new(0.8, 0, 0.8, 0)
-infoTextFrame.Position = UDim2.new(0.1, 0, 0.1, 0)
-infoTextFrame.BackgroundColor3 = Color3.fromRGB(80, 80, 80)
-infoTextFrame.Parent = infoFrame
-
--- Crear un texto para la información adicional
-local infoLabel = Instance.new("TextLabel")
-infoLabel.Text = "Auto collect All - UGC\nYT: OneCreatorX"
-infoLabel.TextSize = 18
-infoLabel.Size = UDim2.new(1, 0, 1, 0)
-infoLabel.TextColor3 = Color3.fromRGB(255, 255, 255)
-infoLabel.Parent = infoTextFrame
-
--- Sobresalir la interfaz gráfica
-mainFrame.Position = UDim2.new(0.5, -150, 0, -100)
-
--- Restaurar la posición cuando se inicia el juego
-game.Players.LocalPlayer.CharacterAdded:Connect(function()
-    mainFrame.Position = UDim2.new(0.5, -150, 0, 20)
-end)
-
--- Función para cambiar la posición del jugador al Collision de cada modelo
+-- Function to move player to collision
 local function movePlayerToCollision()
-    local collectablesFolder = workspace:WaitForChild("Powerpuff Girls Collectables")
-
     for _, powerpuffBubblesFolder in pairs(collectablesFolder:GetChildren()) do
         if powerpuffBubblesFolder:IsA("Folder") then
-            print("Encontrado folder:", powerpuffBubblesFolder.Name)
-
             for _, collectableModel in pairs(powerpuffBubblesFolder:GetChildren()) do
                 if collectableModel:IsA("Model") then
                     local collisionPart = collectableModel:FindFirstChild("Collision")
-
                     if collisionPart and collisionPart:IsA("Part") then
-                        -- Cambiar la posición del jugador al Collision de cada modelo
-                        game.Players.LocalPlayer.Character:SetPrimaryPartCFrame(collisionPart.CFrame)
-
-                        print("Cambiando posición a Collision en modelo:", collectableModel.Name)
-
-                        -- Esperar un tiempo antes de pasar al siguiente
+                        player.Character:SetPrimaryPartCFrame(collisionPart.CFrame)
                         wait(1)
-                    else
-                        print("ADVERTENCIA: No se encontró Collision en el modelo:", collectableModel.Name)
                     end
-                else
-                    print("ADVERTENCIA: Se encontró un elemento que no es un modelo en 'PowerpuffBubbles':", collectableModel.Name)
                 end
             end
-        else
-            print("ADVERTENCIA: Se encontró un elemento que no es una carpeta en 'Powerpuff Girls Collectables':", powerpuffBubblesFolder.Name)
         end
     end
 end
 
--- Función para teleportar al jugador a los CollectableRootPart
+-- Function to teleport player to collectable root parts
 local function teleportPlayerToCollectableRootParts(folder)
     for _, child in pairs(folder:GetChildren()) do
         if child:IsA("Model") and child.Name == "ChemicalX" then
             local collectableRootPart = child:FindFirstChild("CollectableRootPart")
-
             if collectableRootPart and collectableRootPart:IsA("Part") then
-                -- Teletransportar al jugador al CollectableRootPart
-                game.Players.LocalPlayer.Character:SetPrimaryPartCFrame(collectableRootPart.CFrame)
-
-                print("Teletransportando al jugador a: " .. collectableRootPart.Name)
-
-                -- Esperar un tiempo antes de pasar al siguiente
+                player.Character:SetPrimaryPartCFrame(collectableRootPart.CFrame)
                 wait(1)
-            else
-                print("ADVERTENCIA: No se encontró CollectableRootPart en el modelo 'ChemicalX'")
             end
         elseif child:IsA("Folder") then
             teleportPlayerToCollectableRootParts(child)
@@ -109,9 +53,66 @@ local function teleportPlayerToCollectableRootParts(folder)
     end
 end
 
--- Ruta de la carpeta "Powerpuff Girls Collectables"
-local collectablesFolder = workspace:WaitForChild("Powerpuff Girls Collectables")
+-- Function to claim UGC (safe version)
+local function claimUGC()
+    local args = {
+        [1] = "PowerpuffBubbles",
+        [2] = 1700204402
+    }
 
+    local success, result = pcall(function()
+        return game:GetService("ReplicatedStorage"):WaitForChild("Packages"):WaitForChild("Knit"):WaitForChild("Services"):WaitForChild("ProgressionDataService"):WaitForChild("RF"):WaitForChild("CheckUGCAward"):InvokeServer(unpack(args))
+    end)
+
+    if success then
+        print("UGC reclamado exitosamente:", result)
+    else
+        warn("Error al reclamar UGC:", result)
+    end
+end
+
+-- Setup GUI
+screenGui.Parent = playerGui
+background.Size = UDim2.new(0, 200, 0, 120)
+background.Position = UDim2.new(0.5, -100, 0.5, -60)
+background.BackgroundColor3 = Color3.new(0, 0, 0)
+background.BackgroundTransparency = 0.3
+background.BorderSizePixel = 0
+background.Active = true
+background.Draggable = true
+background.Parent = screenGui
+
+-- Configuración de colores y apariencia
+local titleText = Instance.new("TextLabel")
+titleText.Size = UDim2.new(1, 0, 0.4, 0)
+titleText.Position = UDim2.new(0, 0, 0, 0)
+titleText.Text = "UGC Cartoon Network Game On"
+titleText.Font = Enum.Font.SourceSansBold
+titleText.TextSize = 16
+titleText.TextColor3 = Color3.new(1, 1, 1)
+titleText.Parent = background
+
+local smallText = Instance.new("TextLabel")
+smallText.Size = UDim2.new(0, 100, 0, 20)
+smallText.Position = UDim2.new(0.8, 0, 0.9, 0)
+smallText.Text = "YT: OneCreatorX"
+smallText.Font = Enum.Font.SourceSans
+smallText.TextSize = 12
+smallText.TextColor3 = Color3.new(1, 1, 1)
+smallText.Parent = background
+
+createButton("Ejecutar Funciones", UDim2.new(0.5, -50, 0.7, -15), UDim2.new(0, 100, 0, 30), function()
+    print("Ejecutar Funciones button clicked")
     movePlayerToCollision()
-wait(5)
-teleportPlayerToCollectableRootParts(collectablesFolder)
+    wait(1)
+    teleportPlayerToCollectableRootParts(collectablesFolder)
+end)
+
+createButton("Spam para reclamar UGC", UDim2.new(0.5, -50, 0.5, -15), UDim2.new(0, 100, 0, 30), function()
+    print("Spam para reclamar UGC button clicked")
+    isUGCButtonActive = not isUGCButtonActive
+    while isUGCButtonActive do
+        claimUGC()
+        wait(0.1)
+    end
+end)
