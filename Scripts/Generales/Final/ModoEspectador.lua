0a8b1bf2a017d8c3e82c043c9b8f528e70c5b721
OneCreatorX <138682257+OneCreatorX@users.noreply.github.com>
2023-11-30 03:52:43 -0300

    Update ModoEspectador.lua

--- Scripts/Generales/Final/ModoEspectador.lua
+++ Scripts/Generales/Final/ModoEspectador.lua
@@ -3,56 +3,30 @@ local UserInputService = game:GetService("UserInputService")
 local LocalPlayer = Players.LocalPlayer
 local Camera = game.Workspace.CurrentCamera
 
-local playerCameras = {}
-local visiblePlayers = {}
-local currentPlayerIndex = 0
-local dragging = false
-local dragStartX, dragStartY
+local currentPlayerIndex = 1
 
-local textbox -- Declaración de la variable textbox
-
-local function getPlayerCameras()
-    playerCameras = {}
-    for _, player in ipairs(Players:GetPlayers()) do
-        local playerCamera = player.Character and player.Character:FindFirstChildOfClass("Camera")
-        if playerCamera then
-            table.insert(playerCameras, playerCamera)
-        end
-    end
-end
-
-local function updateVisiblePlayers()
-    for _, player in ipairs(Players:GetPlayers()) do
-        if player ~= LocalPlayer then
-            player.Character.Humanoid.Visible = table.find(visiblePlayers, player) ~= nil
-        end
-    end
-end
+local textbox
 
 local function teleportToSelectedPlayer()
-    if currentPlayerIndex > 0 and currentPlayerIndex <= #visiblePlayers then
-        local seconds = tonumber(textbox.Text) -- Obtener los segundos ingresados como número
+    local selectedPlayer = Players:GetPlayers()[currentPlayerIndex]
+    if selectedPlayer then
+        local seconds = tonumber(textbox.Text)
         if seconds then
             local originalPosition = LocalPlayer.Character.HumanoidRootPart.CFrame
-            LocalPlayer.Character:SetPrimaryPartCFrame(visiblePlayers[currentPlayerIndex].Character.HumanoidRootPart.CFrame)
-            wait(seconds) -- Esperar durante los segundos especificados
-            LocalPlayer.Character:SetPrimaryPartCFrame(originalPosition) -- Volver a la posición original
+            LocalPlayer.Character:SetPrimaryPartCFrame(selectedPlayer.Character.HumanoidRootPart.CFrame)
+            wait(seconds)
+            LocalPlayer.Character:SetPrimaryPartCFrame(originalPosition)
         else
-            -- Si el TextBox está vacío o contiene texto no válido, simplemente teleportarse sin regreso
-            LocalPlayer.Character:SetPrimaryPartCFrame(visiblePlayers[currentPlayerIndex].Character.HumanoidRootPart.CFrame)
+            LocalPlayer.Character:SetPrimaryPartCFrame(selectedPlayer.Character.HumanoidRootPart.CFrame)
         end
     end
 end
 
 local function selectPlayer(index)
-    if #visiblePlayers > 0 then
-        currentPlayerIndex = index
-        if currentPlayerIndex < 1 then
-            currentPlayerIndex = #visiblePlayers
-        elseif currentPlayerIndex > #visiblePlayers then
-            currentPlayerIndex = 1
-        end
-        Camera.CameraSubject = visiblePlayers[currentPlayerIndex].Character.Humanoid
+    local playerCount = #Players:GetPlayers()
+    if playerCount > 0 then
+        currentPlayerIndex = (index - 1) % playerCount + 1
+        Camera.CameraSubject = Players:GetPlayers()[currentPlayerIndex].Character.Humanoid
     end
 end
 
@@ -61,11 +35,11 @@ local function resetCamera()
 end
 
 local screenGui = Instance.new("ScreenGui")
-screenGui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+screenGui.Parent = LocalPlayer:WaitForChild("PlayerGui")
 
 local frame = Instance.new("Frame")
 frame.Position = UDim2.new(0, 10, 0, 10)
-frame.Size = UDim2.new(0, 280, 0, 40) -- Aumenté el ancho para acomodar el TextBox
+frame.Size = UDim2.new(0, 280, 0, 40)
 frame.BackgroundColor3 = Color3.fromRGB(30, 30, 30)
 frame.BorderSizePixel = 0
 frame.Parent = screenGui
@@ -111,13 +85,7 @@ createButton(">", buttonSizeX + distanceX, buttonFunctions[2])
 createButton("[∆]", (buttonSizeX + distanceX) * 2, buttonFunctions[3])
 local teleportButton = createButton("TP", (buttonSizeX + distanceX) * 3, buttonFunctions[4])
 
-teleportButton.MouseButton1Down:Connect(function(input)
-    dragging = true
-    dragStartX = input.Position.X.Scale - teleportButton.Position.X.Scale
-    dragStartY = input.Position.Y.Scale - teleportButton.Position.Y.Scale
-end)
-
-textbox = Instance.new("TextBox") -- Creación de textbox
+textbox = Instance.new("TextBox")
 textbox.Size = UDim2.new(0, 50, 0, buttonSizeY)
 textbox.Position = UDim2.new(0, (buttonSizeX + distanceX) * 4, 0, 5)
 textbox.BackgroundTransparency = 0.8
@@ -129,25 +97,4 @@ textbox.TextSize = 14
 textbox.Text = "Seconds"
 textbox.Parent = frame
 
-local function onButtonMouseDrag(input)
-    if dragging then
-        local delta = input.Position - UDim2.new(dragStartX, dragStartY)
-        teleportButton.Position = teleportButton.Position + delta
-    end
-end
-
-teleportButton.MouseButton1Down:Connect(function(input)
-    dragging = true
-    dragStartX = input.Position.X.Scale - teleportButton.Position.X.Scale
-    dragStartY = input.Position.Y.Scale - teleportButton.Position.Y.Scale
-end)
-
-Players.PlayerAdded:Connect(getPlayerCameras)
-Players.PlayerRemoving:Connect(getPlayerCameras)
-Players.PlayerAdded:Connect(updateVisiblePlayersOnPlayerAdded)
-Players.PlayerRemoving:Connect(updateVisiblePlayersOnPlayerRemoving)
-
-getPlayerCameras()
-visiblePlayers = Players:GetPlayers()
-
-selectPlayer(1)
+resetCamera()
