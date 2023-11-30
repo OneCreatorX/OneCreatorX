fd1741054df416b5ef353263c4ae914ed7814d42
OneCreatorX <138682257+OneCreatorX@users.noreply.github.com>
2023-11-30 04:49:57 -0300

    Update AntiAfk.lua

--- Scripts/Generales/Final/AntiAfk.lua
+++ Scripts/Generales/Final/AntiAfk.lua
@@ -1,19 +1,34 @@
-wait(0.5)local ba=Instance.new("ScreenGui")
-local ca=Instance.new("TextLabel")local da=Instance.new("Frame")
-local _b=Instance.new("TextLabel")local ab=Instance.new("TextLabel")ba.Parent=game.CoreGui
-ba.ZIndexBehavior=Enum.ZIndexBehavior.Sibling;ca.Parent=ba;ca.Active=true
-ca.BackgroundColor3=Color3.new(0.176471,0.176471,0.176471)ca.Draggable=true
-ca.Position=UDim2.new(0.698610067,0,0.098096624,0)ca.Size=UDim2.new(0,370,0,52)
-ca.Font=Enum.Font.SourceSansSemibold;ca.Text="Anti AFK Script"ca.TextColor3=Color3.new(0,1,1)
-ca.TextSize=22;da.Parent=ca
-da.BackgroundColor3=Color3.new(0.196078,0.196078,0.196078)da.Position=UDim2.new(0,0,1.0192306,0)
-da.Size=UDim2.new(0,370,0,107)_b.Parent=da
-_b.BackgroundColor3=Color3.new(0.176471,0.176471,0.176471)_b.Position=UDim2.new(0,0,0.800455689,0)
-_b.Size=UDim2.new(0,370,0,21)_b.Font=Enum.Font.Arial;_b.Text="OneCreatorX "
-_b.TextColor3=Color3.new(0,1,1)_b.TextSize=20;ab.Parent=da
-ab.BackgroundColor3=Color3.new(0.176471,0.176471,0.176471)ab.Position=UDim2.new(0,0,0.158377,0)
-ab.Size=UDim2.new(0,370,0,44)ab.Font=Enum.Font.ArialBold;ab.Text="Status: Active"
-ab.TextColor3=Color3.new(0,1,1)ab.TextSize=20;local bb=game:service'VirtualUser'
+wait(5)
+local ba = Instance.new("ScreenGui")
+local ca = Instance.new("Frame")
+local ab = Instance.new("TextLabel")
+
+ba.Parent = game.CoreGui
+ba.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+
+ca.Parent = ba
+ca.Draggable = true
+ca.BackgroundColor3 = Color3.new(0.176471, 0.176471, 0.176471)
+ca.Position = UDim2.new(0.6, 0, -0.1, 0)
+ca.Size = UDim2.new(0, 150, 0, 30)
+ca.Active = true
+
+ab.Parent = ca
+ab.BackgroundColor3 = Color3.new(0.176471, 0.176471, 0.176471)
+ab.Position = UDim2.new(0, 0, 0, 0)
+ab.Size = UDim2.new(0, 150, 0, 30)
+ab.Font = Enum.Font.ArialBold
+ab.Draggable = true
+ab.Text = "AntiAFK: Active"
+ab.TextColor3 = Color3.new(0, 1, 1)
+
+ab.TextSize = 14
+
+local bb = game:service'VirtualUser'
 game:service'Players'.LocalPlayer.Idled:connect(function()
-bb:CaptureController()bb:ClickButton2(Vector2.new())
-ab.Text="Roblox trató de Sacarte :P el mamón"wait(2)ab.Text="Status : Active"end)
+    bb:CaptureController()
+    bb:ClickButton2(Vector2.new())
+    ab.Text = "Roblox trató de Sacarte :P el mamón"
+    wait(2)
+    ab.Text = "AntiAFK: Active"
+end)
