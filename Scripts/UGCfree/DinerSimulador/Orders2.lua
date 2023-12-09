5e8b09c7d918c252a71b755e5bbce036043fcce7
OneCreatorX <138682257+OneCreatorX@users.noreply.github.com>
2023-12-09 03:59:16 -0300

    Update Orders2.lua

--- Scripts/UGCfree/DinerSimulador/Orders2.lua
+++ Scripts/UGCfree/DinerSimulador/Orders2.lua
@@ -1,43 +1,124 @@
 local player = game.Players.LocalPlayer
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
-local freePositions = {
-    Vector3.new(209, 6.20046329498291, -86),
-    Vector3.new(210, 6.20046329498291, -86),
-    Vector3.new(211, 6.20046329498291, -86),
-    Vector3.new(212, 6.20046329498291, -86),
-    Vector3.new(213, 6.20046329498291, -86),
-    Vector3.new(214, 6.20046329498291, -86),
-    Vector3.new(215, 6.20046329498291, -86),
-    Vector3.new(216, 6.20046329498291, -86),
-    Vector3.new(217, 6.20046329498291, -86),
-}
-
-local foodPositions = {}
 local lastSelectedFood = nil
+local originalPlayerPosition = nil
 
-local screenGui = player.PlayerGui:FindFirstChild("MyScreenGui")
-if not screenGui then
-    screenGui = Instance.new("ScreenGui")
-    screenGui.Name = "MyScreenGui"
-    screenGui.Parent = player.PlayerGui
-end
+local screenGui = player.PlayerGui:FindFirstChild("MyScreenGui") or Instance.new("ScreenGui", player.PlayerGui)
+screenGui.Name = "MyScreenGui"
 
 local function markAsProcessed(npcModel)
     local processedMarker = Instance.new("BoolValue")
     processedMarker.Name = "Processed"
     processedMarker.Value = true
     processedMarker.Parent = npcModel
+
+    local foodDeliveredMarker = Instance.new("BoolValue")
+    foodDeliveredMarker.Name = "FoodDelivered"
+    foodDeliveredMarker.Value = false
+    foodDeliveredMarker.Parent = npcModel
+end
+
+local function teleportToNPCPosition(npcPosition)
+    local playerPosition = player.Character and player.Character:FindFirstChild("HumanoidRootPart") and player.Character.HumanoidRootPart.Position
+
+    if playerPosition then
+        if not originalPlayerPosition then
+            originalPlayerPosition = playerPosition
+        end
+
+        print("[DEBUG] Posición del jugador:", playerPosition)
+        player.Character.HumanoidRootPart.CFrame = CFrame.new(npcPosition)
+        wait(0.2)
+    else
+        warn("[WARN] No se encontró el HumanoidRootPart en el personaje del jugador.")
+    end
+end
+
+local function findNPCByName(npcName)
+    for _, npc in pairs(workspace.NPCS.Active:GetChildren()) do
+        local npcObj = npc:FindFirstChild("TrackerObj")
+        local foodDeliveredMarker = npc:FindFirstChild("FoodDelivered")
+
+        if npcObj and npcObj:IsA("ObjectValue") and npcObj.Value and typeof(npcObj.Value) == "Instance" then
+            local foodName = npcObj.Value.Name
+            print("[DEBUG] Buscando NPC por nombre:", npcName)
+            print("[DEBUG] Nombre de comida del NPC:", foodName)
+
+            if foodName:lower() == npcName:lower() and not (foodDeliveredMarker and foodDeliveredMarker.Value) then
+                print("[DEBUG] NPC encontrado por nombre:", npcName)
+                return npc
+            end
+        else
+            print("[DEBUG] NPC sin TrackerObj o valor no válido:", npc.Name)
+        end
+    end
+    print("[DEBUG] No se encontró un NPC con el nombre o ya recibió comida:", npcName)
+    return nil
+end
+
+local function deliverFoodToNPC(npc, platePosition)
+    print("[DEBUG] Entregando comida al NPC...")
+    local npcHeadPosition = npc:FindFirstChild("Head") and npc.Head.Position
+
+    if npcHeadPosition then
+        ReplicatedStorage.Remotes.Plating:InvokeServer({
+            PlateShelf = workspace.DinerPlaceHolder.PlateShelves.PlateShelf,
+            Position = npcHeadPosition + Vector3.new(0, 2, 0)
+        })
+        wait(0.2)
+        print("[DEBUG] Regresando al jugador a su posición original...")
+        teleportToNPCPosition(originalPlayerPosition)
+
+        -- Actualizar el estado de la propiedad FoodDelivered a true
+        npc:FindFirstChild("FoodDelivered").Value = true
+    else
+        warn("[WARN] No se encontró la cabeza del NPC para la posición de entrega.")
+    end
+end
+
+local function handleDeliverButtonClick()
+    print("[DEBUG] Botón 'Entregar Comida' clickeado.")
+    if lastSelectedFood then
+        print("[DEBUG] Comida seleccionada:", lastSelectedFood)
+        print("[DEBUG] Buscando el NPC con la misma comida...")
+        local npc = findNPCByName(lastSelectedFood)
+        if npc then
+            local npcPosition = npc:FindFirstChild("HumanoidRootPart") and npc.HumanoidRootPart.Position
+            if npcPosition then
+                print("[DEBUG] NPC encontrado:")
+                print("  - Nombre de la comida:", lastSelectedFood)
+                print("  - Posición del NPC:", npcPosition)
+                print("[DEBUG] Teleportando al jugador a la posición del NPC...")
+                
+                teleportToNPCPosition(npcPosition)
+                wait(0.2)
+                deliverFoodToNPC(npc)
+
+            else
+                warn("[WARN] No se encontró la cabeza del NPC para la posición de teletransporte.")
+            end
+        else
+            warn("[WARN] No se encontró el HumanoidRootPart en el NPC.")
+        end
+    else
+        warn("[WARN] No hay comida seleccionada.")
+    end
+end
+
+local function createDeliverButton()
+    local deliverButton = Instance.new("TextButton")
+    deliverButton.Parent = screenGui
+    deliverButton.Size = UDim2.new(0, 150, 0, 30)
+    deliverButton.Position = UDim2.new(0.931, -75, 0, 60)
+    deliverButton.Text = "Entregar Comida"
+    deliverButton.TextScaled = true
+
+    deliverButton.MouseButton1Click:Connect(handleDeliverButtonClick)
 end
 
 local function updateTableAndButtons()
-    print("[INFO] Actualizando tabla y botones...")
-
-    local previousFoodPositions = {}
-    for key, value in pairs(foodPositions) do
-        previousFoodPositions[key] = value
-    end
-
+    print("[DEBUG] Actualizando tabla y botones...")
     for _, npcModel in pairs(workspace.NPCS.Active:GetChildren()) do
         local trackerObj, processedMarker = npcModel:FindFirstChild("TrackerObj"), npcModel:FindFirstChild("Processed")
 
@@ -48,103 +129,21 @@ local function updateTableAndButtons()
 
             button.Parent = screenGui
             button.Size = UDim2.new(0, 150, 0, 30)
-            button.Position = UDim2.new(0.5, -75, 0, 0)
+            button.Position = UDim2.new(0.07, -75, 0, 40)
             button.Text = tostring(textValue)
-
-            local npcNumber = 1
-            while screenGui:FindFirstChild("NPC_" .. npcNumber) do
-                npcNumber = npcNumber + 1
-            end
-
-            local npcIdentifier = "NPC_" .. npcNumber
-
-            button.Name = npcIdentifier  -- Asignar el nombre único como nombre del botón
-            button.ZIndex = 1
+            button.TextScaled = true
 
             button.MouseButton1Click:Connect(function()
-                button:Destroy()
-                trackerObj:Destroy()
+                lastSelectedFood = tostring(textValue)
                 markAsProcessed(npcModel)
                 ReplicatedStorage.Remotes.ChangeMenu:InvokeServer(tostring(textValue), "CreatePlate")
-                
-                lastSelectedFood = tostring(textValue)
-                print("[INFO] Último plato seleccionado:", lastSelectedFood)
+                button:Destroy()
             end)
 
             markAsProcessed(npcModel)
-
-            -- Convertir el nombre a minúsculas para evitar problemas de mayúsculas y minúsculas
-            local lowercaseTextValue = string.lower(tostring(textValue))
-
-            if not foodPositions[lowercaseTextValue] then
-
-                local foodPosition = table.remove(freePositions, 1)
-                foodPositions[lowercaseTextValue] = foodPosition
-                print("[INFO] Se agregó a la tabla: Nombre:", lowercaseTextValue, "Posición:", foodPosition)
-
-                local npcFile = Instance.new("ObjectValue")
-                npcFile.Name = "NPCFile"
-                npcFile.Value = npcIdentifier
-                npcFile.Parent = npcModel
-            else
-                print("[INFO] La comida '" .. lowercaseTextValue .. "' ya existe en la tabla. Posición actual:", foodPositions[lowercaseTextValue])
-            end
         end
     end
-
-    for key, _ in pairs(previousFoodPositions) do
-        if not foodPositions[key] then
-            -- El nombre ya no existe en la tabla, destruir el botón correspondiente
-            local existingButton = screenGui:FindFirstChild(tostring(key))
-            if existingButton then
-                existingButton:Destroy()
-                print("[INFO] Se destruyó el botón correspondiente a la comida '" .. key .. "'.")
-            end
-        end
-    end
-
-    print("[INFO] Tabla de posiciones de comida actualizada:", foodPositions)
-end
-
-local function createPlaceOnTableButton()
-    print("[INFO] Creando botón 'Colocar en mesa'...")
-    local button = Instance.new("TextButton")
-    button.Parent = screenGui
-    button.Size = UDim2.new(0, 150, 0, 30)
-    button.Position = UDim2.new(0.5, -75, 0, 40)
-    button.Text = "Colocar en mesa"
-    button.ZIndex = #screenGui:GetChildren() + 1
-
-    button.MouseButton1Click:Connect(function()
-        print("[INFO] Botón 'Colocar en mesa' clickeado.")
-
-        local currentFoodName = lastSelectedFood or "Colocar en mesa" 
-
-        local lowercaseCurrentFoodName = string.lower(currentFoodName)
-
-        if foodPositions[lowercaseCurrentFoodName] then
-            local foodPosition = foodPositions[lowercaseCurrentFoodName]
-
-            local playerPosition = player.Character and player.Character:FindFirstChild("HumanoidRootPart") and player.Character.HumanoidRootPart.Position
-
-            if playerPosition then
-                local newPosition = Vector3.new(212.617172, 6.14484358, -83.0590057, 0.999130905, 2.48580339e-08, -0.0416823365, -2.91946627e-08, 1, -1.03431248e-07, 0.0416823365, 1.04558261e-07, 0.999130905)
-                player.Character.HumanoidRootPart.CFrame = CFrame.new(newPosition)
-
-                wait(0.2)
-                ReplicatedStorage.Remotes.Plating:InvokeServer({PlateShelf = workspace.DinerPlaceHolder.PlateShelves.PlateShelf, Position = foodPosition})
-                wait(0.2)
-                player.Character.HumanoidRootPart.CFrame = CFrame.new(playerPosition)
-                print("[INFO] Plato colocado en la mesa en posición:", foodPosition)
-            else
-                warn("[WARN] No se encontró el HumanoidRootPart en el personaje del jugador.")
-            end
-        else
-            warn("[WARN] No se encontró la posición para el plato seleccionado.")
-        end
-    end)
-end
-
+                    
 local function buscarIngredient(parent)
     local storages = parent.Workspace.DinerPlaceHolder.Storages
 
@@ -217,12 +216,16 @@ local function buscarIngredient(parent)
     end
 end
 
+    print("[DEBUG] Lista de comidas y NPCs:")
+    for _, npc in pairs(workspace.NPCS.Active:GetChildren()) do
+        local npcName = npc:FindFirstChild("TrackerObj") and npc.TrackerObj.Value
+        print("  - NPC:", npcName)
+    end
+end
+
 buscarIngredient(game)
 
 createPlaceOnTableButton()
 
 workspace.NPCS.Active.ChildAdded:Connect(updateTableAndButtons)
 workspace.NPCS.Active.ChildRemoved:Connect(updateTableAndButtons)
-
--- Imprimir la tabla completa de posiciones de comida
-print("[INFO] Tabla de posiciones de comida:", foodPositions)
