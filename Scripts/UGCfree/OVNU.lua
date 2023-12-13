8a120f7a00ac719fa2b75f79f2135701cbc2a6e7
OneCreatorX <138682257+OneCreatorX@users.noreply.github.com>
2023-12-13 01:23:02 -0300

    Update OVNU.lua

--- Scripts/UGCfree/OVNU.lua
+++ Scripts/UGCfree/OVNU.lua
@@ -15,6 +15,20 @@ local function realizarTransaccion(id)
     game:GetService("ReplicatedStorage"):WaitForChild("Shopping"):WaitForChild("Remotes"):WaitForChild("ToServer"):WaitForChild("RequestTransaction"):FireServer(unpack(args))
 end
 
+local function spamTransaction2()
+    local args = {
+        [1] = 2,
+        [2] = 142,
+        [3] = 1,
+        [4] = true
+    }
+
+    for _ = 1, 90000 do
+        wait(0.1)
+        game:GetService("ReplicatedStorage"):WaitForChild("Shopping"):WaitForChild("Remotes"):WaitForChild("ToServer"):WaitForChild("RequestTransaction"):FireServer(unpack(args))
+    end
+end
+
 local args = {
     [1] = 19
 }
@@ -36,21 +50,12 @@ end
 
 local coroutineLimitedPickups = coroutine.create(spamLimitedPickups)
 local coroutineTransacciones = coroutine.create(spamTransacciones)
+local coroutineTransaction2 = coroutine.create(spamTransaction2)
 
 coroutine.resume(coroutineLimitedPickups)
 coroutine.resume(coroutineTransacciones)
+coroutine.resume(coroutineTransaction2)
 
-game.Players.LocalPlayer.Character:SetPrimaryPartCFrame(CFrame.new(Vector3.new(205.243, 51.4019, 194.182)))
-task.wait(2)
-local args = {
-    [1] = 2,
-    [2] = 142,
-    [3] = 1,
-    [4] = true
-}
-
-game:GetService("ReplicatedStorage"):WaitForChild("Shopping"):WaitForChild("Remotes"):WaitForChild("ToServer"):WaitForChild("RequestTransaction"):FireServer(unpack(args))
-task.wait(10)
 local NPCsFolder = workspace:WaitForChild("GAMEPLAY"):WaitForChild("NPCs"):WaitForChild("ActiveNPCs")
 
 for _, npc in ipairs(NPCsFolder:GetChildren()) do
@@ -73,11 +78,21 @@ game:GetService("ReplicatedStorage"):WaitForChild("LoginCalendar"):WaitForChild(
 game:GetService("ReplicatedStorage"):WaitForChild("LoginCalendar"):WaitForChild("Remotes"):WaitForChild("ToServer"):WaitForChild("ClaimDailyRewards"):FireServer()
 
 local gui = Instance.new("ScreenGui")
+gui.IgnoreGuiInset = true
 gui.Parent = game.Players.LocalPlayer.PlayerGui
+
+local fondo = Instance.new("Frame")
+fondo.Size = UDim2.new(0.5, 0, 0.2, 0)
+fondo.Position = UDim2.new(0.25, 0, 0.75, 0)
+fondo.BackgroundColor3 = Color3.new(0, 0, 0)
+fondo.BackgroundTransparency = 0.5
 
 local texto = Instance.new("TextLabel")
 texto.Size = UDim2.new(1, 0, 1, 0)
 texto.Position = UDim2.new(0, 0, 0, 0)
 texto.Text = "Reset Character"
 texto.TextSize = 40
-texto.Parent = gui
+texto.TextColor3 = Color3.new(1, 1, 1)
+texto.BackgroundTransparency = 1
+texto.Parent = fondo
+fondo.Parent = gui
