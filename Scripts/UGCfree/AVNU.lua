0d7a9bcbcaeac15fcd12575b7a1b53309e745c6a
OneCreatorX <138682257+OneCreatorX@users.noreply.github.com>
2023-12-14 00:53:13 -0300

    Update AVNU.lua

--- Scripts/UGCfree/AVNU.lua
+++ Scripts/UGCfree/AVNU.lua
@@ -1,24 +1,19 @@
 -- YT:@OneCreatorX 
+local function fireTransactionRequest(itemID)
+    local args = {2, itemID, 1, true}
+    game:GetService("ReplicatedStorage"):WaitForChild("Shopping"):WaitForChild("Remotes"):WaitForChild("ToServer"):WaitForChild("RequestTransaction"):FireServer(unpack(args))
+end
+
 -- UGC 1
+fireTransactionRequest(142)
 
-local args = {
-    [1] = 2,
-    [2] = 142,
-    [3] = 1,
-    [4] = true
-}
-
-game:GetService("ReplicatedStorage"):WaitForChild("Shopping"):WaitForChild("Remotes"):WaitForChild("ToServer"):WaitForChild("RequestTransaction"):FireServer(unpack(args))
-
--- espera 20 segundos antes de intentar reclamar el siguiente 
+-- Espera 20 segundos antes de intentar reclamar el siguiente
 task.wait(20)
 
 -- UGC 2
-local args = {
-    [1] = 2,
-    [2] = 139,
-    [3] = 1,
-    [4] = true
-}
+fireTransactionRequest(139)
 
-game:GetService("ReplicatedStorage"):WaitForChild("Shopping"):WaitForChild("Remotes"):WaitForChild("ToServer"):WaitForChild("RequestTransaction"):FireServer(unpack(args))
+task.wait(4)
+
+-- Reintentar por si acaso XD
+fireTransactionRequest(139)
