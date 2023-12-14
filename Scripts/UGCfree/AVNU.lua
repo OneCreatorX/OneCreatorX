bac419841ffde39afc28e099001ae9e0e3a6355e
OneCreatorX <138682257+OneCreatorX@users.noreply.github.com>
2023-12-13 22:38:01 -0300

    Update AVNU.lua

--- Scripts/UGCfree/AVNU.lua
+++ Scripts/UGCfree/AVNU.lua
@@ -1,3 +1,17 @@
+-- UGC 1
+
+local args = {
+    [1] = 2,
+    [2] = 142,
+    [3] = 1,
+    [4] = true
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Shopping"):WaitForChild("Remotes"):WaitForChild("ToServer"):WaitForChild("RequestTransaction"):FireServer(unpack(args))
+
+task.wait(10)
+
+-- UGC 2
 local args = {
     [1] = 2,
     [2] = 139,
