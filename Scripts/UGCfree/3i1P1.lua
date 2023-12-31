d157f2d5ff8b060b92dc3565a62ae5b34dd0dfad
OneCreatorX <138682257+OneCreatorX@users.noreply.github.com>
2023-12-31 13:44:48 -0300

    Update 3i1P1.lua

--- Scripts/UGCfree/3i1P1.lua
+++ Scripts/UGCfree/3i1P1.lua
@@ -1,78 +1,97 @@
-local p = game.Players.LocalPlayer
-local t = game:GetService("TeleportService")
-local rs = game:GetService("ReplicatedStorage")
+local player = game.Players.LocalPlayer
+local TeleportService = game:GetService("TeleportService")
 
-local function j()
-    p.Character:WaitForChild("Humanoid").Jump = true
+local function activateJump()
+    player.Character:WaitForChild("Humanoid").Jump = true
 end
 
-local function tp(pos)
-    p.Character:WaitForChild("HumanoidRootPart").CFrame = CFrame.new(pos)
+local function teleportTo(position)
+    player.Character:WaitForChild("HumanoidRootPart").CFrame = CFrame.new(position)
     wait(1)
 end
 
-local function s(args, event)
-    local e = rs.Events:WaitForChild(event)
-    e:FireServer(unpack(args))
+local function playSuperpowerSimulator()
+    local args = {[1] = "Chakra"}
+    local events = game:GetService("ReplicatedStorage"):WaitForChild("Events")
+
+    events:WaitForChild("QuestGiverInteract"):FireServer(unpack(args))
+
+    for i = 1, 10 do
+        events:WaitForChild("Training"):FireServer({[1] = "Strength"})
+        events:WaitForChild("QuestGiverInteract"):FireServer(args)
+        wait(0.3)
+    end
+
+    wait(15)
+    showFinishNotification("¡Secuencia completada!")
 end
 
-local function f(msg)
-    local sg = p.PlayerGui:FindFirstChild("FSG") or Instance.new("ScreenGui", p.PlayerGui)
-    sg.Name = "FSG"
-    
-    local f = Instance.new("Frame", sg)
-    f.Size, f.Position, f.BackgroundColor3, f.BorderSizePixel = UDim2.new(0, 200, 0, 50), UDim2.new(0.5, -100, 0.5, -25), Color3.new(0, 0, 0), 0
+local function playTennisBallRain()
+    teleportTo(Vector3.new(287, 14, -343))
+    wait(1)
+    local court = game:GetService("ReplicatedStorage"):WaitForChild("Court")
 
-    local tl = Instance.new("TextLabel", f)
-    tl.Size, tl.Text, tl.TextColor3, tl.BackgroundTransparency = UDim2.new(1, 0, 1, 0), msg, Color3.new(1, 1, 1), 1
+    court:WaitForChild("ServerStartServe"):InvokeServer()
+    wait(0.3)
+
+    court:WaitForChild("ClientHitBall"):InvokeServer({[1] = Vector3.new(254.6953582763672, 0, -313.5354309082031), [2] = false})
+    wait(2)
+
+    wait(15)
+    showFinishNotification("¡Secuencia completada!")
+end
+
+local function playRobloxianHighSchool()
+    local modules = game:GetService("ReplicatedStorage"):WaitForChild("Modules")
+    local args = {[1] = workspace:WaitForChild("BlingObby"):WaitForChild("Office_Chair_Key"):WaitForChild("Seat")}
+
+    modules:WaitForChild("ObjectCode"):WaitForChild("Seat"):WaitForChild("Sit"):FireServer(unpack(args))
+
+    wait(11)
+    activateJump()
+
+    modules:WaitForChild("ObjectCode"):WaitForChild("BlingObby"):WaitForChild("EventClassroomKey"):WaitForChild("PickUp"):InvokeServer()
+
+    teleportTo(Vector3.new(145, 8, -337))
+    wait(1)
+    teleportTo(Vector3.new(882, -286, 7984))
+    wait(2)
+
+    wait(15)
+    showFinishNotification("¡Secuencia completada!")
+end
+
+local function showFinishNotification(message)
+    local screenGui = player.PlayerGui:FindFirstChild("FinishScreenGui")
+    if not screenGui then
+        screenGui = Instance.new("ScreenGui", player.PlayerGui)
+        screenGui.Name = "FinishScreenGui"
+    end
+
+    local frame = Instance.new("Frame", screenGui)
+    frame.Size = UDim2.new(0, 200, 0, 50)
+    frame.Position = UDim2.new(0.5, -100, 0.5, -25)
+    frame.BackgroundColor3 = Color3.new(0, 0, 0)
+    frame.BorderSizePixel = 0
+
+    local textLabel = Instance.new("TextLabel", frame)
+    textLabel.Size = UDim2.new(1, 0, 1, 0)
+    textLabel.Text = message
+    textLabel.TextColor3 = Color3.new(1, 1, 1)
+    textLabel.BackgroundTransparency = 1
 
     wait(5)
-    f:Destroy()
+    frame:Destroy()
 end
 
 while true do
-    local pId = game.PlaceId
-
-    if pId == 11696357981 then
-        s({"Chakra"}, "QuestGiverInteract")
-
-        for i = 1, 10 do
-            s({"Strength"}, "Training")
-            s({"Chakra"}, "QuestGiverInteract")
-            wait(0.3)
-        end
-
-        wait(5)
-        f("¡Secuencia completada!")
-
-    elseif pId == 11268121492 then
-        tp(Vector3.new(287, 14, -343))
-        wait(1)
-        
-        local court = rs:WaitForChild("Court")
-        court.ServerStartServe:InvokeServer()
-
-        while true do
-            local args = {[1] = Vector3.new(241.35153198242188, 0, -329.5710144042969), [2] = false}
-            rs.Court_ClientHitBall:InvokeServer(unpack(args))
-            wait(0.05)  -- Utilizando wait para controlar el intervalo
-        end
-
-        wait(5)
-        f("¡Secuencia completada!")
-
-    elseif pId == 447452406 then
-        s({workspace.BlingObby.Office_Chair_Key.Seat}, "Seat.Sit")
-        wait(11)
-        j()
-        s({}, "BlingObby.EventClassroomKey.PickUp")
-        tp(Vector3.new(145, 8, -337))
-        wait(1)
-        tp(Vector3.new(882, -286, 7984))
-        wait(2)
-        wait(5)
-        f("¡Secuencia completada!")
-
+    local placeId = game.PlaceId
+    if placeId == 11696357981 then
+        playSuperpowerSimulator()
+    elseif placeId == 11268121492 then
+        playTennisBallRain()
+    elseif placeId == 447452406 then
+        playRobloxianHighSchool()
     else
         warn("Script no configurado para el juego actual.")
     end
