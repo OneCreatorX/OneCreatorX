31ced147c1cf748e61d0d78918d8bd0101eee614
OneCreatorX <138682257+OneCreatorX@users.noreply.github.com>
2024-01-05 11:50:51 -0300

    Update RunForUGC.lua

--- Scripts/UGCfree/RunForUGC.lua
+++ Scripts/UGCfree/RunForUGC.lua
@@ -1,18 +1,24 @@
-local P,LP,W,RS = game:GetService("Players"), game.Players.LocalPlayer, game:GetService("Workspace"), game:GetService("RunService")
+local P, LP, W, RS = game:GetService("Players"), game.Players.LocalPlayer, game:GetService("Workspace"), game:GetService("RunService")
 local SG = Instance.new("ScreenGui", LP:WaitForChild("PlayerGui"))
 
-local RunBtn = Instance.new("TextButton", SG) RunBtn.Size, RunBtn.Position, RunBtn.Text = UDim2.new(0,100,0,50), UDim2.new(0,10,0,10), "Run"
-local AnclarBtn = Instance.new("TextButton", SG) AnclarBtn.Size, AnclarBtn.Position, AnclarBtn.Text = UDim2.new(0,100,0,50), UDim2.new(0,120,0,10), "Anclar"
-local DesactivarBtn = Instance.new("TextButton", SG) DesactivarBtn.Size, DesactivarBtn.Position, DesactivarBtn.Text, DesactivarBtn.Visible = UDim2.new(0,100,0,50), UDim2.new(0,10,0,70), "Desactivar", false
+local RunBtn = Instance.new("TextButton", SG) RunBtn.Size, RunBtn.Position, RunBtn.Text = UDim2.new(0, 100, 0, 50), UDim2.new(0, 10, 0, 10), "Run"
+local AnclarBtn = Instance.new("TextButton", SG) AnclarBtn.Size, AnclarBtn.Position, AnclarBtn.Text = UDim2.new(0, 100, 0, 50), UDim2.new(0, 120, 0, 10), "Anclar"
+local DesactivarBtn = Instance.new("TextButton", SG) DesactivarBtn.Size, DesactivarBtn.Position, DesactivarBtn.Text, DesactivarBtn.Visible = UDim2.new(0, 100, 0, 50), UDim2.new(0, 10, 0, 70), "Desactivar", false
 
-local Pl,AG,AA = nil,0,false
+local Pl, AG, AA = nil, 0, false
 
 local function Run()
-    if not Pl then
+    if Pl then
+        Pl:Destroy()
+        Pl, AG = nil, nil
+
+        LP.Character.Humanoid.WalkSpeed = 16
+        RunBtn.Visible, DesactivarBtn.Visible = true, false
+    else
         AG = LP.Character.HumanoidRootPart.Position.Y
 
         Pl = Instance.new("Part")
-        Pl.Size, Pl.BrickColor, Pl.Anchored, Pl.Position = Vector3.new(5, 1, 5), BrickColor.new("Sand blue"), true, Vector3.new(LP.Character.HumanoidRootPart.Position.X,AG + 900 - 3.5,LP.Character.HumanoidRootPart.Position.Z)
+        Pl.Size, Pl.BrickColor, Pl.Anchored, Pl.Position = Vector3.new(5, 1, 5), BrickColor.new("Sand blue"), true, Vector3.new(LP.Character.HumanoidRootPart.Position.X, AG + 900 - 3.5, LP.Character.HumanoidRootPart.Position.Z)
         Pl.Parent = W
 
         LP.Character:SetPrimaryPartCFrame(CFrame.new(LP.Character.HumanoidRootPart.Position + Vector3.new(0, 900, 0)))
@@ -20,7 +26,7 @@ local function Run()
 
         RS.Heartbeat:Connect(function()
             if Pl and LP.Character then
-                Pl.Position = Vector3.new(LP.Character.HumanoidRootPart.Position.X,AG + 900 - 3.5,LP.Character.HumanoidRootPart.Position.Z)
+                Pl.Position = Vector3.new(LP.Character.HumanoidRootPart.Position.X, AG + 900 - 3.5, LP.Character.HumanoidRootPart.Position.Z)
             end
         end)
 
@@ -49,19 +55,9 @@ local function Anclar()
     end
 end
 
-local function Desactivar()
-    if Pl then
-        Pl:Destroy()
-        Pl, AG = nil, nil
-
-        LP.Character.Humanoid.WalkSpeed = 16
-        RunBtn.Visible, DesactivarBtn.Visible = true, false
-    end
-end
-
 RunBtn.MouseButton1Down:Connect(Run)
 AnclarBtn.MouseButton1Down:Connect(Anclar)
-DesactivarBtn.MouseButton1Down:Connect(Desactivar)
+DesactivarBtn.MouseButton1Down:Connect(Run) -- Desactivar también activa Run
 
 LocalPlayer.Idled:Connect(function()
     local VirtualUser = game:GetService('VirtualUser')
