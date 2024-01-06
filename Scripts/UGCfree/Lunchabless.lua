46628ae03b44afafef36971bfacb674d4938f5f0
OneCreatorX <138682257+OneCreatorX@users.noreply.github.com>
2024-01-06 13:53:39 -0300

    Update Lunchabless.lua

--- Scripts/UGCfree/Lunchabless.lua
+++ Scripts/UGCfree/Lunchabless.lua
@@ -1,24 +1,6 @@
 local p = game.Players.LocalPlayer
 local m = p.Character:WaitForChild("HumanoidRootPart")
 
-local screenGui = Instance.new("ScreenGui")
-screenGui.Name = "OneCreatorXGui"
-screenGui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
-
--- Crear TextLabel
-local textLabel = Instance.new("TextLabel")
-textLabel.Name = "StatusTextLabel"
-textLabel.Text = "OneCreatorX Working for you, wait"
-textLabel.Size = UDim2.new(1, 0, 0, 30)
-textLabel.Position = UDim2.new(0, 0, 0.9, 0)
-textLabel.BackgroundColor3 = Color3.new(0, 0, 0)  -- Fondo negro
-textLabel.BackgroundTransparency = 1  -- Sin fondo
-textLabel.TextColor3 = Color3.new(1, 1, 1)  -- Texto blanco
-textLabel.Font = Enum.Font.SourceSansBold
-textLabel.TextSize = 40
-textLabel.Parent = screenGui
-
-
 local function sendPurchaseRequest(productId)
     game:GetService("ReplicatedStorage")
         :WaitForChild("UGCOwnershipComms")
@@ -49,10 +31,40 @@ end
 
 spawn(activateProximityPromptsInFolder)
 
-for _, object in ipairs(workspace:WaitForChild("VisiblePointObjects"):GetDescendants()) do
-    if object:IsA("MeshPart") then
-        m.CFrame = CFrame.new(object.Position)
-        sendPurchaseRequest("14751842192")
-        wait(2)
+local screenGui = Instance.new("ScreenGui")
+screenGui.Name = "OneCreatorXGui"
+screenGui.Parent = p:WaitForChild("PlayerGui")
+
+local textLabel = Instance.new("TextLabel")
+textLabel.Name = "StatusTextLabel"
+textLabel.Text = "OneCreatorX Working for you, wait"
+textLabel.Size = UDim2.new(1, 0, 0, 30)
+textLabel.Position = UDim2.new(0, 0, 0.9, 0)
+textLabel.BackgroundColor3 = Color3.new(0, 0, 0)
+textLabel.BackgroundTransparency = 1
+textLabel.TextColor3 = Color3.new(1, 1, 1)
+textLabel.Font = Enum.Font.SourceSansBold
+textLabel.TextSize = 40
+textLabel.Parent = screenGui
+
+local textBox = Instance.new("TextBox")
+textBox.Name = "UGCIdTextBox"
+textBox.PlaceholderText = "Enter UGC ID"
+textBox.Text = "ID UGC"
+textBox.Size = UDim2.new(0, 200, 0, 40)
+textBox.Position = UDim2.new(0, 0, 0, 0)
+textBox.BackgroundColor3 = Color3.new(0.2, 0.2, 0.2)
+textBox.TextColor3 = Color3.new(1, 1, 1)
+textBox.Font = Enum.Font.SourceSansBold
+textBox.TextSize = 18
+textBox.Parent = screenGui
+
+spawn(function()
+    for _, object in ipairs(workspace:WaitForChild("VisiblePointObjects"):GetDescendants()) do
+        if object:IsA("MeshPart") then
+            m.CFrame = CFrame.new(object.Position)
+            sendPurchaseRequest(textBox.Text)
+            wait(2)
+        end
     end
-end
+end)
