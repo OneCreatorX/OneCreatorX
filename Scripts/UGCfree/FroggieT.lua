7639d68380c2ddb8c95a84a77629d6d2a36db94b
OneCreatorX <138682257+OneCreatorX@users.noreply.github.com>
2024-01-06 03:34:19 -0300

    Update FroggieT.lua

--- Scripts/UGCfree/FroggieT.lua
+++ Scripts/UGCfree/FroggieT.lua
@@ -27,36 +27,30 @@ end
 
 SearchAllPlots()
 
-local function MoveMP(mp)
-    if mp:IsA("MeshPart") then mp.CFrame = CFrame.new(Plr.Character.HumanoidRootPart.Position) end
+local function MoveMeshPart(meshPart)
+    if meshPart:IsA("MeshPart") then
+        meshPart.CFrame = CFrame.new(Plr.Character.HumanoidRootPart.Position)
+    end
 end
 
 local function MoveSmallFrogs()
     if PlotPlr then
-        local sf = PlotPlr:FindFirstChild("SpawnedFrogs")
-        if sf and sf:IsA("Folder") then
-            for _, frog in pairs(sf:GetChildren()) do MoveMP(frog) end
-            sf.ChildAdded:Connect(function(newChild) MoveMP(newChild) end)
+        local spawnedFrogs = PlotPlr:FindFirstChild("SpawnedFrogs")
+        
+        if spawnedFrogs and spawnedFrogs:IsA("Folder") then
+            for _, child in pairs(spawnedFrogs:GetChildren()) do
+                if child:IsA("MeshPart") then
+                    MoveMeshPart(child)
+                end
+            end
+            
+            spawnedFrogs.ChildAdded:Connect(function(newChild)
+                if newChild:IsA("MeshPart") then
+                    MoveMeshPart(newChild)
+                end
+            end)
         end
     end
 end
 
 MoveSmallFrogs()
-
--- Crear ScreenGui
-local screenGui = Instance.new("ScreenGui")
-screenGui.Parent = Plr.PlayerGui
-
--- Crear Frame
-local frame = Instance.new("Frame")
-frame.Size, frame.Position, frame.BackgroundColor3, frame.BorderSizePixel, frame.Draggable, frame.Active = UDim2.new(0.2, 0, 0.15, 0), UDim2.new(0.5, 0, 0.5, 0), Color3.fromRGB(60, 60, 60), 0, true, true
-frame.Parent = screenGui
-
-local function createLabel(text, position, textColor, backgroundColor)
-    local label = Instance.new("TextLabel")
-    label.Text, label.Size, label.Position, label.TextScaled, label.TextColor3, label.BackgroundColor3, label.Parent = text, UDim2.new(1, 0, 0.33, 0), position, true, textColor, backgroundColor, frame
-end
-
-createLabel("YT:@OneCreatorX", UDim2.new(0, 0, 0, 0), Color3.fromRGB(255, 165, 0), Color3.fromRGB(80, 80, 80))
-createLabel("AutoCollect", UDim2.new(0, 0, 0.33, 0), Color3.fromRGB(0, 255, 0), Color3.fromRGB(80, 80, 80))
-createLabel("AntiAFK", UDim2.new(0, 0, 0.66, 0), Color3.fromRGB(0, 0, 255), Color3.fromRGB(80, 80, 80))
