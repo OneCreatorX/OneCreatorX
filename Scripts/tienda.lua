5836a242acc854654f300b058d0e268592ff4e9b
OneCreatorX <138682257+OneCreatorX@users.noreply.github.com>
2024-01-07 14:28:26 -0300

    Update tienda.lua

--- Scripts/tienda.lua
+++ Scripts/tienda.lua
@@ -1,10 +1,10 @@
 local function createFrame(assetId, itemValue)
     local frame = Instance.new("Frame")
     frame.Size = UDim2.new(0, 150, 0, 160)
-    frame.BackgroundColor3 = Color3.fromRGB(200, 200, 200)
+    frame.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
     frame.BorderSizePixel = 2
     frame.BorderColor3 = Color3.fromRGB(100, 100, 100)
-    frame.BackgroundTransparency = 0.5
+    frame.BackgroundTransparency = 0.7
     frame.LayoutOrder = 1
 
     local imageLabel = Instance.new("ImageLabel")
@@ -12,19 +12,15 @@ local function createFrame(assetId, itemValue)
     imageLabel.Position = UDim2.new(0, 0, 0, 0)
     imageLabel.Image = "rbxthumb://type=Asset&id=" .. assetId .. "&w=150&h=150"
     imageLabel.Parent = frame
-    imageLabel.BackgroundTransparency = 1
+    imageLabel.BackgroundTransparency = 0.6
 
     local textLabel = Instance.new("TextLabel")
     textLabel.Size = UDim2.new(0, 150, 0, 20)
     textLabel.Position = UDim2.new(0, 0, 0, 150)
     textLabel.Text = "$" .. itemValue
     textLabel.TextScaled = true
-    textLabel.TextColor3 = Color3.fromRGB(50, 50, 50)
+    textLabel.TextColor3 = Color3.fromRGB(0, 128, 255)
     textLabel.Parent = frame
-
-    -- Agregar colores
-    frame.BackgroundColor3 = Color3.fromRGB(255, 255, 255)  -- Color de fondo del frame
-    textLabel.TextColor3 = Color3.fromRGB(0, 128, 255)  -- Color del texto
 
     local isTouching = false
     local touchStartPosition = nil
@@ -66,7 +62,7 @@ local screenGui = Instance.new("ScreenGui")
 screenGui.Parent = game.Players.LocalPlayer.PlayerGui
 
 local scrollingFrame = Instance.new("ScrollingFrame")
-scrollingFrame.Size = UDim2.new(0, 470, 0, 300)
+scrollingFrame.Size = UDim2.new(0, 470, 0, 338)
 scrollingFrame.Position = UDim2.new(0, 50, 0, 80)
 scrollingFrame.CanvasSize = UDim2.new(0, 0, 0, 0)
 scrollingFrame.Parent = screenGui
@@ -75,7 +71,7 @@ scrollingFrame.ScrollBarImageTransparency = 0.2
 
 local titleTextLabel = Instance.new("TextLabel")
 titleTextLabel.Size = UDim2.new(0.4, 0, 0, 30)
-titleTextLabel.Position = UDim2.new(0.09, 0, 0.08, 10)
+titleTextLabel.Position = UDim2.new(0.09, 0, 0.085, 10)
 titleTextLabel.Text = "YT:@OneCreatorX"
 titleTextLabel.TextScaled = true
 titleTextLabel.TextColor3 = Color3.fromRGB(50, 50, 50)
