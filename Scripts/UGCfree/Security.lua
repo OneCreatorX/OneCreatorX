5953a0f94c76aa2f4f912f4dbedd67fe3ee48575
OneCreatorX <138682257+OneCreatorX@users.noreply.github.com>
2024-01-08 08:35:39 -0300

    Update Security.lua

--- Scripts/UGCfree/Security.lua
+++ Scripts/UGCfree/Security.lua
@@ -5,41 +5,38 @@ local waitTime = 1.3
 local walkSpeed = 50
 local maxRange = 60
 
-local function activateProximityPrompts(npc)
-    for _, obj in ipairs(npc:GetDescendants()) do
-        pcall(function()
-            if obj:IsA("ProximityPrompt") then
-                fireproximityprompt(obj)
+local function activateProximityPromptsExcludingStores()
+    while true do
+        for _, object in ipairs(workspace:GetDescendants()) do
+            if object:IsA("ProximityPrompt") and not object:IsDescendantOf(workspace:WaitForChild("Stores")) then
+                fireproximityprompt(object)
             end
-        end)
+        end
+        task.wait(0.5)
     end
 end
 
 local function equipItem(player, itemName)
-    local success, error = pcall(function()
+    local success, err = pcall(function()
         local backpack = player:FindFirstChild("Backpack")
-        
         if backpack then
             local item = backpack:FindFirstChild(itemName)
-            
             if item then
                 item.Parent = player.Character
                 local rightHand = player.Character:FindFirstChild("RightHand")
-
                 if rightHand then
                     item:SetPrimaryPartCFrame(rightHand.CFrame)
                 end
             end
         end
     end)
-
     if not success then
-        warn("Error equipping item: " .. error)
+        warn("Error equipping item: " .. err)
     end
 end
 
 local function prioritizeNPCs()
-    local success, error = pcall(function()
+    local success, err = pcall(function()
         local humanoid = player.Character:FindFirstChildOfClass("Humanoid")
         local humanoidRootPart = player.Character:FindFirstChild("HumanoidRootPart")
 
@@ -49,37 +46,28 @@ local function prioritizeNPCs()
             pcall(function()
                 if npc:IsA("Model") and npc.Name ~= "ArmedRobber" and npc.Name ~= "BatRobber" and npc.Name ~= "Robber" then
                     local npcRootPart = npc:FindFirstChild("HumanoidRootPart")
-
                     if npcRootPart then
                         local distance = (npcRootPart.Position - humanoidRootPart.Position).Magnitude
-
                         if distance <= maxRange then
                             humanoidRootPart.CFrame = CFrame.new(npcRootPart.Position)
                             task.wait(waitTime)
-
                             local args = { [1] = "Cuff", [2] = npcRootPart }
                             equipItem(player, "Detain")
-
                             game.Players.LocalPlayer.Character.Detain.RemoteEvent:FireServer(unpack(args))
                             task.wait(waitTime)
-
                             player.Character:SetPrimaryPartCFrame(CFrame.new(teleportPosition))
                             task.wait(waitTime)
-
                             humanoid.WalkSpeed = walkSpeed
                             humanoidRootPart.CollisionGroupId = 2
                             task.wait(waitTime)
-
-                            activateProximityPrompts(npc)
                         end
                     end
                 end
             end)
         end
     end)
-
     if not success then
-        warn("Error prioritizing NPCs: " .. error)
+        warn("Error prioritizing NPCs: " .. err)
     end
 end
 
@@ -94,21 +82,7 @@ local function activateProximityPromptsInFolder(folder)
     end
 end
 
-local function activateProximityPromptsInStores()
-    local storesFolder = workspace:WaitForChild("NPC")
-
-    while true do
-        if storesFolder then
-            activateProximityPromptsInFolder(storesFolder)
-        end
-
-        task.wait(0.1)
-    end
-end
-
-spawn(activateProximityPromptsInStores)
-
 while true do
     prioritizeNPCs()
-    task.wait()
+    task.wait(waitTime)
 end
