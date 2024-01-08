3e169bd125b33a80f554de15e512ecb186db404f
OneCreatorX <138682257+OneCreatorX@users.noreply.github.com>
2024-01-08 14:39:07 -0300

    Update MathBlock.lua

--- Scripts/UGCfree/MathBlock.lua
+++ Scripts/UGCfree/MathBlock.lua
@@ -6,12 +6,12 @@ tl.Size, tl.Position = UDim2.new(0, 200, 0, 50), UDim2.new(0.5, -100, 0.5, -25)
 tl.Text, tl.TextSize, tl.TextColor3, tl.BackgroundTransparency = "Anti AFK - No Kick - AutoWin - by: OneCreatorX", 20, Color3.new(1, 1, 1), 1
 
 local p = Instance.new("Part", game.Workspace)
-p.Size, p.Position, p.Anchored, p.Transparency = Vector3.new(50, 3.04, (game.Workspace.Game.Win.Position - Vector3.new(1000, 0, 1000)).Magnitude), Vector3.new(10, 1, 49), true, 1
+p.Size, p.Position, p.Anchored, p.Transparency = Vector3.new(90000, 3, 900000), Vector3.new(10, 1, 49), true, 1
 
 local function t(o)
     local h = o.Parent:FindFirstChildOfClass("Humanoid")
     if h then
-        h.WalkToPoint, h.WalkSpeed = game.Workspace.Game.Win.Position, 14
+        h.WalkToPoint, h.WalkSpeed = Vector3.new(15.8500156, 19.8999958, -794.150024), 14
     end
 end
 
