eadd8e0fd6d04a6248d9624ceddcaf9d01e0ccef
OneCreatorX <138682257+OneCreatorX@users.noreply.github.com>
2024-02-09 03:21:51 -0300

    Update Mango.lua

--- Scripts/UGCfree/Mango.lua
+++ Scripts/UGCfree/Mango.lua
@@ -1,4 +1,4 @@
-local placeholder = game.Workspace.A_placeholder
+local placeholder = workspace.A_placeholder
 local clockTextLabel = placeholder:FindFirstChild("Clock", true)
 
 if clockTextLabel then
@@ -19,25 +19,16 @@ if clockTextLabel then
                     game.Players.LocalPlayer.Character:SetPrimaryPartCFrame(CFrame.new(Vector3.new(-240, 58, -346)))
                     task.wait(20)
                     local RFQuest = game:GetService("ReplicatedStorage"):FindFirstChild("RFQuest")
-                    if RFQuest then
-                        RFQuest:InvokeServer(tonumber(questID))
-                    else
-                        warn("No se encontró el Remote RFQuest.")
-                    end
+                    if RFQuest then RFQuest:InvokeServer(tonumber(questID)) end
                     task.wait(15)
-                        game.Players.LocalPlayer.Character:SetPrimaryPartCFrame(CFrame.new(Vector3.new(-239, 41, -494)))
-              
+                    game.Players.LocalPlayer.Character:SetPrimaryPartCFrame(CFrame.new(Vector3.new(-239, 41, -494)))
                 end
-            else
-                warn("No se encontró ImageLabel bajo A_placeholder.")
             end
         end
     end)
 else
     warn("No se encontró el Clock TextLabel como descendiente de A_placeholder.")
 end
-
-
 
 local player = game.Players.LocalPlayer
 local workspace = game.Workspace
@@ -47,14 +38,12 @@ local function movePlayerToCameraCFrame()
     game:GetService("ReplicatedStorage"):WaitForChild("TMReplicatedStorage"):WaitForChild("InvitaAmigos"):FireServer()
     
     local tmStarterGui = playerGui:WaitForChild("TMStarterGui", 5)
-    if not tmStarterGui then
-        return
-    end
+    if not tmStarterGui then return end
 
     local success, camera = pcall(function()
         return tmStarterGui:WaitForChild("Camera", 5)
     end)
-    
+
     if success and camera and camera:IsA("Camera") then
         local character = player.Character
         if character then
@@ -62,16 +51,15 @@ local function movePlayerToCameraCFrame()
             local cameraCFrame = camera.CFrame
 
             character:SetPrimaryPartCFrame(cameraCFrame)
-            task.wait(2)
+            wait(1.5)
 
             for _, part in pairs(workspace:GetDescendants()) do
                 if (part:IsA("BasePart") or part:IsA("MeshPart")) and part:FindFirstChild("ProximityPrompt") then
                     local proximity = part.ProximityPrompt
                     if proximity and (part.Position - character:GetPrimaryPartCFrame().Position).Magnitude < 10 then
                         fireproximityprompt(proximity)
-                        task.wait(1)
+                        wait(2)
                         game:GetService("ReplicatedStorage"):WaitForChild("FB-ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("ResetearAspecto"):FireServer()
-                        
                     end
                 end
             end
@@ -90,7 +78,6 @@ local function checkTaskTimer()
     for _, descendant in ipairs(descendants) do
         if descendant:IsA("TextLabel") and descendant.Name == "taskTimer" then
             descendant:GetPropertyChangedSignal("Text"):Connect(function()
-                   
                 if descendant.Text == "Next task in: 00:00" then
                     activateFunction()
                 end
