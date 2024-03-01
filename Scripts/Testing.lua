7746eec4650d90555c223ceba2834dae79d391e2
OneCreatorX <138682257+OneCreatorX@users.noreply.github.com>
2024-03-01 02:18:54 -0300

    Update Testing.lua

--- Scripts/Testing.lua
+++ Scripts/Testing.lua
@@ -1,33 +1,42 @@
 task.spawn(function()
-while true do
-    for i = 8, 15 do
-        local args = {
-            [1] = i,
-            [2] = "MakeMain"
-        }
-
-        game:GetService("ReplicatedStorage"):WaitForChild("GameCommon"):WaitForChild("Messages"):WaitForChild("Interaction"):WaitForChild("OperatePart"):FireServer(unpack(args))
-    end
-
-    for i = 30, 34 do
-        local args = {
-            [1] = i,
-            [2] = "TakeOrder"
-        }
-
-        game:GetService("ReplicatedStorage"):WaitForChild("GameCommon"):WaitForChild("Messages"):WaitForChild("Interaction"):WaitForChild("OperatePart"):FireServer(unpack(args))
-    end
-
-    for i = 1, 80 do
-        for j = 1, 15 do
+    while true do
+        for i = 8, 15 do
             local args = {
                 [1] = i,
-                [2] = j
+                [2] = "MakeMain"
             }
 
-            game:GetService("ReplicatedStorage"):WaitForChild("GameCommon"):WaitForChild("Messages"):WaitForChild("Interaction"):WaitForChild("FinishOrderRecipes"):FireServer(unpack(args))
+            game:GetService("ReplicatedStorage"):WaitForChild("GameCommon"):WaitForChild("Messages"):WaitForChild("Interaction"):WaitForChild("OperatePart"):FireServer(unpack(args))
         end
+
+        for i = 30, 34 do
+            local args = {
+                [1] = i,
+                [2] = "TakeOrder"
+            }
+
+            game:GetService("ReplicatedStorage"):WaitForChild("GameCommon"):WaitForChild("Messages"):WaitForChild("Interaction"):WaitForChild("OperatePart"):FireServer(unpack(args))
+        end
+
+        for i = 1, 100 do
+            for j = 1, 15 do
+                local args = {
+                    [1] = i,
+                    [2] = j
+                }
+
+                game:GetService("ReplicatedStorage"):WaitForChild("GameCommon"):WaitForChild("Messages"):WaitForChild("Interaction"):WaitForChild("FinishOrderRecipes"):FireServer(unpack(args))
+            for u = 30, 34 do
+            local args = {
+                [1] = u,
+                [2] = "TakeOrder"
+            }
+
+            game:GetService("ReplicatedStorage"):WaitForChild("GameCommon"):WaitForChild("Messages"):WaitForChild("Interaction"):WaitForChild("OperatePart"):FireServer(unpack(args))
+                    end
+                    
+                end
+        end
+        task.wait(4)
     end
-    task.wait(2)
-end
 end)
