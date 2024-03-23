10f1f642daa7a0c75ca038a659470490c4c3bc89
OneCreatorX <138682257+OneCreatorX@users.noreply.github.com>
2024-03-23 08:05:54 -0300

    Update RapidR.lua

--- Scripts/UGCfree/RapidR.lua
+++ Scripts/UGCfree/RapidR.lua
@@ -1,3 +1,21 @@
+local p = game.Players.LocalPlayer
+local g = Instance.new("ScreenGui")
+g.ResetOnSpawn = false
+g.Parent = p.PlayerGui
+
+local f = Instance.new("Frame", g)
+f.Size, f.Position, f.BackgroundColor3, f.BorderSizePixel, f.BorderColor3, f.Draggable, f.Active = UDim2.new(0, 150, 0, 60), UDim2.new(0.3, -100, 0.9, -80), Color3.fromRGB(32, 32, 32), 2, Color3.new(1, 1, 1), true, true
+
+local t = Instance.new("TextLabel", f)
+t.Size, t.Position, t.Text, t.TextSize, t.TextColor3, t.BackgroundColor3 = UDim2.new(1, 0, 0, 30), UDim2.new(0, 0, 0, 0), "YT:OneCreatorX", 12, Color3.fromRGB(255, 255, 255), Color3.fromRGB(46, 46, 46)
+
+local ti = Instance.new("TextLabel", f)
+ti.Size, ti.Position, ti.Text, ti.TextSize, ti.TextColor3, ti.BackgroundColor3 = UDim2.new(1, 0, 0, 30), UDim2.new(0, 0, 0.53, 0), "all tasks: ON", 10, Color3.fromRGB(255, 255, 255), Color3.fromRGB(46, 46, 46)
+
+game:GetService('Players').LocalPlayer.Idled:Connect(function()
+game:GetService('VirtualUser'):CaptureController()   game:GetService('VirtualUser'):ClickButton2(Vector2.new())
+end)
+
 local function onPartAdded(part)
     if part.Name == "Wipeout" or part.Name == "FloodEscape" or part.Name == "BombDrop" or part.Name == "Spleef" then
         wait(6)
