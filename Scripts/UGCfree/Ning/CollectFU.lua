f6a40195acdc0e764cfa81225a1c969ca6ea1c23
OneCreatorX <138682257+OneCreatorX@users.noreply.github.com>
2024-04-30 12:24:03 -0300

    Update CollectFU.lua

--- Scripts/UGCfree/Ning/CollectFU.lua
+++ Scripts/UGCfree/Ning/CollectFU.lua
@@ -1,11 +1,11 @@
 local Lib = loadstring(game:HttpGet("https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/wizard"))()
-local Win = Lib:NewWindow("Collect For UGC v7")
+local Win = Lib:NewWindow("Collect For UGC v8")
 local Sec = Win:NewSection("Options")
 local Sec2 = Win:NewSection("Credits: OneCreatorX")
 local Players = game:GetService("Players")
 local Player = Players.LocalPlayer
 local b = false
-local speed = 70
+local speed = 68
 
 local RS = game:GetService("RunService")
 local WS = game:GetService("Workspace")
@@ -22,7 +22,7 @@ local function moveHearts()
     local function collectHeart(heartPart)
         local HPos = Vector3.new(heartPart.Position.X, PPos.Y, heartPart.Position.Z)
         local dist = (HPos - PPos).magnitude
-        if dist < 14 then
+        if dist < 10 then
             heartPart.Transparency = 1
             heartPart.Position = PPos
         end
@@ -37,7 +37,7 @@ local function moveHearts()
             if H:IsA("MeshPart") and H.Transparency ~= 1 then
                 local HPos = Vector3.new(H.Position.X, PPos.Y, H.Position.Z)
                 local dist = (HPos - PPos).magnitude
-                if dist < 14 then
+                if dist < 10 then
                     collectHeart(H)
                 elseif dist < minDist then
                     minDist = dist
@@ -67,44 +67,8 @@ end
 
 function has()
     b = not b
-    if b then 
-        moveHearts()
-        for _, da in ipairs(game.Players.LocalPlayer.Character:GetChildren()) do
-            if da:IsA("BasePart") then 
-                da.CanCollide = false 
-            end
-        end
-    else
-        for _, da in ipairs(game.Players.LocalPlayer.Character:GetChildren()) do
-            if da:IsA("BasePart") then 
-                da.CanCollide = true
-            end
-        end
-    end
 end
 
-
-local AC = Instance.new("Part")
-AC.Size = Vector3.new(0, 0.1, 0)
-AC.Anchored = true
-AC.CanCollide = false
-AC.Shape = Enum.PartType.Cylinder
-AC.Transparency = 0.5
-AC.Color = Color3.new(0, 0, 0)
-AC.Parent = WS
-
-local CircleMesh = Instance.new("SpecialMesh")
-CircleMesh.MeshType = Enum.MeshType.Cylinder 
-CircleMesh.Scale = Vector3.new(25, 0.1, 25)
-CircleMesh.Parent = AC
-CircleMesh.TextureId = "rbxassetid://15189831814"
-
-local function updateAuraCircle()
-    if Player.Character and b then
-        local PPos = Player.Character.HumanoidRootPart.Position
-        AC.Position = Vector3.new(PPos.X, PPos.Y - Player.Character.HumanoidRootPart.Size.Y / 1, PPos.Z)
-    end
-end
 
 function copyd()
     copyToClipboard("https://discord.com/invite/23kFrRBSfD")
@@ -116,13 +80,38 @@ end
 
 workspace.Camera.FieldOfView = 100
 
-for _, f in ipairs(workspace.Map.Terrain:GetChildren()) do
-    if f.Name ~= "Main" then
+for _, f in ipairs(workspace.Map.Terrain:GetDescendants()) do
+    if f.Name ~= "Main" and f.Name ~= "islandTop" then
         f:Destroy()
     end
 end
 
-RS.Heartbeat:Connect(updateAuraCircle)
+local parent = workspace.Map.Terrain.Main
+workspace.Map.Terrain.Main.islandTop.Transparency = 1
+local AC = Instance.new("Part")
+AC.Size = Vector3.new(522.132, 34.9476, 611.233)
+AC.Anchored = true
+AC.CanCollide = false
+AC.Shape = Enum.PartType.Cylinder
+AC.Transparency = 0
+AC.Color = Color3.new(0, 0, 0)
+AC.Parent = parent
+AC.Position = Vector3.new(-14.8902, -7.86472, 13.5819)
+
+local skyID = "rbxassetid://15189831814"
+
+game.Lighting.Sky.SkyboxBk = skyID
+game.Lighting.Sky.SkyboxDn = skyID
+game.Lighting.Sky.SkyboxFt = skyID
+game.Lighting.Sky.SkyboxLf = skyID
+game.Lighting.Sky.SkyboxRt = skyID
+game.Lighting.Sky.SkyboxUp = skyID
+local CircleMesh = Instance.new("SpecialMesh")
+CircleMesh.MeshType = Enum.MeshType.Cylinder 
+CircleMesh.Scale = Vector3.new(522.132, 34.9476, 611.233)
+CircleMesh.Parent = AC
+CircleMesh.TextureId = skyID
+
 RS.Heartbeat:Connect(moveHearts)
 
 Sec:CreateToggle("Auto Hears", has)
@@ -133,7 +122,31 @@ game:GetService('Players').LocalPlayer.Idled:Connect(function()
 game:GetService('VirtualUser'):CaptureController()   game:GetService('VirtualUser'):ClickButton2(Vector2.new())
 end)
 
-Sec:CreateTextbox("Speed Auto Hears 75", function(value)
-        speed = value
-        
+Sec:CreateTextbox("Speed Auto Hears 70", function(value)
+             speed = value
 end)
+
+Sec:CreateTextbox("ID Texture", function(value)
+    skyID = "rbxassetid://" .. value
+    game.Lighting.Sky.SkyboxBk = skyID
+    game.Lighting.Sky.SkyboxDn = skyID
+    game.Lighting.Sky.SkyboxFt = skyID
+    game.Lighting.Sky.SkyboxLf = skyID
+    game.Lighting.Sky.SkyboxRt = skyID
+    game.Lighting.Sky.SkyboxUp = skyID
+    CircleMesh.TextureId = skyID
+end)
+ 
+local StarterGui = game:GetService("StarterGui")
+StarterGui:SetCore("SendNotification", {
+            Title = "No Use Character low",
+            Text = "No uses personaje bajito",
+            Duration = 10,
+        })
+
+local StarterGui = game:GetService("StarterGui")
+StarterGui:SetCore("SendNotification", {
+            Title = "better more high",
+            Text = "Mejor si es alto",
+            Duration = 10,
+        })
