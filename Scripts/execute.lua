c490f8088d17621a1ab2932c16be2ca549b94848
OneCreatorX <138682257+OneCreatorX@users.noreply.github.com>
2024-05-08 11:50:02 -0300

    Update execute.lua

--- Scripts/execute.lua
+++ Scripts/execute.lua
@@ -8,8 +8,8 @@ sg.Parent = playerGui
 
 local frame = Instance.new("Frame")
 frame.Name = "ControlFrame"
-frame.Size = UDim2.new(0, 250, 0, 130)
-frame.Position = UDim2.new(0.5, -125, 0.3, -60)
+frame.Size = UDim2.new(0, 200, 0, 50)
+frame.Position = UDim2.new(0.5, -100, 0.9, -50)
 frame.BackgroundColor3 = Color3.fromRGB(40, 20, 10)
 frame.BorderSizePixel = 1
 frame.Style = Enum.FrameStyle.DropShadow
@@ -19,7 +19,7 @@ frame.Draggable = true
 
 local title = Instance.new("TextLabel")
 title.Name = "Title"
-title.Text = "YT:@OneCreatorX"
+title.Text = "OneCreatorX"
 title.Size = UDim2.new(1, 0, 0, 30)
 title.Position = UDim2.new(0, 0, 0, 0)
 title.BackgroundColor3 = Color3.fromRGB(30, 30, 30)
@@ -30,23 +30,36 @@ title.Parent = frame
 
 local textbox = Instance.new("TextBox")
 textbox.Name = "ScriptTextBox"
-textbox.Size = UDim2.new(0.9, 0, 0, 30)
-textbox.Position = UDim2.new(0.05, 0, 0.4, 0)
-textbox.BackgroundColor3 = Color3.fromRGB(50, 50, 50)
+textbox.Size = UDim2.new(0.9, 0, 0.6, 0)
+textbox.Position = UDim2.new(0.05, 0, 0.3, 0) 
+textbox.BackgroundTransparency = 1 
 textbox.TextColor3 = Color3.fromRGB(255, 255, 255)
 textbox.Font = Enum.Font.SourceSans
 textbox.TextSize = 14
+textbox.TextWrapped = true 
+textbox.TextXAlignment = Enum.TextXAlignment.Left  
+textbox.TextYAlignment = Enum.TextYAlignment.Top 
 textbox.Text = "Script Here Execute"
 textbox.Parent = frame
 
+local function executeScript()
+    local success, errorMessage = pcall(loadstring(textbox.Text))
+    textbox.Text = "Ready Execute"
+    wait(1)
+      textbox.Text = "Script Here Execute"
+    if not success then
+        
+    end
+end
+
 textbox.FocusLost:Connect(function(enterPressed)
     if enterPressed then
-        local success, errorMessage = pcall(loadstring(textbox.Text))
-             textbox.Text = "Execute Ready"
-            wait(1)
-              textbox.Text = "Script Here Execute"
-        if not success then
-            
-        end
+        executeScript()
+    end
+end)
+
+textbox:GetPropertyChangedSignal("Text"):Connect(function()
+    if textbox:IsFocused() and textbox.Text:find("\n") then
+        executeScript()
     end
 end)
