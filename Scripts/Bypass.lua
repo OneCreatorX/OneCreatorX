8b9da17f7126c5efd383307442d89864023f9a2a
OneCreatorX <138682257+OneCreatorX@users.noreply.github.com>
2024-05-08 19:21:00 -0300

    Update Bypass.lua

--- Scripts/Bypass.lua
+++ Scripts/Bypass.lua
@@ -1,4 +1,5 @@
 task.wait(5)
+
 local coreGui = game:GetService("CoreGui")
 local fileList = coreGui:GetChildren()
 
@@ -11,69 +12,59 @@ local ultimoArchivo2 = fileList[#fileList - 1]
 
 local archivoMasLargo
 local archivoMasCorto
-if ultimoArchivo1 and ultimoArchivo2 then
-    if #ultimoArchivo1.Name > #ultimoArchivo2.Name then
-        archivoMasLargo = ultimoArchivo1
-        archivoMasCorto = ultimoArchivo2
-    else
-        archivoMasLargo = ultimoArchivo2
-        archivoMasCorto = ultimoArchivo1
+
+repeat
+    if ultimoArchivo1 and ultimoArchivo2 then
+        if #ultimoArchivo1.Name > #ultimoArchivo2.Name then
+            archivoMasLargo = ultimoArchivo1
+            archivoMasCorto = ultimoArchivo2
+        else
+            archivoMasLargo = ultimoArchivo2
+            archivoMasCorto = ultimoArchivo1
+        end
     end
-end
 
-function by()
-if archivoMasCorto.MainFrame.KeySection.Buttons.aKeyContainer.KeyBox and then 
-wait(1)
- archivoMasCorto.MainFrame.KeySection.Buttons.aKeyContainer.KeyBox.Text = "Try Bypass key."
-    wait(0.2)
-    archivoMasCorto.MainFrame.KeySection.Buttons.aKeyContainer.KeyBox.Text = "Try Bypass key.."
-    wait(0.2)
-    archivoMasCorto.MainFrame.KeySection.Buttons.aKeyContainer.KeyBox.Text = "Try Bypass key..."
-    wait(0.2)
-    archivoMasCorto.MainFrame.KeySection.Buttons.aKeyContainer.KeyBox.Text = "Try Bypass key."
-    wait(0.2)
-    archivoMasCorto.MainFrame.KeySection.Buttons.aKeyContainer.KeyBox.Text = "Try Bypass key.."
-    wait(0.2)
-    archivoMasCorto.MainFrame.KeySection.Buttons.aKeyContainer.KeyBox.Text = "Try Bypass key..."
-    wait(0.2)
-    archivoMasCorto.MainFrame.KeySection.Buttons.aKeyContainer.KeyBox.Text = "Try Bypass key."
-    wait(0.2)
-    archivoMasCorto.MainFrame.KeySection.Buttons.aKeyContainer.KeyBox.Text = "Try Bypass key.."
-    wait(0.2)
-    archivoMasCorto.MainFrame.KeySection.Buttons.aKeyContainer.KeyBox.Text = "Try Bypass key..."
-    wait(0.2)
-    archivoMasCorto.MainFrame.KeySection.Buttons.aKeyContainer.KeyBox.Text = "Try Bypass key."
-    wait(0.2)
-    archivoMasCorto.MainFrame.KeySection.Buttons.aKeyContainer.KeyBox.Text = "Try Bypass key.."
-    wait(0.2)
-    archivoMasCorto.MainFrame.KeySection.Buttons.aKeyContainer.KeyBox.Text = "Try Bypass key..."
-    wait(1)
-    archivoMasCorto.MainFrame.KeySection.Buttons.aKeyContainer.KeyBox.Text = "Ready By:OneCreatorX"
-    wait(2)
+    if not (archivoMasCorto.MainFrame and archivoMasCorto.MainFrame.KeySection and archivoMasCorto.MainFrame.KeySection.Buttons and archivoMasCorto.MainFrame.KeySection.Buttons.aKeyContainer and archivoMasCorto.MainFrame.KeySection.Buttons.aKeyContainer.KeyBox) then
+        task.wait(1)
+        ultimoArchivo1 = fileList[#fileList]
+        ultimoArchivo2 = fileList[#fileList - 1]
+    end
+until archivoMasCorto.MainFrame and archivoMasCorto.MainFrame.KeySection and archivoMasCorto.MainFrame.KeySection.Buttons and archivoMasCorto.MainFrame.KeySection.Buttons.aKeyContainer and archivoMasCorto.MainFrame.KeySection.Buttons.aKeyContainer.KeyBox
 
-    archivoMasCorto.Enabled = false
-else
-by()
-end
-end
-wait(2)
+local propertyName = "Text"
+local event = archivoMasCorto.MainFrame.KeySection.Buttons.aKeyContainer.KeyBox:GetPropertyChangedSignal(propertyName)
+event:Connect(function()
+    local newText = archivoMasCorto.MainFrame.KeySection.Buttons.aKeyContainer.KeyBox.Text
+    if newText:lower() == "bypass" then
+        wait(1)
+        archivoMasCorto.MainFrame.KeySection.Buttons.aKeyContainer.KeyBox.Text = "Try Bypass key."
+        for i = 1, 3 do
+            wait(0.2)
+            archivoMasCorto.MainFrame.KeySection.Buttons.aKeyContainer.KeyBox.Text = ("Try Bypass key%s"):format(string.rep(".", i))
+        end
+        wait(1)
+        archivoMasCorto.MainFrame.KeySection.Buttons.aKeyContainer.KeyBox.Text = "Ready By:OneCreatorX"
+        wait(2)
+        archivoMasCorto.Enabled = false
 
-local screenGui = Instance.new("ScreenGui")
-screenGui.Name = "ControlGui"
-screenGui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+        local screenGui = Instance.new("ScreenGui")
+        screenGui.Name = "ControlGui"
+        screenGui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
 
-local screenHeight = game:GetService("GuiService"):GetScreenResolution().Y
-local offset = screenHeight * 0.05
+        local screenHeight = game:GetService("GuiService"):GetScreenResolution().Y
+        local offset = screenHeight * 0.05
 
-if archivoMasLargo then
-    local imageButton = Instance.new("ImageButton")
-    imageButton.Image = "rbxassetid://15844306310"
-    imageButton.BackgroundColor3 = Color3.fromRGB(0, 0, 0) 
-    imageButton.Parent = screenGui
-    imageButton.Position = UDim2.new(0, 0, 0, offset)
-    imageButton.Size = UDim2.new(0, 30, 0, 30)  
-    
-    imageButton.MouseButton1Click:Connect(function()
-        archivoMasLargo.Enabled = not archivoMasLargo.Enabled
-    end)
-end
+        if archivoMasLargo then
+            local imageButton = Instance.new("ImageButton")
+            imageButton.Image = "rbxassetid://15844306310"
+            imageButton.BackgroundColor3 = Color3.fromRGB(0, 0, 0) 
+            imageButton.Parent = screenGui
+            imageButton.Position = UDim2.new(0, 0, 0, offset)
+            imageButton.Size = UDim2.new(0, 30, 0, 30)  
+
+            imageButton.MouseButton1Click:Connect(function()
+                archivoMasLargo.Enabled = not archivoMasLargo.Enabled
+            end)
+        end
+    end
+end)
