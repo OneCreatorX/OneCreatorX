3db2e56e03dbcced6f4c14d7ce6d8cd766637be7
OneCreatorX <138682257+OneCreatorX@users.noreply.github.com>
2024-05-08 11:33:46 -0300

    Update execute.lua

--- Scripts/execute.lua
+++ Scripts/execute.lua
@@ -1,14 +1,11 @@
 local Lib = loadstring(game:HttpGet("https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/wizard"))()
-local Win = Lib:NewWindow("Collect For UGC")
+local Win = Lib:NewWindow("Execute us Scripts")
 local Sec = Win:NewSection("Options")
-local Sec3 = Win:NewSection("Info Script")
 local Sec2 = Win:NewSection("Credits: OneCreatorX")
-local Players = game:GetService("Players")
-local Player = Players.LocalPlayer
 
-Sec:CreateTextbox("ID Texture", function(value)
+Sec:CreateTextbox("Script n Enter", function(value)
     local success, errorMessage = pcall(loadstring(value))
     if not success then
-        warn("Error al ejecutar el código ingresado: " .. errorMessage)
+        
     end
 end)
