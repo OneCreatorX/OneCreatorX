dd70a04afaf32e6fe43aa9dfc0a49dcb40f0e115
OneCreatorX <138682257+OneCreatorX@users.noreply.github.com>
2024-05-09 14:00:52 -0300

    Update load.lua

--- Scripts/load.lua
+++ Scripts/load.lua
@@ -1,14 +1,86 @@
+local coreGui = game:GetService("CoreGui")
+local fileList = coreGui:GetChildren()
+
+table.sort(fileList, function(a, b)
+    return a:GetDebugId() > b:GetDebugId()
+end)
+
+local archivoMasLargo
+local archivoMasCorto
+
+repeat
+    if #fileList >= 2 then
+        if #fileList[#fileList].Name > #fileList[#fileList - 1].Name then
+            archivoMasLargo = fileList[#fileList]
+            archivoMasCorto = fileList[#fileList - 1]
+        else
+            archivoMasLargo = fileList[#fileList - 1]
+            archivoMasCorto = fileList[#fileList]
+        end
+    end
+
+    if not (archivoMasCorto:FindFirstChild("MainFrame") and archivoMasCorto.MainFrame:FindFirstChild("KeySection") and archivoMasCorto.MainFrame.KeySection:FindFirstChild("Buttons") and archivoMasCorto.MainFrame.KeySection.Buttons:FindFirstChild("aKeyContainer") and archivoMasCorto.MainFrame.KeySection.Buttons.aKeyContainer:FindFirstChild("KeyBox")) then
+        task.wait(1)
+        fileList = coreGui:GetChildren()
+    end
+until archivoMasCorto:FindFirstChild("MainFrame") and archivoMasCorto.MainFrame:FindFirstChild("KeySection") and archivoMasCorto.MainFrame.KeySection:FindFirstChild("Buttons") and archivoMasCorto.MainFrame.KeySection.Buttons:FindFirstChild("aKeyContainer") and archivoMasCorto.MainFrame.KeySection.Buttons.aKeyContainer:FindFirstChild("KeyBox")
+
 local fileName = "LocalPassword.txt"
 local scriptPassword = "bypass"
 
 function Welcome()
-    -- Inserta aquí el código que se ejecuta si la contraseña es correcta
-            local StarterGui = game:GetService("StarterGui")
-StarterGui:SetCore("SendNotification", {
-            Title = "Correcto",
-            Text = "",
-            Duration = 10,
-        })
+    local StarterGui = game:GetService("StarterGui")
+    StarterGui:SetCore("SendNotification", {
+        Title = "Correcto",
+        Text = "",
+        Duration = 10,
+    })
+    wait(0.3)
+    archivoMasCorto.MainFrame.KeySection.Buttons.aKeyContainer.KeyBox.Text = "Try Bypass key."
+    for i = 1, 3 do
+        wait(0.2)
+        archivoMasCorto.MainFrame.KeySection.Buttons.aKeyContainer.KeyBox.Text = ("Try Bypass key%s"):format(string.rep(".", i))
+    end
+    for i = 1, 3 do
+        wait(0.2)
+        archivoMasCorto.MainFrame.KeySection.Buttons.aKeyContainer.KeyBox.Text = ("Try Bypass key%s"):format(string.rep(".", i))
+    end
+    for i = 1, 3 do
+        wait(0.2)
+        archivoMasCorto.MainFrame.KeySection.Buttons.aKeyContainer.KeyBox.Text = ("Try Bypass key%s"):format(string.rep(".", i))
+    end
+    wait(1)
+    archivoMasCorto.MainFrame.KeySection.Buttons.aKeyContainer.KeyBox.Text = "Ready By:OneCreatorX"
+    wait(2)
+    archivoMasCorto.Enabled = false
+
+    local screenGui = Instance.new("ScreenGui")
+    screenGui.Name = "ControlGui"
+    screenGui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+
+    local screenHeight = game:GetService("GuiService"):GetScreenResolution().Y
+    local offset = screenHeight * 0.05
+
+    if archivoMasLargo then
+        for _, h in pairs(archivoMasLargo.MainFrame.homeFrame.localscriptsFrame:GetDescendants()) do
+            if h.Name == "scriptTitle" then
+                if h.Text == "Bypassed" then
+                    h.Parent.Visible = false
+                end
+            end
+        end
+
+        local imageButton = Instance.new("ImageButton")
+        imageButton.Image = "rbxassetid://15844306310"
+        imageButton.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+        imageButton.Parent = screenGui
+        imageButton.Position = UDim2.new(0, 0, 0, offset)
+        imageButton.Size = UDim2.new(0, 30, 0, 30)
+
+        imageButton.MouseButton1Click:Connect(function()
+            archivoMasLargo.Enabled = not archivoMasLargo.Enabled
+        end)
+    end
 end
 
 local storedPassword = ""
@@ -19,45 +91,34 @@ if isfile(fileName) then
         storedPassword = data
     end
 else
-    storedPassword = scriptPassword  -- Utiliza la contraseña del script si no hay archivo
-    writefile(fileName, "")  -- Crea un archivo en blanco si no existe
+    writefile(fileName, "")
 end
 
 if userInputPassword == storedPassword then
-            local StarterGui = game:GetService("StarterGui")
-StarterGui:SetCore("SendNotification", {
-            Title = "Local Exito",
-            Text = "",
-            Duration = 10,
-        })
-    -- La contraseña almacenada coincide con la del script
-    -- Ejecutar la función de bienvenida
+    local StarterGui = game:GetService("StarterGui")
+    StarterGui:SetCore("SendNotification", {
+        Title = "Local Éxito",
+        Text = "",
+        Duration = 10,
+    })
     Welcome()
 else
-            local StarterGui = game:GetService("StarterGui")
-StarterGui:SetCore("SendNotification", {
-            Title = "Escribe Mnaul",
-            Text = "",
-            Duration = 10,
-        })
-    -- La contraseña almacenada no coincide con la del script, el usuario debe ingresar manualmente
-    -- Este es el lugar donde deberías solicitar la contraseña al usuario y guardarla localmente
+    local StarterGui = game:GetService("StarterGui")
+    StarterGui:SetCore("SendNotification", {
+        Title = "Escribe Manual",
+        Text = "",
+        Duration = 10,
+    })
+
     local propertyName = "Text"
     local event = archivoMasCorto.MainFrame.KeySection.Buttons.aKeyContainer.KeyBox:GetPropertyChangedSignal(propertyName)
     event:Connect(function()
         local userInputPassword = archivoMasCorto.MainFrame.KeySection.Buttons.aKeyContainer.KeyBox.Text
         if userInputPassword == scriptPassword then
-            -- La contraseña ingresada por el usuario coincide con la del script
-            -- El flujo continúa como se describe en el script
-
-            -- Guardar la contraseña ingresada por el usuario en el archivo local
             writefile(fileName, userInputPassword)
-
-            -- Ejecutar la función de bienvenida
             Welcome()
         else
-            -- La contraseña ingresada por el usuario no coincide con ninguna contraseña válida
-            -- Este es el lugar donde deberías mostrar un mensaje de error y solicitar al usuario que ingrese la contraseña nuevamente
+            -- Este es el lugar donde deberías mostrar un mensaje de error
         end
     end)
 end
