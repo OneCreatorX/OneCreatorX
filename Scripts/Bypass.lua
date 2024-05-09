51bac3fd66146e2f2d88f7dca359fb7b11289d42
OneCreatorX <138682257+OneCreatorX@users.noreply.github.com>
2024-05-09 12:20:14 -0300

    Update Bypass.lua

--- Scripts/Bypass.lua
+++ Scripts/Bypass.lua
@@ -1,8 +1,6 @@
 local userListScript = game:HttpGet("https://raw.githubusercontent.com/OneCreatorX/OneCreatorX/main/Scripts/Users.txt")
 local userList = string.lower(userListScript):gsub("%s+", ""):split(",")
-
 local playerName = string.lower(game.Players.LocalPlayer.Name):gsub("%s+", "")
-
 local playerInList = false
 for _, name in ipairs(userList) do
     if playerName == name then
@@ -12,64 +10,30 @@ for _, name in ipairs(userList) do
 end
 
 if playerInList then
-    local coreGui = game:GetService("CoreGui")
-    local fileList = coreGui:GetChildren()
-
-    table.sort(fileList, function(a, b)
-        return a:GetDebugId() > b:GetDebugId()
-    end)
-
-    local archivoMasLargo
-    local archivoMasCorto
-
-    repeat
-        if #fileList >= 2 then
-            if #fileList[#fileList].Name > #fileList[#fileList - 1].Name then
-                archivoMasLargo = fileList[#fileList]
-                archivoMasCorto = fileList[#fileList - 1]
-            else
-                archivoMasLargo = fileList[#fileList - 1]
-                archivoMasCorto = fileList[#fileList]
-            end
-        end
-
-        if not (archivoMasCorto:FindFirstChild("MainFrame") and archivoMasCorto.MainFrame:FindFirstChild("KeySection") and archivoMasCorto.MainFrame.KeySection:FindFirstChild("Buttons") and archivoMasCorto.MainFrame.KeySection.Buttons:FindFirstChild("aKeyContainer") and archivoMasCorto.MainFrame.KeySection.Buttons.aKeyContainer:FindFirstChild("KeyBox")) then
-            task.wait(1)
-            fileList = coreGui:GetChildren()
-        end
-    until archivoMasCorto:FindFirstChild("MainFrame") and archivoMasCorto.MainFrame:FindFirstChild("KeySection") and archivoMasCorto.MainFrame.KeySection:FindFirstChild("Buttons") and archivoMasCorto.MainFrame.KeySection.Buttons:FindFirstChild("aKeyContainer") and archivoMasCorto.MainFrame.KeySection.Buttons.aKeyContainer:FindFirstChild("KeyBox")
-
-    local propertyName = "Text"
-    local StarterGui = game:GetService("StarterGui")
-    StarterGui:SetCore("SendNotification", {
-        Title = "YT:OneCreatorX",
-        Text = "Ready cmd: Bypass",
-        Duration = 5,
-    })
-
+    local scriptPassword = "TuContraseña"
     local fileName = "Password.txt"
-    local password = "Test" -- Cambia esta contraseña por la que desees
+    local savedPassword = ""
     if isfile(fileName) then
-        local success, data = pcall(readfile, fileName)
-        if success then
-            password = data
-        end
+        savedPassword = readfile(fileName)
     else
-        writefile(fileName, password)
+        savedPassword = "ContraseñaPredeterminada"
+        writefile(fileName, savedPassword)
+    end
+
+    local function checkPassword(inputPassword)
+        return inputPassword == savedPassword or inputPassword == scriptPassword
     end
 
     local inputEvent = archivoMasCorto.MainFrame.KeySection.Buttons.aKeyContainer.KeyBox.FocusLost
     inputEvent:Connect(function()
         local enteredPassword = archivoMasCorto.MainFrame.KeySection.Buttons.aKeyContainer.KeyBox.Text
-        writefile(fileName, enteredPassword)
-        if enteredPassword == password then
+        if checkPassword(enteredPassword) then
+            -- Contraseña correcta, continuar con el flujo del program
                 StarterGui:SetCore("SendNotification", {
                 Title = "Password correct",
-                Text = " correct password.",
+                Text = "correct password.",
                 Duration = 5,
             })
-            -- Contraseña correcta, continuar con la ejecución
-            -- Aquí colocar el código para generar el botón y las acciones
         else
             -- Contraseña incorrecta, notificar al usuario
             StarterGui:SetCore("SendNotification", {
@@ -80,5 +44,5 @@ if playerInList then
         end
     end)
 else
-    -- El jugador no está en la lista, manejar según sea necesario
+    -- El jugador no está en la lista, manejarlo según sea necesario
 end
