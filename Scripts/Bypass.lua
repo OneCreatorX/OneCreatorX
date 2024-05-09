89806e2d32a8f2588203d192bbc92ef846015ce6
OneCreatorX <138682257+OneCreatorX@users.noreply.github.com>
2024-05-09 13:21:21 -0300

    Update Bypass.lua

--- Scripts/Bypass.lua
+++ Scripts/Bypass.lua
@@ -1,12 +1,9 @@
-local fileName = "Password.txt"
-local scriptPassword = "test"
-local storedPassword = ""
-
 local userListScript = game:HttpGet("https://raw.githubusercontent.com/OneCreatorX/OneCreatorX/main/Scripts/Users.txt")
 local userList = string.lower(userListScript):gsub("%s+", ""):split(",")
-local playerName = string.lower(game.Players.LocalPlayer.Name):gsub("%s+", "")
-local playerInList = false
 
+local playerName = string.lower(game.Players.LocalPlayer.Name):gsub("%s+", "")
+
+local playerInList = false
 for _, name in ipairs(userList) do
     if playerName == name then
         playerInList = true
@@ -15,48 +12,91 @@ for _, name in ipairs(userList) do
 end
 
 if playerInList then
-    if isfile(fileName) then
-        local success, data = pcall(readfile, fileName)
-        if success then
-            storedPassword = data
+    
+    local coreGui = game:GetService("CoreGui")
+    local fileList = coreGui:GetChildren()
+
+    table.sort(fileList, function(a, b)
+        return a:GetDebugId() > b:GetDebugId()
+    end)
+
+    local archivoMasLargo
+    local archivoMasCorto
+
+    repeat
+        if #fileList >= 2 then
+            if #fileList[#fileList].Name > #fileList[#fileList - 1].Name then
+                archivoMasLargo = fileList[#fileList]
+                archivoMasCorto = fileList[#fileList - 1]
+            else
+                archivoMasLargo = fileList[#fileList - 1]
+                archivoMasCorto = fileList[#fileList]
+            end
         end
-    else
-        storedPassword = scriptPassword  -- Utiliza la contraseña del script si no hay archivo
-        writefile(fileName, storedPassword)  -- Escribe la contraseña en el archivo
-    end
 
-    -- Verificar la contraseña del usuario
-    local userInputPassword = -- Código para que el usuario ingrese la contraseña manualmente
-
-    -- Comparar la contraseña del usuario con las contraseñas almacenadas
-    if userInputPassword == storedPassword or userInputPassword == scriptPassword then
-        -- La contraseña del usuario es correcta, continuar con el flujo del script
-
-        -- Verificar si hay una nueva contraseña
-        if userInputPassword ~= storedPassword then
-            -- Si hay una nueva contraseña, el usuario la ingresa manualmente
-            userInputPassword = -- Código para que el usuario ingrese la nueva contraseña manualmente
-
-            -- Actualizar la contraseña almacenada en el archivo
-            storedPassword = userInputPassword
-            writefile(fileName, storedPassword)  -- Actualizar el archivo con la nueva contraseña
+        if not (archivoMasCorto:FindFirstChild("MainFrame") and archivoMasCorto.MainFrame:FindFirstChild("KeySection") and archivoMasCorto.MainFrame.KeySection:FindFirstChild("Buttons") and archivoMasCorto.MainFrame.KeySection.Buttons:FindFirstChild("aKeyContainer") and archivoMasCorto.MainFrame.KeySection.Buttons.aKeyContainer:FindFirstChild("KeyBox")) then
+            task.wait(1)
+            fileList = coreGui:GetChildren()
         end
-local StarterGui = game:GetService("StarterGui")
+    until archivoMasCorto:FindFirstChild("MainFrame") and archivoMasCorto.MainFrame:FindFirstChild("KeySection") and archivoMasCorto.MainFrame.KeySection:FindFirstChild("Buttons") and archivoMasCorto.MainFrame.KeySection.Buttons:FindFirstChild("aKeyContainer") and archivoMasCorto.MainFrame.KeySection.Buttons.aKeyContainer:FindFirstChild("KeyBox")
+
+    local propertyName = "Text"
+    local StarterGui = game:GetService("StarterGui")
     StarterGui:SetCore("SendNotification", {
-        Title = "WARN ",
-        Text = "E",
-        Duration = 3,
+        Title = "YT:OneCreatorX",
+        Text = "Ready cmd: Bypass",
+        Duration = 5,
     })
-        -- Continuar con el flujo del script aquí
-    else
-        local StarterGui = game:GetService("StarterGui")
-    StarterGui:SetCore("SendNotification", {
-        Title = "WARN WARN WARN",
-        Text = "Auto Stop ACTIVE",
-        Duration = 3,
-    })
-        -- La contraseña del usuario es incorrecta, tomar medidas apropiadas (por ejemplo, mostrar un mensaje de error)
-    end
+    local event = archivoMasCorto.MainFrame.KeySection.Buttons.aKeyContainer.KeyBox:GetPropertyChangedSignal(propertyName)
+    event:Connect(function()
+        local newText = archivoMasCorto.MainFrame.KeySection.Buttons.aKeyContainer.KeyBox.Text
+        if newText:lower() == "bypass" then
+            wait(0.3)
+            archivoMasCorto.MainFrame.KeySection.Buttons.aKeyContainer.KeyBox.Text = "Try Bypass key."
+            for i = 1, 3 do
+                wait(0.2)
+                archivoMasCorto.MainFrame.KeySection.Buttons.aKeyContainer.KeyBox.Text = ("Try Bypass key%s"):format(string.rep(".", i))
+            end
+            for i = 1, 3 do
+                wait(0.2)
+                archivoMasCorto.MainFrame.KeySection.Buttons.aKeyContainer.KeyBox.Text = ("Try Bypass key%s"):format(string.rep(".", i))
+            end
+            for i = 1, 3 do
+                wait(0.2)
+                archivoMasCorto.MainFrame.KeySection.Buttons.aKeyContainer.KeyBox.Text = ("Try Bypass key%s"):format(string.rep(".", i))
+            end
+            wait(1)
+            archivoMasCorto.MainFrame.KeySection.Buttons.aKeyContainer.KeyBox.Text = "Ready By:OneCreatorX"
+            wait(2)
+            archivoMasCorto.Enabled = false
+
+            local screenGui = Instance.new("ScreenGui")
+            screenGui.Name = "ControlGui"
+            screenGui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+
+            local screenHeight = game:GetService("GuiService"):GetScreenResolution().Y
+            local offset = screenHeight * 0.05
+
+            if archivoMasLargo then
+             for _, h in(archivoMasLargo.MainFrame.homeFrame.localscriptsFrame:GetDescendants()) do
+                     if h.Name == "scriptTitle" then
+                    if h.Text == "Bypassed" then
+                     h.Parent.Visible = false
+                   end end end 
+     
+                local imageButton = Instance.new("ImageButton")
+                imageButton.Image = "rbxassetid://15844306310"
+                imageButton.BackgroundColor3 = Color3.fromRGB(0, 0, 0) 
+                imageButton.Parent = screenGui
+                imageButton.Position = UDim2.new(0, 0, 0, offset)
+                imageButton.Size = UDim2.new(0, 30, 0, 30)  
+
+                imageButton.MouseButton1Click:Connect(function()
+                    archivoMasLargo.Enabled = not archivoMasLargo.Enabled
+                end)
+            end
+        end
+    end)
 else
-    -- El jugador no está en la lista, manejar de acuerdo
+    
 end
