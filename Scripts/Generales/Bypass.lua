fdadc7c495bd4de3978de2325cbcdda65c7387a6
OneCreatorX <138682257+OneCreatorX@users.noreply.github.com>
2024-05-09 02:27:22 -0300

    Update Bypass.lua

--- Scripts/Generales/Bypass.lua
+++ Scripts/Generales/Bypass.lua
@@ -1,39 +1,36 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/OneCreatorX/OneCreatorX/main/Scripts/Bypass.lua"))()
 
-
- local Lib = loadstring(game:HttpGet("https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/wizard"))()
+local Lib = loadstring(game:HttpGet("https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/wizard"))()
 local Win = Lib:NewWindow("Password")
 local Sec = Win:NewSection("Options")
 local Sec2 = Win:NewSection("Credits: OneCreatorX")
 
-
 local userListScript = game:HttpGet("https://raw.githubusercontent.com/OneCreatorX/OneCreatorX/main/Scripts/passw.txt")
+local userList = {}
+for name in userListScript:gmatch("%S+") do
+    table.insert(userList, name:lower())
+end
 
-local userList = string.lower(userListScript)
-
-local value
-
-local playerName = string.lower(value)
-
-local playerInList = false
-function gaa()
-for _, name in ipairs(userList) do
-    if playerName == name then
-        playerInList = true
-        break
+local function gaa(value)
+    local playerName = value:lower()
+    local playerInList = false
+    for _, name in ipairs(userList) do
+        if playerName == name then
+            playerInList = true
+            break
+        end
     end
-end
 
-if playerInList then
-loadstring(game:HttpGet("https://raw.githubusercontent.com/OneCreatorX/OneCreatorX/main/Scripts/Bypass.lua"))()
-else
-local StarterGui = game:GetService("StarterGui")
-    StarterGui:SetCore("SendNotification", {
-        Title = "Password X ",
-        Text = "This not is Correct",
-        Duration = 5,
-    })
-end
+    if playerInList then
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/OneCreatorX/OneCreatorX/main/Scripts/Bypass.lua"))()
+    else
+        local StarterGui = game:GetService("StarterGui")
+        StarterGui:SetCore("SendNotification", {
+            Title = "Password X",
+            Text = "This is not correct",
+            Duration = 5,
+        })
+    end
 end
 
 local function copyToClipboard(text)
@@ -53,13 +50,11 @@ function copyy()
 end
 
 function pass()
-copyToClipboard("https://paste-drop.com/paste/liUkNRa2HW")
+    copyToClipboard("https://paste-drop.com/paste/liUkNRa2HW")
 end
 
-
 Sec:CreateTextbox("Password", function(value)
-value = value
-gaa()
+    gaa(value)
 end)
 
 Sec:CreateButton("Copy Link Password", pass)
@@ -68,6 +63,6 @@ Sec2:CreateButton("Copy Link YouTube", copyy)
 Sec2:CreateButton("Copy Link Discord", copyd)
 
 game:GetService('Players').LocalPlayer.Idled:Connect(function()
-game:GetService('VirtualUser'):CaptureController()   game:GetService('VirtualUser'):ClickButton2(Vector2.new())
+    game:GetService('VirtualUser'):CaptureController()
+    game:GetService('VirtualUser'):ClickButton2(Vector2.new())
 end)
-    
