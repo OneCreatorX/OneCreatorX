dbbc8e72133aa1df8ca0a81df840a14b68221cf8
OneCreatorX <138682257+OneCreatorX@users.noreply.github.com>
2024-05-09 08:58:24 -0300

    Update Bypass.lua

--- Scripts/Generales/Bypass.lua
+++ Scripts/Generales/Bypass.lua
@@ -19,7 +19,7 @@ end
 
 local frame = Instance.new("Frame")
 frame.Name = "ControlFrame"
-frame.Size = UDim2.new(0, 250, 0, 80)
+frame.Size = UDim2.new(0, 250, 0, 130)
 frame.Position = UDim2.new(0.5, -125, 0.3, -60)
 frame.BackgroundColor3 = Color3.fromRGB(40, 20, 10)
 frame.BorderSizePixel = 1
@@ -39,26 +39,37 @@ title.Font = Enum.Font.GothamBold
 title.TextSize = 20
 title.Parent = frame
 
-local button = Instance.new("TextButton")
-button.Name = "PasswordButton"
-button.Text = "Enter Password"
-button.Size = UDim2.new(0.8, 0, 0, 30)
-button.Position = UDim2.new(0.1, 0, 0.5, -15)
-button.BackgroundColor3 = Color3.fromRGB(60, 60, 60)
-button.TextColor3 = Color3.fromRGB(255, 255, 255)
-button.Font = Enum.Font.GothamBold
-button.TextSize = 16
-button.Parent = frame
-
 local passwordTextbox = Instance.new("TextBox")
 passwordTextbox.Name = "PasswordTextbox"
 passwordTextbox.PlaceholderText = "Enter Password"
 passwordTextbox.Size = UDim2.new(0.8, 0, 0, 30)
-passwordTextbox.Position = UDim2.new(0.1, 0, 0.5, -15)
+passwordTextbox.Position = UDim2.new(0.1, 0, 0.1, 0)
 passwordTextbox.BackgroundTransparency = 1
 passwordTextbox.Font = Enum.Font.GothamBold
 passwordTextbox.TextSize = 16
 passwordTextbox.Parent = frame
+
+local buttonEnter = Instance.new("TextButton")
+buttonEnter.Name = "EnterButton"
+buttonEnter.Text = "Enter Password"
+buttonEnter.Size = UDim2.new(0.8, 0, 0, 30)
+buttonEnter.Position = UDim2.new(0.1, 0, 0.3, 0)
+buttonEnter.BackgroundColor3 = Color3.fromRGB(60, 60, 60)
+buttonEnter.TextColor3 = Color3.fromRGB(255, 255, 255)
+buttonEnter.Font = Enum.Font.GothamBold
+buttonEnter.TextSize = 16
+buttonEnter.Parent = frame
+
+local buttonCopy = Instance.new("TextButton")
+buttonCopy.Name = "CopyButton"
+buttonCopy.Text = "Copy Link"
+buttonCopy.Size = UDim2.new(0.8, 0, 0, 30)
+buttonCopy.Position = UDim2.new(0.1, 0, 0.5, 0)
+buttonCopy.BackgroundColor3 = Color3.fromRGB(60, 60, 60)
+buttonCopy.TextColor3 = Color3.fromRGB(255, 255, 255)
+buttonCopy.Font = Enum.Font.GothamBold
+buttonCopy.TextSize = 16
+buttonCopy.Parent = frame
 
 local notification = Instance.new("TextLabel")
 notification.Name = "Notification"
@@ -97,6 +108,15 @@ local function gaa(password)
     notification.Visible = true
 end
 
-button.MouseButton1Click:Connect(function()
+buttonEnter.MouseButton1Click:Connect(function()
     gaa(passwordTextbox.Text)
+end)
+
+buttonCopy.MouseButton1Click:Connect(function()
+    local passwordLink = "https://paste-drop.com/paste/liUkNRa2HW"
+    if syn then
+        syn.write_clipboard(passwordLink)
+    else
+        setclipboard(passwordLink)
+    end
 end)
