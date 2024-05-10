3eda3f810269560b70eed2d1b804bab7c2dca481
OneCreatorX <138682257+OneCreatorX@users.noreply.github.com>
2024-05-10 18:16:25 -0300

    Update NetflixN.lua

--- Scripts/UGCfree/Ning/NetflixN.lua
+++ Scripts/UGCfree/Ning/NetflixN.lua
@@ -1,5 +1,5 @@
 local Lib = loadstring(game:HttpGet("https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/wizard"))()
-local Win = Lib:NewWindow("Netflix NextWorld v0.1")
+local Win = Lib:NewWindow("Netflix NextWorld v0.2")
 local Sec = Win:NewSection("Opcions")
 local Sec2 = Win:NewSection("Credits: OneCreatorX")
 local Players = game:GetService("Players")
@@ -7,40 +7,67 @@ local Player = Players.LocalPlayer
 local a = false
 
 function Tk()
-a = not a
-while a do
-wait(0.1)
-local enemies = workspace:WaitForChild("ScriptableObjects"):WaitForChild("Enemies"):GetDescendants()
-local args = {[2] = 1}
-
-for _, enemy in ipairs(enemies) do
-    if enemy:IsA("Model") and enemy.PrimaryPart then
-        local args = {[1] = enemy, [2] = 1}
-        game:GetService("ReplicatedStorage"):WaitForChild("RemoteEvents"):WaitForChild("FireSlingshot"):FireServer(unpack(args))
-    end
-end
-end
-end
-
-local a = false
-function collect()
-a = not a
-while a do
-wait(0.5)
-local player = game.Players.LocalPlayer
-local targetPosition = player.Character.HumanoidRootPart.Position
-
-for _, drop in ipairs(workspace._DROPS:GetChildren()) do
-    if drop:IsA("Model") and drop.PrimaryPart then
-        drop:SetPrimaryPartCFrame(CFrame.new(targetPosition))
-        local proximityPrompt = drop.PrimaryPart:FindFirstChildOfClass("ProximityPrompt")
-        if proximityPrompt then
-            fireproximityprompt(proximityPrompt)
+    a = not a
+    while a do
+        wait(0.1)
+        local enemies = workspace:WaitForChild("ScriptableObjects"):WaitForChild("Enemies"):GetDescendants()
+        local args = {[2] = 1}
+        
+        for _, enemy in ipairs(enemies) do
+            if enemy:IsA("Model") and enemy.PrimaryPart then
+                local args = {[1] = enemy, [2] = 1}
+                game:GetService("ReplicatedStorage"):WaitForChild("RemoteEvents"):WaitForChild("FireSlingshot"):FireServer(unpack(args))
+            end
         end
     end
 end
+
+function collect()
+    a = not a
+    while a do
+        wait(0.5)
+        local player = game.Players.LocalPlayer
+        local targetPosition = player.Character.HumanoidRootPart.Position
+
+        for _, drop in ipairs(workspace._DROPS:GetChildren()) do
+            if drop:IsA("Model") and drop.PrimaryPart then
+                drop:SetPrimaryPartCFrame(CFrame.new(targetPosition))
+                local proximityPrompt = drop.PrimaryPart:FindFirstChildOfClass("ProximityPrompt")
+                if proximityPrompt then
+                    fireproximityprompt(proximityPrompt)
+                end
+            end
+        end
+    end
 end
+
+local a = false
+
+function npcs()
+    a = not a
+    local cerca = true
+    while a do
+        task.wait(0.5)
+        local player = game.Players.LocalPlayer
+
+        for _, npc in ipairs(workspace.ScriptableObjects.NPCs:GetChildren()) do
+            if npc:IsA("Model") and npc:FindFirstChild("HumanoidRootPart") then
+                local proximityPrompt = npc.PrimaryPart:FindFirstChildOfClass("ProximityPrompt")
+                if proximityPrompt then
+                    local distance = (player.Character.HumanoidRootPart.Position - npc.PrimaryPart.Position).magnitude
+                    if distance < 5 and cerca then
+                        fireproximityprompt(proximityPrompt)
+                        cerca = false
+                    elseif distance > 5 and distance < 30 and not cerca then
+                        cerca = true
+                    end
+                end
+            end
+        end
+    end
 end
+
+
 
 local function copyToClipboard(text)
     if syn then
@@ -58,16 +85,14 @@ function copyy()
     copyToClipboard("https://youtube.com/@OneCreatorX")
 end
 
-
-
 Sec:CreateToggle("Kill Mobs", Tk)
 Sec:CreateToggle("Auto Collect ", collect)
+Sec:CreateToggle("Auto Npcs ", npcs)
 
 Sec2:CreateButton("Copy Link YouTube", copyy)
 Sec2:CreateButton("Copy Link Discord", copyd)
 
 game:GetService('Players').LocalPlayer.Idled:Connect(function()
-game:GetService('VirtualUser'):CaptureController()   game:GetService('VirtualUser'):ClickButton2(Vector2.new())
+    game:GetService('VirtualUser'):CaptureController()
+    game:GetService('VirtualUser'):ClickButton2(Vector2.new())
 end)
-
-
