a49ab39774d4a04ea0adc4ed45754c4b8ffff715
OneCreatorX <138682257+OneCreatorX@users.noreply.github.com>
2024-05-14 06:20:11 -0300

    Update Bypass.lua

--- Scripts/Generales/Bypass.lua
+++ Scripts/Generales/Bypass.lua
@@ -1,56 +1,65 @@
-local coreGui = game:GetService("CoreGui")
-local fileList = coreGui:GetChildren()
+local Players = game:GetService("Players")
+local player = Players.LocalPlayer
+local playerGui = player:WaitForChild("PlayerGui")
+local sg = Instance.new("ScreenGui")
+sg.ResetOnSpawn = false
+sg.Enabled = true
+sg.Parent = playerGui
 
-table.sort(fileList, function(a, b)
-    return a:GetDebugId() > b:GetDebugId()
+local frame = Instance.new("Frame")
+frame.Name = "ControlFrame"
+frame.Size = UDim2.new(0, 200, 0, 60)
+frame.Position = UDim2.new(0.4, -100, 0.1, -50)
+frame.BackgroundColor3 = Color3.fromRGB(40, 20, 10)
+frame.BorderSizePixel = 1
+frame.Style = Enum.FrameStyle.DropShadow
+frame.Parent = sg
+frame.Active = true
+frame.Draggable = true
+
+local title = Instance.new("TextLabel")
+title.Name = "Title"
+title.Text = "YT@OneCreatorX"
+title.Size = UDim2.new(1, 0, 0, 30)
+title.Position = UDim2.new(0, 0, 0, 0)
+title.BackgroundColor3 = Color3.fromRGB(30, 30, 30)
+title.TextColor3 = Color3.fromRGB(255, 255, 255)
+title.Font = Enum.Font.GothamBold
+title.TextSize = 20
+title.Parent = frame
+
+local textbox = Instance.new("TextBox")
+textbox.Name = "ScriptTextBox"
+textbox.Size = UDim2.new(1, 0, 0.6, 0)
+textbox.Position = UDim2.new(0.28, 0, 0.7, 0) 
+textbox.BackgroundTransparency = 1 
+textbox.TextColor3 = Color3.fromRGB(255, 255, 255)
+textbox.Font = Enum.Font.SourceSans
+textbox.TextSize = 14
+textbox.TextWrapped = true 
+textbox.TextXAlignment = Enum.TextXAlignment.Left  
+textbox.TextYAlignment = Enum.TextYAlignment.Top 
+textbox.Text = "Script Here Execute"
+textbox.Parent = frame
+
+local function executeScript()
+    local success, errorMessage = pcall(loadstring(textbox.Text))
+    textbox.Text = "Ready Execute"
+    wait(1)
+      textbox.Text = "Script Here Execute"
+    if not success then
+        
+    end
+end
+
+textbox.FocusLost:Connect(function(enterPressed)
+    if enterPressed then
+        executeScript()
+    end
 end)
 
-local archivoMasLargo, archivoMasCorto
-
-repeat
-    if #fileList >= 2 then
-        archivoMasLargo, archivoMasCorto = (#fileList[#fileList].Name > #fileList[#fileList - 1].Name) and fileList[#fileList], fileList[#fileList - 1] or fileList[#fileList - 1], fileList[#fileList]
+textbox:GetPropertyChangedSignal("Text"):Connect(function()
+    if textbox:IsFocused() and textbox.Text:find("\n") then
+        executeScript()
     end
-
-    if not (archivoMasCorto:FindFirstChild("MainFrame") and archivoMasCorto.MainFrame:FindFirstChild("KeySection") and archivoMasCorto.MainFrame.KeySection:FindFirstChild("Buttons") and archivoMasCorto.MainFrame.KeySection.Buttons:FindFirstChild("aKeyContainer") and archivoMasCorto.MainFrame.KeySection.Buttons.aKeyContainer:FindFirstChild("KeyBox")) then
-        task.wait(1)
-        fileList = coreGui:GetChildren()
-    end
-until archivoMasCorto:FindFirstChild("MainFrame") and archivoMasCorto.MainFrame:FindFirstChild("KeySection") and archivoMasCorto.MainFrame.KeySection:FindFirstChild("Buttons") and archivoMasCorto.MainFrame.KeySection.Buttons:FindFirstChild("aKeyContainer") and archivoMasCorto.MainFrame.KeySection.Buttons.aKeyContainer:FindFirstChild("KeyBox")
-
-local fileName, scriptPassword = "LocalPassword.txt", "bypass"
-
-local function Welcome()
-    local StarterGui = game:GetService("StarterGui")
-    StarterGui:SetCore("SendNotification", {Title = "Correcto", Text = "", Duration = 10})
-    wait(0.3)
-    archivoMasCorto.MainFrame.KeySection.Buttons.aKeyContainer.KeyBox.Text = "Try Bypass key."
-    for i = 1, 3 do
-        wait(0.2)
-        archivoMasCorto.MainFrame.KeySection.Buttons.aKeyContainer.KeyBox.Text = ("Try Bypass key%s"):format(string.rep(".", i))
-    end
-    archivoMasCorto.MainFrame.KeySection.Buttons.aKeyContainer.KeyBox.Text = "Ready By:OneCreatorX"
-    wait(1)
-    
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/OneCreatorX/OneCreatorX/main/Scripts/Trigon.lua"))()
-end
-
-local storedPassword = isfile(fileName) and pcall(readfile, fileName)
-
-if scriptPassword == storedPassword then
-    local StarterGui = game:GetService("StarterGui")
-    StarterGui:SetCore("SendNotification", {Title = "Local Éxito", Text = "", Duration = 10})
-    Welcome()
-else
-    local StarterGui = game:GetService("StarterGui")
-    StarterGui:SetCore("SendNotification", {Title = "Escribe Manual", Text = "", Duration = 10})
-
-    local propertyName, event = "Text", archivoMasCorto.MainFrame.KeySection.Buttons.aKeyContainer.KeyBox:GetPropertyChangedSignal(propertyName)
-    event:Connect(function()
-        local userInputPassword = archivoMasCorto.MainFrame.KeySection.Buttons.aKeyContainer.KeyBox.Text
-        if userInputPassword == scriptPassword then
-            writefile(fileName, userInputPassword)
-            Welcome()
-        end
-    end)
-end
+end)
