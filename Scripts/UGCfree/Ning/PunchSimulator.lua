15c61ce86c8aeb81c7c1019b0ca90ef744ed027e
OneCreatorX <138682257+OneCreatorX@users.noreply.github.com>
2024-05-17 14:59:54 -0300

    Create PunchSimulator.lua

---
+++ Scripts/UGCfree/Ning/PunchSimulator.lua
@@ -0,0 +1,47 @@
+local Lib = loadstring(game:HttpGet("https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/wizard"))()
+local gameName = game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId).Name
+local Win = Lib:NewWindow(gameName)
+local Sec = Win:NewSection("Options")
+local Sec2 = Win:NewSection("Credits: OneCreatorX")
+local Players = game:GetService("Players")
+local Player = Players.LocalPlayer
+local arg2, arg3 = 1, 1
+
+Sec:CreateTextbox("Choose Boss (1-4)", function(value)
+    arg3 = tonumber(value) or arg3
+end)
+
+local function ah()
+    local args = {[1] = true}
+    game:GetService("ReplicatedStorage"):WaitForChild("Events"):WaitForChild("PushEvent"):FireServer(unpack(args))
+    wait(0.8)
+    Player.Character.HumanoidRootPart.CFrame = workspace.BreakableParts["Stage" .. arg2][arg3].PrimaryPart.CFrame
+end
+
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local Events = ReplicatedStorage:WaitForChild("Events")
+local AutoFight = Events:WaitForChild("AutoFight")
+local originalFireServer = AutoFight.FireServer
+
+local function newFireServer(self, ...)
+    local args = {...}
+    for i = 2, 3 do
+        args[3] = arg3
+    end
+    spawn(ah)
+    return originalFireServer(self, unpack(args))
+end
+
+local mt = getrawmetatable(game)
+local oldNamecall = mt.__namecall
+setreadonly(mt, false)
+
+mt.__namecall = newcclosure(function(self, ...)
+    local method = getnamecallmethod()
+    if method == "FireServer" and self == AutoFight then
+        return newFireServer(self, ...)
+    end
+    return oldNamecall(self, ...)
+end)
+
+setreadonly(mt, true)
