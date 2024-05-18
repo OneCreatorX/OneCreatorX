4e3e27ce2e41535f724f31f8139362320e995769
OneCreatorX <138682257+OneCreatorX@users.noreply.github.com>
2024-05-18 11:39:21 -0300

    Update Spinner.lua

--- Scripts/UGCfree/Ning/Spinner.lua
+++ Scripts/UGCfree/Ning/Spinner.lua
@@ -140,7 +140,7 @@ end
 Sec:CreateToggle("Auto Bubble", click)
 
 Sec:CreateToggle("Auto Spin", spin)
-Sec:CreateToggle("Auto Claim UGC", claim)
+Sec:CreateButton("Auto Claim UGC", claim)
 
 Sec2:CreateButton("Copy Link YouTube", function() copyToClipboard("Link YouTube") end)
 Sec2:CreateButton("Copy Link Discord", function() copyToClipboard("Link Discord") end)
