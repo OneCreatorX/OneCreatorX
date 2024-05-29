a697f209b8165bc3b0b09d8e8aa50d3cb8309ebd
OneCreatorX <138682257+OneCreatorX@users.noreply.github.com>
2024-05-29 02:14:20 -0300

    Update MyLibrery.lua

--- UIs/MyLibrery.lua
+++ UIs/MyLibrery.lua
@@ -1,5 +1,5 @@
 local UL = {}
-print("Version UI 0.1 Beta")
+print("Version UI 0.2 Beta")
 
 local uiProperties = {
     BackgroundColor3 = Color3.fromRGB(65, 65, 65),
@@ -36,7 +36,7 @@ function UL:CrFrm(parent, title)
     frm.Position = UDim2.new(0.2, 0, 2, 0)
     frm.BackgroundColor3 = Color3.fromRGB(45, 45, 45)
     frm.BackgroundTransparency = 0.4
-    frm.BorderSizePixel = 0
+    frm.BorderSizePixel = 1
     frm.Active = true
     frm.Draggable = true
 
@@ -44,7 +44,7 @@ function UL:CrFrm(parent, title)
 lbl.Parent = frm
 lbl.Text = title
 lbl.Size = UDim2.new(1, 0, 0, 35)
-lbl.Position = UDim2.new(0, 0, -0.05, 0)
+lbl.Position = UDim2.new(0, 0, 0, 0)
 lbl.BackgroundColor3 = Color3.fromRGB(128, 0, 128)
 lbl.BackgroundTransparency = 0.8
 lbl.TextColor3 = Color3.fromRGB(255, 255, 255) 
