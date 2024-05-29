0f9089ed0933429ff1ec102afc566c0910349a13
OneCreatorX <138682257+OneCreatorX@users.noreply.github.com>
2024-05-28 21:48:07 -0300

    Update MyLibrery.lua

--- UIs/MyLibrery.lua
+++ UIs/MyLibrery.lua
@@ -188,8 +188,8 @@ end
 function UL:AddOBtn(parent, name)
     local oFrm = Instance.new("Frame")
     oFrm.Parent = parent.Parent
-    oFrm.Size = UDim2.new(1, 0, 0.4, 0)
-    oFrm.Position = UDim2.new(1, 0, 0.3, 0)
+    oFrm.Size = UDim2.new(0.8, 0, 0.8, 0)
+    oFrm.Position = UDim2.new(parent.Position.X.Scale + 0.35, 0, parent.Position.Y.Scale, parent.Position.Y.Offset)
     oFrm.BackgroundColor3 = Color3.fromRGB(45, 45, 45)
     oFrm.BackgroundTransparency = 0.1
     oFrm.BorderSizePixel = 1
