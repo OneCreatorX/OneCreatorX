e2965fde7a9367461a5cd5c4e6f1dcac4d3003a5
OneCreatorX <138682257+OneCreatorX@users.noreply.github.com>
2024-05-28 22:03:47 -0300

    Update MyLibrery.lua

--- UIs/MyLibrery.lua
+++ UIs/MyLibrery.lua
@@ -23,8 +23,8 @@ end
 function UL:CrFrm(parent, title)
     local frm = Instance.new("Frame")
     frm.Parent = parent
-    frm.Size = UDim2.new(0.25, 0, 0, 30)
-    frm.Position = UDim2.new(0.325, 0, 0.3, 0)
+    frm.Size = UDim2.new(0.20, 0, 0, 30)
+    frm.Position = UDim2.new(0.1, 0, 0.2, 0)
     frm.BackgroundColor3 = Color3.fromRGB(45, 45, 45)
     frm.BackgroundTransparency = 0.1
     frm.BorderSizePixel = 0
@@ -166,6 +166,7 @@ function UL:AddTBox(parent, placeholder, cb)
     local tbox = Instance.new("TextBox")
     tbox.Parent = parent
     tbox.PlaceholderText = placeholder
+    tbox.Text = placeholder
     tbox.Size = UDim2.new(1, 0, 0, 30)
     tbox.Position = UDim2.new(0, 0, 0, #parent:GetChildren() * 30 - 30)
     tbox.BackgroundColor3 = Color3.fromRGB(65, 65, 65)
@@ -189,7 +190,7 @@ function UL:AddOBtn(parent, name)
     local oFrm = Instance.new("Frame")
     oFrm.Parent = parent.Parent
     oFrm.Size = UDim2.new(0.7, 0, 0.8, 0)
-    oFrm.Position = UDim2.new(parent.Position.X.Scale + 0.95, 0, parent.Position.Y.Scale, parent.Position.Y.Offset)
+    oFrm.Position = UDim2.new(parent.Position.X.Scale + 1, 0, parent.Position.Y.Scale, parent.Position.Y.Offset)
     oFrm.BackgroundColor3 = Color3.fromRGB(45, 45, 45)
     oFrm.BackgroundTransparency = 0.1
     oFrm.BorderSizePixel = 1
