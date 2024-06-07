8121df3fe76eee57125f1e2afffdb03bf6b13707
OneCreatorX <138682257+OneCreatorX@users.noreply.github.com>
2024-06-07 16:30:30 -0300

    Update DespicableMe4.lua

--- Scripts/UGCfree/Ning/DespicableMe4.lua
+++ Scripts/UGCfree/Ning/DespicableMe4.lua
@@ -1,5 +1,20 @@
 local UL = loadstring(game:HttpGet("https://raw.githubusercontent.com/OneCreatorX/OneCreatorX/main/UIs/MyLibrery.lua"))()
 
+local gameName = ""
+if gameName == "" then
+    gameName = game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId).Name
+end
+
+local function cleanGameName(name)
+    name = name:gsub("%b[]", "")
+    name = name:match("^[^:]*")
+    return name:match("^%s*(.-)%s*$")
+end
+
+gameName = cleanGameName(gameName)
+
+if gameName == "Story World" then
+    
 local HttpService = game:GetService("HttpService")
 local fileName = 'Rejoin.json'
 
@@ -26,20 +41,6 @@ end
 local config = readConfig()
 local rejoin = config.rejoin
 
-local gameName = ""
-if gameName == "" then
-    gameName = game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId).Name
-end
-
-local function cleanGameName(name)
-    name = name:gsub("%b[]", "")
-    name = name:match("^[^:]*")
-    return name:match("^%s*(.-)%s*$")
-end
-
-gameName = cleanGameName(gameName)
-
-if gameName == "Story World" then
     local p = game.Players.LocalPlayer
     local sg = UL:CrSG("Default")
     local frm, cfrm, crFrm = UL:CrFrm(sg, gameName)
@@ -77,7 +78,7 @@ if gameName == "Story World" then
     UL:AddBtn(crFrm, "Copy link YouTube", function() setclipboard("https://youtube.com/@onecreatorx") end)
     UL:AddBtn(crFrm, "Copy link Discord", function() setclipboard("https://discord.com/invite/UNJpdJx7c4") end)
 
-    for _, obj in ipairs(workspace.BananaSpawnPoints:GetDescendants()) do
+    for _, obj in ipairs(workspace.BananaSpawnPoints["Lobby_1"]:GetDescendants()) do
         if obj:IsA("BasePart") and obj:FindFirstChild("TouchInterest") then
             local plr = game.Players.LocalPlayer
             firetouchinterest(plr.Character.HumanoidRootPart, obj, 0)
@@ -86,11 +87,12 @@ if gameName == "Story World" then
         end
     end
 
+local p = game.Players.LocalPlayer
     firetouchinterest(p.Character.HumanoidRootPart, workspace.Maps["Lobby-1"].MissionSwitch.TriggerPart, 0)
     wait()
     firetouchinterest(p.Character.HumanoidRootPart, workspace.Maps["Lobby-1"].MissionSwitch.TriggerPart, 1)
-
-    if rejoin then
-        rejoinGame()
-    end
+if rejoin then
+    rejoinGame()
+else
+end
 end
