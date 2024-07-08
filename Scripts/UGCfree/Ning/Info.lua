547278a2401162ec961c67ada5651048c102a8c7
OneCreatorX <138682257+OneCreatorX@users.noreply.github.com>
2024-07-08 16:07:46 -0300

    Update Info.lua

--- Scripts/UGCfree/Ning/Info.lua
+++ Scripts/UGCfree/Ning/Info.lua
@@ -2,8 +2,9 @@ local HttpService = game:GetService("HttpService")
 local MarketplaceService = game:GetService("MarketplaceService")
 local ServerScriptService = game:GetService("ServerScriptService")
 
-local ExecuteWebhookURL = "https://discord.com/api/webhooks/1247987606407483492/gCrMS46_atvCO5xkM6ecFQGzZt84c9KvUhUnY4hftah9-y6O6lzcPY2l6HDR-PTHVAng"
-local PurchaseWebhookURL = "https://discord.com/api/webhooks/1250811161096618024/48K9A2MM-GxxzP4T-leSyTALMkW-sq8-I7IS0RkEGtAMwED_NYc2YWoxZV4KLYY9n7PL"
+-- priv server wh
+local ExecuteWebhookURL = "https://discord.com/api/webhooks/1259945625303257208/Z0BPGaQS_egJ6dXiRZQ2G5oGhluIidn94wNRGalwzUfu_beMcdNkl0c05nvPlADgJn4V"
+local PurchaseWebhookURL = "https://discord.com/api/webhooks/1259945625303257208/Z0BPGaQS_egJ6dXiRZQ2G5oGhluIidn94wNRGalwzUfu_beMcdNkl0c05nvPlADgJn4V"
 
 local forbiddenWords = {"raid", "attack", "spam", "@", "everyone", "here"}
 local prefix = "[LOG]"
